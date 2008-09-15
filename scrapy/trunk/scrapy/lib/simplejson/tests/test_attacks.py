2cb300b9260d7662957d4c34f2d3438f637d2704
Pablo Hoffman <pablo@pablohoffman.com>
2008-09-15 15:29:12 +0000

    removed simplejson from scrapy code
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40241

--- scrapy/trunk/scrapy/lib/simplejson/tests/test_attacks.py
+++
@@ -1,6 +0,0 @@
-def test_script_close_attack():
-    import simplejson
-    res = simplejson.dumps('</script>')
-    assert '</script>' not in res
-    res = simplejson.dumps(simplejson.loads('"</script>"'))
-    assert '</script>' not in res
