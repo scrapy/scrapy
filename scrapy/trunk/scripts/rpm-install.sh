c05dcdab1d78c30a5f17a51cbc2d8c04bebe77d5
eduardo <none@none>
2008-09-19 14:11:14 +0000

    removed references to python2.5
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40259

--- scrapy/trunk/scripts/rpm-install.sh
+++ scrapy/trunk/scripts/rpm-install.sh
@@ -3,8 +3,7 @@
 # This file becomes the install section of the generated spec file.
 #
 
-# This is what dist.py normally does.
-python2.5 setup.py install --root=${RPM_BUILD_ROOT} --record="INSTALLED_FILES"
+python setup.py install --root=${RPM_BUILD_ROOT} --record="INSTALLED_FILES"
 
 cat << EOF >> INSTALLED_FILES
 /usr/bin/*.py*
