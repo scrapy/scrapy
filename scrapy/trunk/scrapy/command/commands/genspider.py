9a7b5a7cbf4e2f2ae053de93629f50c5c73740c5
elpolilla <none@none>
2008-11-18 11:34:03 +0000

    Improved genspider command
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40384

--- scrapy/trunk/scrapy/command/commands/genspider.py
+++ scrapy/trunk/scrapy/command/commands/genspider.py
@@ -1,64 +1,66 @@
+from __future__ import with_statement
+
 import os
 import string
+import shutil
 
 from scrapy.spider import spiders
 from scrapy.command import ScrapyCommand
 from scrapy.conf import settings
+from scrapy.utils.misc import render_templatefile
 
 class Command(ScrapyCommand):
-
-    """ Default template file name """
-    template_name = 'spider.tmpl'
-
     """ Childs can define custom tvars """
     custom_tvars = {}
 
     def syntax(self):
-        return "<spider_name> <spider_domain_name>"
+        return "<spider_name> <spider_domain_name> [--template=template_name]"
 
     def short_desc(self):
-        return "Generate new spider based on predefined template"
+        return "Generate new spider based on a predefined template"
+
+    def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option("--template", dest="template", help="Uses a custom template.", default="crawl")
 
     def run(self, args, opts):
-        if len(args) != 2:
+        if len(args) < 2:
             return False
+
+        template_file = os.path.join(settings['TEMPLATES_DIR'], 'spider_%s.tmpl' % opts.template)
+        if not os.path.exists(template_file):
+            print "Template named %s.tmpl does not exist" % opts.template
+            return
 
         name = args[0]
         site = args[1]
         spiders_dict = spiders.asdict()
-        if not name in spiders_dict.keys():
-            self._genspider(name, site)
+        if not site in spiders_dict.keys():
+            name = self.normalize_name(name)
+            self._genspider(name, site, template_file)
         else:
-            print "Spider '%s' exist" % name
+            print "Spider '%s' already exists" % name
 
-    def _genspider(self, name, site):
+    def normalize_name(self, name):
+        name = name.replace('-', '_') # - are replaced by _, for valid python modules
+        if name[0] not in string.letters: # name must start with a letter, for valid python modules
+            name = "a" + name
+            print "Spider names must start with a letter; converted to %s." % name
+        return name
+        
+    def _genspider(self, name, site, template_file):
         """ Generate spider """
         tvars = {
             'name': name,
             'site': site,
-            'classname': '%sSpider' % ''.join([s.capitalize() for s in name.split('-')])
+            'classname': '%sSpider' % ''.join([s.capitalize() for s in name.split('_')])
         }
         tvars.update(self.custom_tvars)
 
         spiders_module = __import__(settings['NEWSPIDER_MODULE'], {}, {}, [''])
-        spidersdir = os.path.abspath(os.path.dirname(spiders_module.__file__))
-        if name[0] not in string.letters: # must start with a letter, for valid python modules
-            name = "a" + name
-        name = name.replace('-', '_') # - are replaced by _, for valid python modules
-        self._genfiles(self.template_name, '%s/%s.py' % (spidersdir, name), tvars)
+        spiders_dir = os.path.abspath(os.path.dirname(spiders_module.__file__))
+        spider_file = '%s/%s.py' % (spiders_dir, name)
 
-    def _genfiles(self, template_name, source_name, tvars):
-        """ Generate source from template, substitute variables """
-        template_file = os.path.join(settings['TEMPLATES_DIR'], template_name)
-        tmpl = open(template_file)
-        clines = []
-        for l in tmpl.readlines():
-            for key, val in tvars.items():
-                l = l.replace('@%s@' % key, val)
-            clines.append(l)
-        tmpl.close()
-        source = ''.join(clines)
-        if not os.path.exists(source_name):
-            sfile = open(source_name, "w")
-            sfile.write(source)
-            sfile.close()
+        shutil.copyfile(template_file, spider_file)
+        render_templatefile(spider_file, **tvars)
+
