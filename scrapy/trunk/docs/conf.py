be04a7f328a2f391a77965848a1006eb2c51127a
Ismael Carnales <icarnales@gmail.com>
2009-02-05 15:39:52 +0000

    modify docs configuration for autodoc to work
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40827

--- scrapy/trunk/docs/conf.py
+++ scrapy/trunk/docs/conf.py
@@ -12,18 +12,21 @@
 # serve to show the default.
 
 import sys, os
+from os import path
 
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.append(os.path.join(os.path.dirname(__file__), "_ext"))
+sys.path.append(path.join(path.dirname(__file__), "_ext"))
+sys.path.append(path.join(path.dirname(path.dirname(__file__)), "scrapy"))
+
 
 # General configuration
 # ---------------------
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['scrapydocs']
+extensions = ['scrapydocs', 'sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['../templates/docs']
