f4b55bfd203b22db2f101f804ddfbdb015ad90e4
Daniel Grana <dangra@gmail.com>
2009-02-24 06:57:28 +0000

    datatypes: add CaselessDict test and cleanup
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40911

--- scrapy/trunk/scrapy/utils/datatypes.py
+++ scrapy/trunk/scrapy/utils/datatypes.py
@@ -184,15 +184,10 @@ class SiteNode(object):
         return s
 
 class CaselessDict(dict):
-    def __init__(self, other=None):
-        if other:
-            # Doesn't do keyword args
-            if isinstance(other, dict):
-                for k, v in other.items():
-                    dict.__setitem__(self, self.normkey(k), v)
-            else:
-                for k, v in other:
-                    dict.__setitem__(self, self.normkey(k), v)
+    def __init__(self, seq=None):
+        dict.__init__(self)
+        if seq:
+            self.update(seq)
 
     def __getitem__(self, key):
         return dict.__getitem__(self, self.normkey(key))
@@ -205,12 +200,10 @@ class CaselessDict(dict):
 
     def __contains__(self, key):
         return dict.__contains__(self, self.normkey(key))
+    has_key = __contains__
 
     def normkey(self, key):
         return key.lower()
-
-    def has_key(self, key):
-        return dict.has_key(self, self.normkey(key))
 
     def get(self, key, def_val=None):
         return dict.get(self, self.normkey(key), def_val)
@@ -218,18 +211,16 @@ class CaselessDict(dict):
     def setdefault(self, key, def_val=None):
         return dict.setdefault(self, self.normkey(key), def_val)
 
-    def update(self, other):
-        for k, v in other.items():
-            dict.__setitem__(self, self.normkey(k), v)
+    def update(self, seq):
+        items = seq.iteritems() if isinstance(seq, dict) else seq
+        dict.update(self, ((self.normkey(k), v) for k, v in items))
 
-    def fromkeys(self, iterable, value=None):
-        d = CaselessDict()
-        for k in iterable:
-            dict.__setitem__(d, self.normkey(k), value)
-        return d
+    @classmethod
+    def fromkeys(cls, keys, value=None):
+        return cls((k, value) for k in keys)
 
-    def pop(self, key, def_val=None):
-        return dict.pop(self, self.normkey(key), def_val)
+    def pop(self, key, *args):
+        return dict.pop(self, self.normkey(key), *args)
 
 class PriorityQueue(object):
     """A simple priority queue"""
