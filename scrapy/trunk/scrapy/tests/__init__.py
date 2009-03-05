4093a4b83e51bb9d0208ddd5406f26ba0ba163ca
Pablo Hoffman <pablo@pablohoffman.com>
2009-03-05 11:12:07 +0000

    added support to run scrapy tests with: python -m scrapy.tests
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%40970

--- scrapy/trunk/scrapy/tests/__init__.py
+++ scrapy/trunk/scrapy/tests/__init__.py
@@ -1,1 +1,20 @@
-""" """
+"""
+scrapy.tests: this package contains all Scrapy unittests
+
+To run all Scrapy unittests type: 
+
+    python -m scrapy.tests
+
+Keep in mind that some tests may be skipped if you don't have a MySQL database
+up and configured appropriately.
+"""
+
+if __name__ == '__main__':
+    import os
+    from twisted.trial import runner, reporter
+
+    os.environ['SCRAPY_SETTINGS_DISABLED'] = '1'
+    loader = runner.TestLoader()
+    runner = runner.TrialRunner(reporter.TreeReporter)
+    suite = loader.loadByNames(['scrapy'], recurse=True)
+    runner.run(suite)
