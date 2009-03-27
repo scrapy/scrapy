186d0e93b0614abab27c77c9e40f122ec4044f71
Daniel Grana <dangra@gmail.com>
2009-03-27 06:46:06 +0000

    Revert "added rule shorthand, for creating CrawlSpider rules"
    This reverts commit r863.
    
    This is not the place for this shorthand, and it hasn't the blessing of
    BDFL. I look forward for the return of this shorthand.
    
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%401023

--- scrapy/trunk/scrapy/contrib/spiders/__init__.py
+++ scrapy/trunk/scrapy/contrib/spiders/__init__.py
@@ -1,2 +1,2 @@
-from scrapy.contrib.spiders.crawl import CrawlSpider, Rule, rule
+from scrapy.contrib.spiders.crawl import CrawlSpider, Rule
 from scrapy.contrib.spiders.feed import XMLFeedSpider, CSVFeedSpider
