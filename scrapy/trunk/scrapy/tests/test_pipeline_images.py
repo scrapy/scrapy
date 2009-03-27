4fa7e6cac4816c9532bc3a2fcc5871282c283ca9
Daniel Grana <dangra@gmail.com>
2009-03-27 05:42:47 +0000

    tests: fix images tests
    --HG--
    extra : convert_revision : svn%3Ab85faa78-f9eb-468e-a121-7cced6da292c%401020

--- scrapy/trunk/scrapy/tests/test_pipeline_images.py
+++ scrapy/trunk/scrapy/tests/test_pipeline_images.py
@@ -1,31 +1,39 @@
 import unittest
-from scrapy.contrib.pipeline.images import thumbnail_name, image_path
+from scrapy.contrib.pipeline.images import BaseImagesPipeline
 
 class ImagesPipelineTestCase(unittest.TestCase):
+    def setUp(self):
+        self.pipeline = BaseImagesPipeline()
+
+    def tearDown(self):
+        del self.pipeline
+
     def test_image_path(self):
+        image_path = self.pipeline.image_key
         self.assertEqual(image_path("https://dev.mydeco.com/mydeco.gif"),
-                         'dev.mydeco.com/mydeco.gif')
+                         'full/3fd165099d8e71b8a48b2683946e64dbfad8b52d.jpg')
         self.assertEqual(image_path("http://www.maddiebrown.co.uk///catalogue-items//image_54642_12175_95307.jpg"),
-                         'www.maddiebrown.co.uk/catalogue-items/image_54642_12175_95307.jpg')
+                         'full/0ffcd85d563bca45e2f90becd0ca737bc58a00b2.jpg')
         self.assertEqual(image_path("https://dev.mydeco.com/two/dirs/with%20spaces%2Bsigns.gif"),
-                         'dev.mydeco.com/two/dirs/with spaces+signs.gif')
+                         'full/b250e3a74fff2e4703e310048a5b13eba79379d2.jpg')
         self.assertEqual(image_path("http://www.dfsonline.co.uk/get_prod_image.php?img=status_0907_mdm.jpg"),
-                         'www.dfsonline.co.uk/4507be485f38b0da8a0be9eb2e1dfab8a19223f2')
+                         'full/4507be485f38b0da8a0be9eb2e1dfab8a19223f2.jpg')
         self.assertEqual(image_path("http://www.dorma.co.uk/images/product_details/2532/"),
-                         'www.dorma.co.uk/images/product_details/2532.jpg')
+                         'full/97ee6f8a46cbbb418ea91502fd24176865cf39b2.jpg')
         self.assertEqual(image_path("http://www.dorma.co.uk/images/product_details/2532"),
-                         'www.dorma.co.uk/images/product_details/2532')
+                         'full/244e0dd7d96a3b7b01f54eded250c9e272577aa1.jpg')
 
     def test_thumbnail_name(self):
+        thumbnail_name = self.pipeline.thumb_key
         name = '50'
         self.assertEqual(thumbnail_name("/tmp/foo.jpg", name),  
-                         '/tmp/foo_50.jpg')
+                         'thumbs/50/271f172bb4727281011c80fe763e93a47bb6b3fe.jpg')
         self.assertEqual(thumbnail_name("foo.png", name),
-                         'foo_50.jpg')
+                         'thumbs/50/0945c699b5580b99e4f40dffc009699b2b6830a7.jpg')
         self.assertEqual(thumbnail_name("/tmp/foo", name),
-                         '/tmp/foo_50.jpg')
+                         'thumbs/50/469150566bd728fc90b4adf6495202fd70ec3537.jpg')
         self.assertEqual(thumbnail_name("/tmp/some.name/foo", name),
-                         '/tmp/some.name/foo_50.jpg')
+                         'thumbs/50/92dac2a6a2072c5695a5dff1f865b3cb70c657bb.jpg')
 
 if __name__ == "__main__":
     unittest.main()
