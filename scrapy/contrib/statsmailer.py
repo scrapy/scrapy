a4a1c4b130a48f8729b9c89fcf0a9800461852bf
Pablo Hoffman <pablo@pablohoffman.com>
2009-07-16 09:46:30 -0300

    fixed and improved formatting of StatsMailer extension

--- scrapy/contrib/statsmailer.py
+++ scrapy/contrib/statsmailer.py
@@ -4,14 +4,11 @@ StatsMailer extension sends an email when a domain finishes scraping.
 Use STATSMAILER_RCPTS setting to enable and give the recipient mail address
 """
 
-import pprint
-
 from scrapy.xlib.pydispatch import dispatcher
 
-from scrapy.stats import stats
+from scrapy.stats import stats, signals
 from scrapy.mail import MailSender
 from scrapy.conf import settings
-from scrapy.core import signals
 from scrapy.core.exceptions import NotConfigured
 
 class StatsMailer(object):
@@ -20,9 +17,12 @@ class StatsMailer(object):
         self.recipients = settings.getlist("STATSMAILER_RCPTS")
         if not self.recipients:
             raise NotConfigured
-        dispatcher.connect(self.send_stats, signal=signals.domain_closed)
+        dispatcher.connect(self.send_stats, signal=signals.stats_domain_closing)
         
     def send_stats(self, domain):
         mail = MailSender()
-        body = pprint.pformat(stats[domain])
+        body = "Global stats\n\n"
+        body += "\n".join("%-50s : %s" % i for i in stats.get_stats().items())
+        body += "\n\n%s stats\n\n" % domain
+        body += "\n".join("%-50s : %s" % i for i in stats.get_stats(domain).items())
         mail.send(self.recipients, "Scrapy stats for: %s" % domain, body)
