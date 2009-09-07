43bd00dea2226ab7fd1ba36476bfea462111bfc3
Ismael Carnales <icarnales@gmail.com>
2009-09-07 11:14:46 -0300

    added referer spider middleware test

---
+++ scrapy/tests/test_spidermiddleware_referer.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from scrapy.http import Response, Request
+from scrapy.spider import BaseSpider
+from scrapy.contrib.spidermiddleware.referer import RefererMiddleware
+
+
+class TestRefererMiddleware(TestCase):
+
+    def setUp(self):
+        self.spider = BaseSpider()
+        self.mw = RefererMiddleware()
+
+    def test_process_spider_output(self):
+        res = Response('http://scrapytest.org')
+        reqs = [Request('http://scrapytest.org/')]
+
+        out = list(self.mw.process_spider_output(res, reqs, self.spider))
+        self.assertEquals(out[0].headers.get('Referer'),
+                          'http://scrapytest.org')
+
