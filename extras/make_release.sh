6845505e7a996fec27f5cb111ec99b85f26061b9
Pablo Hoffman <pablo@pablohoffman.com>
2009-09-17 11:46:47 -0300

    added make_release.sh script

---
+++ extras/make_release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Script to make a Scrapy release. To be run from root dir in Scrapy
+# distribution.
+
+# clean repo
+hg purge --all
+
+# build packages
+version=$(python -c "import scrapy; print scrapy.__version__")
+python setup.py sdist bdist_wininst -t "Scrapy $version" -p "win32"
+
+# hash and sign
+cd dist
+md5sum scrapy-$version* > MD5SUMS
+sha1sum scrapy-$version* > SHA1SUMS
+gpg -ba MD5SUMS
+gpg -ba SHA1SUMS
+
+# list files created
+ls -l
