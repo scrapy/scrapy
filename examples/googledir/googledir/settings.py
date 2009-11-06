4a2a20489d3d379a23c7eac3573993fb1d61e9ba
Pablo Hoffman <pablo@pablohoffman.com>
2009-11-06 16:39:15 -0200

    removed deprecated ScrapedItem (previously kept for backwards compatibility)

--- examples/googledir/googledir/settings.py
+++ examples/googledir/googledir/settings.py
@@ -7,7 +7,7 @@ BOT_VERSION = '1.0'
 
 SPIDER_MODULES = ['googledir.spiders']
 NEWSPIDER_MODULE = 'googledir.spiders'
-DEFAULT_ITEM_CLASS = 'scrapy.item.ScrapedItem'
+DEFAULT_ITEM_CLASS = 'scrapy.item.Item'
 USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
 
 ITEM_PIPELINES = ['googledir.pipelines.FilterWordsPipeline']
