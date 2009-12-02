dda7e01d2f0bf1262f5ec10485ca05dde47464b7
Pablo Hoffman <pablo@pablohoffman.com>
2009-12-02 11:26:34 -0200

    don't fail if OpenSSL is not installed in test_dependencies.py

--- scrapy/tests/test_dependencies.py
+++ scrapy/tests/test_dependencies.py
@@ -1,11 +1,11 @@
-from unittest import TestCase, main
+from twisted.trial import unittest
 
-class ScrapyUtilsTest(TestCase):
-    def test_openssl(self):
+class ScrapyUtilsTest(unittest.TestCase):
+    def test_required_openssl_version(self):
         try:
             module = __import__('OpenSSL', {}, {}, [''])
         except ImportError, ex:
-            return # no openssl installed
+            raise unittest.SkipTest("OpenSSL is not available")
 
         required_version = '0.6'
         if hasattr(module, '__version__'):
@@ -13,4 +13,4 @@ class ScrapyUtilsTest(TestCase):
                 self.assertFalse(cur < req, "module %s >= %s required" % ('OpenSSL', required_version))
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
