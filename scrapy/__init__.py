f22fe95ae9e8b5fffc856745fc4ce24a7085df0f
Pablo Hoffman <pablo@pablohoffman.com>
2009-12-12 15:31:03 -0200

    changed version to 0.8-rc1

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -2,9 +2,8 @@
 Scrapy - a screen scraping framework written in Python
 """
 
-# IMPORTANT: remember to also update the version in docs/conf.py
-version_info = (0, 8, 0, '', 0)
-__version__ = "0.8-dev"
+version_info = (0, 8, 0, 'rc1', 0)
+__version__ = "0.8-rc1"
 
 import sys, os
 
