cca4be4d64f5e23e324a14f537cc8d13c1bc1403
Pablo Hoffman <pablo@pablohoffman.com>
2009-12-11 23:52:53 -0200

    uncommented some lines in extras/build_release.sh

--- extras/build_release.sh
+++ extras/build_release.sh
@@ -6,8 +6,8 @@
 hg purge --all
 
 # build packages
-#version=$(python -c "import scrapy; print scrapy.__version__")
-#python setup.py sdist
+version=$(python -c "import scrapy; print scrapy.__version__")
+python setup.py sdist
 # FIXME: bdist_wininst doesn't work on Unix (it doesn't include the data_files)
 # To build the win32 release you need to use Windows for now.
 #python setup.py bdist_wininst -t "Scrapy $version" -p "win32"
