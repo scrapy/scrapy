2f75839e7a9a92e8f01943492d56e454809b2123
Pablo Hoffman <pablo@pablohoffman.com>
2010-03-27 13:23:13 -0300

    Ignore noisy Twisted deprecation warnings

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -5,11 +5,14 @@ Scrapy - a screen scraping framework written in Python
 version_info = (0, 8, 0, '', 0)
 __version__ = "0.8"
 
-import sys, os
+import sys, os, warnings
 
 if sys.version_info < (2,5):
     print "Scrapy %s requires Python 2.5 or above" % __version__
     sys.exit(1)
+
+# ignore noisy twisted deprecation warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning, module='twisted')
 
 # monkey patches to fix external library issues
 from scrapy.xlib import twisted_250_monkeypatches
