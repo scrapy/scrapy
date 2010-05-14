c87a29eb9edda23d70861ab041e1816efbdfa01a
Pablo Hoffman <pablo@pablohoffman.com>
2010-05-14 14:48:34 -0300

    improved docstring

--- scrapy/contrib/ibl/__init__.py
+++ scrapy/contrib/ibl/__init__.py
@@ -2,9 +2,15 @@
 This contrib implements an automatic extraction library based on an Instance
 Based Learning (IBL) algorithm, as described in the following papers:
 
-A hierarchical approach to wrapper induction
+    A hierarchical approach to wrapper induction
     http://portal.acm.org/citation.cfm?id=301191 
 
-Extracting web data using instance based learning
+    Extracting web data using instance based learning
     http://portal.acm.org/citation.cfm?id=1265174 
+
+This code has some additional dependencies too:
+
+* numpy
+* nltk
+
 """
