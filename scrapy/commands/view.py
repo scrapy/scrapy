2d3135603ecf58eaa46cd31b59782836804bf945
Pablo Hoffman <pablo@pablohoffman.com>
2010-05-26 10:29:32 -0300

    added scrapy-ctl view command

---
+++ scrapy/commands/view.py
@@ -0,0 +1,20 @@
+from scrapy.command import ScrapyCommand
+from scrapy.commands import fetch
+from scrapy.utils.response import open_in_browser
+
+class Command(fetch.Command):
+
+    def short_desc(self):
+        return "Open a URL in browser, as seen by Scrapy"
+
+    def long_desc(self):
+        return "Fetch a URL using the Scrapy downloader and show its " \
+            "contents in a browser"
+
+    def add_options(self, parser):
+        ScrapyCommand.add_options(self, parser)
+        parser.add_option("--spider", dest="spider",
+            help="use this spider")
+
+    def _print_response(self, response, opts):
+        open_in_browser(response)
