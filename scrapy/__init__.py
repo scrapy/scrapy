6525d3fe8c0db067841118830577ac21ee637699
Pablo Hoffman <pablo@pablohoffman.com>
2010-06-28 00:54:43 -0300

    bumped version to 0.9 final

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -2,8 +2,8 @@
 Scrapy - a screen scraping framework written in Python
 """
 
-version_info = (0, 9, 0, 'rc1')
-__version__ = "0.9-rc1"
+version_info = (0, 9, 0, '')
+__version__ = "0.9"
 
 import sys, os, warnings
 
