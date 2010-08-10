dedd6b2256fa2079cbbfaefbd9875084ac443389
Pablo Hoffman <pablo@pablohoffman.com>
2010-08-10 17:42:05 -0300

    added more information to deprecation notice

--- scrapy/core/exceptions.py
+++ scrapy/core/exceptions.py
@@ -1,5 +1,5 @@
 from scrapy.exceptions import *
 
 import warnings
-warnings.warn("scrapy.core.exceptions is deprecated, use scrapy.exceptions instead", \
+warnings.warn("scrapy.core.exceptions is deprecated and will be removed in Scrapy 0.11, use scrapy.exceptions instead", \
     DeprecationWarning, stacklevel=2)
--- scrapy/core/signals.py
+++ scrapy/core/signals.py
@@ -1,5 +1,5 @@
 from scrapy.signals import *
 
 import warnings
-warnings.warn("scrapy.core.signals is deprecated, use scrapy.signals instead", \
+warnings.warn("scrapy.core.signals is deprecated and will be removed in Scrapy 0.11, use scrapy.signals instead", \
     DeprecationWarning, stacklevel=2)
