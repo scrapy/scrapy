648f700ed11ec9ab45cf91fae940e4f8b8aeb0fd
Pablo Hoffman <pablo@pablohoffman.com>
2010-08-26 23:23:58 -0300

    Fixed log formatter tests
    --HG--
    rename : scrapy/tests/test_contrib_logformatter.py => scrapy/tests/test_logformatter.py

--- scrapy/tests/test_logformatter.py
+++ scrapy/tests/test_logformatter.py
@@ -1,20 +1,25 @@
 import unittest
 
+from scrapy.spider import BaseSpider
 from scrapy.http import Request, Response
-from scrapy.contrib.logformatter import crawled_logline
+from scrapy.logformatter import LogFormatter
 
 
 class LoggingContribTest(unittest.TestCase):
 
-    def test_crawled_logline(self):
+    def setUp(self):
+        self.formatter = LogFormatter()
+        self.spider = BaseSpider('default')
+
+    def test_crawled(self):
         req = Request("http://www.example.com")
         res = Response("http://www.example.com")
-        self.assertEqual(crawled_logline(req, res),
+        self.assertEqual(self.formatter.crawled(req, res, self.spider),
             "Crawled (200) <GET http://www.example.com> (referer: None)")
 
         req = Request("http://www.example.com", headers={'referer': 'http://example.com'})
         res = Response("http://www.example.com", flags=['cached'])
-        self.assertEqual(crawled_logline(req, res),
+        self.assertEqual(self.formatter.crawled(req, res, self.spider),
             "Crawled (200) <GET http://www.example.com> (referer: http://example.com) ['cached']")
 
 
