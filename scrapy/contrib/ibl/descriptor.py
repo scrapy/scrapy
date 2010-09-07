dbc0ba7236ef8828b2e566cd2aee6a00cc62c049
Pablo Hoffman <pablo@pablohoffman.com>
2010-09-07 14:26:50 -0300

    Added class constructors scrapy.contrib.ibl FieldDescriptor and ItemDescriptor

--- scrapy/contrib/ibl/descriptor.py
+++ scrapy/contrib/ibl/descriptor.py
@@ -17,6 +17,12 @@ class FieldDescriptor(object):
         self.required = required
         self.allow_markup = allow_markup
     
+    @classmethod
+    def from_field(cls, name, field):
+        return cls(name, field.get('description'), \
+            field.get('ibl_extractor', text), field.get('required', False), \
+            field.get('allow_markup', False))
+
     def __str__(self):
         return "FieldDescriptor(%s)" % self.name
 
@@ -32,6 +38,11 @@ class ItemDescriptor(object):
         self.attribute_map = dict((d.name, d) for d in attribute_descriptors)
         self._required_attributes = [d.name for d in attribute_descriptors \
                 if d.required]
+
+    @classmethod
+    def from_item(cls, name, description, item):
+        a = [FieldDescriptor.from_field(n, f) for n, f in item.fields.items()]
+        return cls(name, description, a)
 
     def validated(self, data):
         """Only return the items in the data that are valid"""
