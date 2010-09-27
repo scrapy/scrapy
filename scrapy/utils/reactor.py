49ffe528a39e29a84246b31097e2e6aa90cbbb8f
Pablo Hoffman <pablo@pablohoffman.com>
2010-09-27 12:27:32 -0300

    Fixed listen_tcp function when receiving None or 0 in portrange argument. Closes #252

--- scrapy/utils/reactor.py
+++ scrapy/utils/reactor.py
@@ -2,9 +2,11 @@ from twisted.internet import reactor, error
 
 def listen_tcp(portrange, factory):
     """Like reactor.listenTCP but tries different ports in a range."""
-    assert len(portrange) in [1, 2], "invalid portrange: %s" % portrange
+    assert len(portrange) <= 2, "invalid portrange: %s" % portrange
     if not hasattr(portrange, '__iter__'):
         return reactor.listenTCP(portrange, factory)
+    if not portrange:
+        return reactor.listenTCP(0, factory)
     if len(portrange) == 1:
         return reactor.listenTCP(portrange[0], factory)
     for x in range(portrange[0], portrange[1]+1):
