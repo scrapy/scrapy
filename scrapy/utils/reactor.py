130276605b0610175f8317c6e535467de5dbfae0
dfdeshom <dfdeshom@gmail.com>
2010-11-01 00:59:04 -0200

    Bind the web server and telnet server to a configurable interface (WEBSERVICE_HOST). The default is to bind to all interfaces. Also add documentation for WEBSERVICE_HOST and TELNETCONSOLE_HOST.

--- scrapy/utils/reactor.py
+++ scrapy/utils/reactor.py
@@ -1,17 +1,17 @@
 from twisted.internet import reactor, error
 
-def listen_tcp(portrange, factory):
+def listen_tcp(portrange, host, factory):
     """Like reactor.listenTCP but tries different ports in a range."""
     assert len(portrange) <= 2, "invalid portrange: %s" % portrange
     if not hasattr(portrange, '__iter__'):
-        return reactor.listenTCP(portrange, factory)
+        return reactor.listenTCP(portrange, factory, interface=host)
     if not portrange:
-        return reactor.listenTCP(0, factory)
+        return reactor.listenTCP(0, factory, interface=host)
     if len(portrange) == 1:
-        return reactor.listenTCP(portrange[0], factory)
+        return reactor.listenTCP(portrange[0], factory, interface=host)
     for x in range(portrange[0], portrange[1]+1):
         try:
-            return reactor.listenTCP(x, factory)
+            return reactor.listenTCP(x, factory, interface=host)
         except error.CannotListenError:
             if x == portrange[1]:
                 raise
