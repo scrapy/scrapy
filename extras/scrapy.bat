91e6753035782356a812b0b2e4700a1894a141dd
Pablo Hoffman <pablo@pablohoffman.com>
2010-11-22 00:39:45 -0200

    scrapy.bat: minor fix to support spaces in python installation dir (windows)

--- extras/scrapy.bat
+++ extras/scrapy.bat
@@ -6,5 +6,5 @@ rem Use a full path to Python (relative to this script) as the standard Python
 rem install does not put python.exe on the PATH...
 rem %~dp0 is the directory of this script
 
-%~dp0..\python "%~dp0scrapy" %*
+"%~dp0..\python" "%~dp0scrapy" %*
 endlocal
