3df9070ffd2ab7b475af29d36a7a8a8b4559cb48
Pablo Hoffman <pablo@pablohoffman.com>
2010-12-14 12:20:38 -0200

    fixed bin\runtests.bat windows script

--- bin/runtests.bat
+++ bin/runtests.bat
@@ -1,19 +1,6 @@
 @ECHO off
 
-SET test="scrapy scrapyd"
+SET test=scrapy
+SET PYTHONPATH=%CD%
 IF NOT "%1" == "" SET test="%1"
-
-IF EXIST c:\python26\scripts\trial.py GOTO py26
-IF EXIST c:\python25\scripts\trial.py GOTO py25
-
-ECHO "Unable to run tests: trial command (included with Twisted) not found"
-GOTO end
-
-:py26
-c:\python26\scripts\trial.py %test%
-GOTO end
-
-:py25
-c:\python25\scripts\trial.py %test%
-
-:end
+trial --reporter=text %test%
