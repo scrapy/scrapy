b10f4fae3534b0b3e25cbb3e4d3770ff0501913b
Pablo Hoffman <pablo@pablohoffman.com>
2011-04-18 22:37:19 -0300

    Moved several functions from scrapy.utils.{http,markup,multipart,response,url} (and their tests) to a new library called 'w3lib'. Scrapy will now depend on w3lib.

--- scrapy/tests/test_utils_url.py
+++ scrapy/tests/test_utils_url.py
@@ -1,9 +1,6 @@
-import os
 import unittest
 from scrapy.spider import BaseSpider
-from scrapy.utils.url import url_is_from_any_domain, safe_url_string, safe_download_url, \
-    url_query_parameter, add_or_replace_parameter, url_query_cleaner, canonicalize_url, \
-    urljoin_rfc, url_is_from_spider, file_uri_to_path, path_to_file_uri, any_to_uri
+from scrapy.utils.url import url_is_from_any_domain, url_is_from_spider, canonicalize_url
 
 class UrlUtilsTest(unittest.TestCase):
 
@@ -54,144 +51,6 @@ class UrlUtilsTest(unittest.TestCase):
         self.assertTrue(url_is_from_spider('http://www.example.org/some/page.html', MySpider))
         self.assertTrue(url_is_from_spider('http://www.example.net/some/page.html', MySpider))
         self.assertFalse(url_is_from_spider('http://www.example.us/some/page.html', MySpider))
-
-    def test_urljoin_rfc(self):
-        self.assertEqual(urljoin_rfc('http://example.com/some/path', 'newpath/test'),
-                                     'http://example.com/some/newpath/test')
-        self.assertEqual(urljoin_rfc('http://example.com/some/path/a.jpg', '../key/other'),
-                                     'http://example.com/some/key/other')
-        u = urljoin_rfc(u'http://example.com/lolo/\xa3/lele', u'lala/\xa3')
-        self.assertEqual(u, 'http://example.com/lolo/\xc2\xa3/lala/\xc2\xa3')
-        assert isinstance(u, str)
-        u = urljoin_rfc(u'http://example.com/lolo/\xa3/lele', 'lala/\xa3', encoding='latin-1')
-        self.assertEqual(u, 'http://example.com/lolo/\xa3/lala/\xa3')
-        assert isinstance(u, str)
-        u = urljoin_rfc('http://example.com/lolo/\xa3/lele', 'lala/\xa3')
-        self.assertEqual(u, 'http://example.com/lolo/\xa3/lala/\xa3')
-        assert isinstance(u, str)
-
-    def test_safe_url_string(self):
-        # Motoko Kusanagi (Cyborg from Ghost in the Shell)
-        motoko = u'\u8349\u8599 \u7d20\u5b50'
-        self.assertEqual(safe_url_string(motoko),  # note the %20 for space
-                        '%E8%8D%89%E8%96%99%20%E7%B4%A0%E5%AD%90')
-        self.assertEqual(safe_url_string(motoko),
-                         safe_url_string(safe_url_string(motoko)))
-        self.assertEqual(safe_url_string(u'\xa9'), # copyright symbol
-                         '%C2%A9')
-        self.assertEqual(safe_url_string(u'\xa9', 'iso-8859-1'),
-                         '%A9')
-        self.assertEqual(safe_url_string("http://www.scrapy.org/"),
-                        'http://www.scrapy.org/')
-
-        alessi = u'/ecommerce/oggetto/Te \xf2/tea-strainer/1273'
-
-        self.assertEqual(safe_url_string(alessi),
-                         '/ecommerce/oggetto/Te%20%C3%B2/tea-strainer/1273')
-
-        self.assertEqual(safe_url_string("http://www.example.com/test?p(29)url(http://www.another.net/page)"),
-                                         "http://www.example.com/test?p(29)url(http://www.another.net/page)")
-        self.assertEqual(safe_url_string("http://www.example.com/Brochures_&_Paint_Cards&PageSize=200"),
-                                         "http://www.example.com/Brochures_&_Paint_Cards&PageSize=200")
-
-        safeurl = safe_url_string(u"http://www.example.com/\xa3", encoding='latin-1')
-        self.assert_(isinstance(safeurl, str))
-        self.assertEqual(safeurl, "http://www.example.com/%A3")
-
-        safeurl = safe_url_string(u"http://www.example.com/\xa3", encoding='utf-8')
-        self.assert_(isinstance(safeurl, str))
-        self.assertEqual(safeurl, "http://www.example.com/%C2%A3")
-
-    def test_safe_download_url(self):
-        self.assertEqual(safe_download_url('http://www.scrapy.org/../'),
-                         'http://www.scrapy.org/')
-        self.assertEqual(safe_download_url('http://www.scrapy.org/../../images/../image'),
-                         'http://www.scrapy.org/image')
-        self.assertEqual(safe_download_url('http://www.scrapy.org/dir/'),
-                         'http://www.scrapy.org/dir/')
-
-    def test_url_query_parameter(self):
-        self.assertEqual(url_query_parameter("product.html?id=200&foo=bar", "id"),
-                         '200')
-        self.assertEqual(url_query_parameter("product.html?id=200&foo=bar", "notthere", "mydefault"),
-                         'mydefault')
-        self.assertEqual(url_query_parameter("product.html?id=", "id"),
-                         None)
-        self.assertEqual(url_query_parameter("product.html?id=", "id", keep_blank_values=1),
-                         '')
-
-    def test_url_query_parameter_2(self):
-        """
-        This problem was seen several times in the feeds. Sometime affiliate URLs contains
-        nested encoded affiliate URL with direct URL as parameters. For example:
-        aff_url1 = 'http://www.tkqlhce.com/click-2590032-10294381?url=http%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FArgosCreateReferral%3FstoreId%3D10001%26langId%3D-1%26referrer%3DCOJUN%26params%3Dadref%253DGarden+and+DIY-%3EGarden+furniture-%3EChildren%26%2339%3Bs+garden+furniture%26referredURL%3Dhttp%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FProductDisplay%253FstoreId%253D10001%2526catalogId%253D1500001501%2526productId%253D1500357023%2526langId%253D-1'
-        the typical code to extract needed URL from it is:
-        aff_url2 = url_query_parameter(aff_url1, 'url')
-        after this aff2_url is:
-        'http://www.argos.co.uk/webapp/wcs/stores/servlet/ArgosCreateReferral?storeId=10001&langId=-1&referrer=COJUN&params=adref%3DGarden and DIY->Garden furniture->Children&#39;s gardenfurniture&referredURL=http://www.argos.co.uk/webapp/wcs/stores/servlet/ProductDisplay%3FstoreId%3D10001%26catalogId%3D1500001501%26productId%3D1500357023%26langId%3D-1'
-        the direct URL extraction is
-        url = url_query_parameter(aff_url2, 'referredURL')
-        but this will not work, because aff_url2 contains &#39; (comma sign encoded in the feed)
-        and the URL extraction will fail, current workaround was made in the spider,
-        just a replace for &#39; to %27
-        """
-        return # FIXME: this test should pass but currently doesnt
-        # correct case
-        aff_url1 = "http://www.anrdoezrs.net/click-2590032-10294381?url=http%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FArgosCreateReferral%3FstoreId%3D10001%26langId%3D-1%26referrer%3DCOJUN%26params%3Dadref%253DGarden+and+DIY-%3EGarden+furniture-%3EGarden+table+and+chair+sets%26referredURL%3Dhttp%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FProductDisplay%253FstoreId%253D10001%2526catalogId%253D1500001501%2526productId%253D1500357199%2526langId%253D-1"
-        aff_url2 = url_query_parameter(aff_url1, 'url')
-        self.assertEqual(aff_url2, "http://www.argos.co.uk/webapp/wcs/stores/servlet/ArgosCreateReferral?storeId=10001&langId=-1&referrer=COJUN&params=adref%3DGarden and DIY->Garden furniture->Garden table and chair sets&referredURL=http://www.argos.co.uk/webapp/wcs/stores/servlet/ProductDisplay%3FstoreId%3D10001%26catalogId%3D1500001501%26productId%3D1500357199%26langId%3D-1")
-        prod_url = url_query_parameter(aff_url2, 'referredURL')
-        self.assertEqual(prod_url, "http://www.argos.co.uk/webapp/wcs/stores/servlet/ProductDisplay?storeId=10001&catalogId=1500001501&productId=1500357199&langId=-1")
-        # weird case
-        aff_url1 = "http://www.tkqlhce.com/click-2590032-10294381?url=http%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FArgosCreateReferral%3FstoreId%3D10001%26langId%3D-1%26referrer%3DCOJUN%26params%3Dadref%253DGarden+and+DIY-%3EGarden+furniture-%3EChildren%26%2339%3Bs+garden+furniture%26referredURL%3Dhttp%3A%2F%2Fwww.argos.co.uk%2Fwebapp%2Fwcs%2Fstores%2Fservlet%2FProductDisplay%253FstoreId%253D10001%2526catalogId%253D1500001501%2526productId%253D1500357023%2526langId%253D-1"
-        aff_url2 = url_query_parameter(aff_url1, 'url')
-        self.assertEqual(aff_url2, "http://www.argos.co.uk/webapp/wcs/stores/servlet/ArgosCreateReferral?storeId=10001&langId=-1&referrer=COJUN&params=adref%3DGarden and DIY->Garden furniture->Children&#39;s garden furniture&referredURL=http://www.argos.co.uk/webapp/wcs/stores/servlet/ProductDisplay%3FstoreId%3D10001%26catalogId%3D1500001501%26productId%3D1500357023%26langId%3D-1")
-        prod_url = url_query_parameter(aff_url2, 'referredURL')
-        # fails, prod_url is None now
-        self.assertEqual(prod_url, "http://www.argos.co.uk/webapp/wcs/stores/servlet/ProductDisplay?storeId=10001&catalogId=1500001501&productId=1500357023&langId=-1")
-
-    def test_add_or_replace_parameter(self):
-        url = 'http://domain/test'
-        self.assertEqual(add_or_replace_parameter(url, 'arg', 'v'),
-                         'http://domain/test?arg=v')
-        url = 'http://domain/test?arg1=v1&arg2=v2&arg3=v3'
-        self.assertEqual(add_or_replace_parameter(url, 'arg4', 'v4'),
-                         'http://domain/test?arg1=v1&arg2=v2&arg3=v3&arg4=v4')
-        self.assertEqual(add_or_replace_parameter(url, 'arg3', 'nv3'),
-                         'http://domain/test?arg1=v1&arg2=v2&arg3=nv3')
-        url = 'http://domain/test?arg1=v1'
-        self.assertEqual(add_or_replace_parameter(url, 'arg2', 'v2', sep=';'),
-                         'http://domain/test?arg1=v1;arg2=v2')
-        self.assertEqual(add_or_replace_parameter("http://domain/moreInfo.asp?prodID=", 'prodID', '20'),
-                         'http://domain/moreInfo.asp?prodID=20')
-        url = 'http://rmc-offers.co.uk/productlist.asp?BCat=2%2C60&CatID=60'
-        self.assertEqual(add_or_replace_parameter(url, 'BCat', 'newvalue', url_is_quoted=True),
-                         'http://rmc-offers.co.uk/productlist.asp?BCat=newvalue&CatID=60')
-        url = 'http://rmc-offers.co.uk/productlist.asp?BCat=2,60&CatID=60'
-        self.assertEqual(add_or_replace_parameter(url, 'BCat', 'newvalue'),
-                         'http://rmc-offers.co.uk/productlist.asp?BCat=newvalue&CatID=60')
-
-    def test_url_query_cleaner(self):
-        self.assertEqual('product.html?id=200',
-                url_query_cleaner("product.html?id=200&foo=bar&name=wired", ['id']))
-        self.assertEqual('product.html?id=200',
-                url_query_cleaner("product.html?&id=200&&foo=bar&name=wired", ['id']))
-        self.assertEqual('product.html',
-                url_query_cleaner("product.html?foo=bar&name=wired", ['id']))
-        self.assertEqual('product.html?id=200&name=wired',
-                url_query_cleaner("product.html?id=200&foo=bar&name=wired", ['id', 'name']))
-        self.assertEqual('product.html?id',
-                url_query_cleaner("product.html?id&other=3&novalue=", ['id']))
-        self.assertEqual('product.html?d=1&d=2&d=3',
-                url_query_cleaner("product.html?d=1&e=b&d=2&d=3&other=other", ['d'], unique=False))
-        self.assertEqual('product.html?id=200&foo=bar',
-                url_query_cleaner("product.html?id=200&foo=bar&name=wired#id20", ['id', 'foo']))
-        self.assertEqual('product.html?foo=bar&name=wired',
-                url_query_cleaner("product.html?id=200&foo=bar&name=wired", ['id'], remove=True))
-        self.assertEqual('product.html?name=wired',
-                url_query_cleaner("product.html?id=2&foo=bar&name=wired", ['id', 'foo'], remove=True))
-        self.assertEqual('product.html?foo=bar&name=wired',
-                url_query_cleaner("product.html?id=2&foo=bar&name=wired", ['id', 'footo'], remove=True))
 
     def test_canonicalize_url(self):
         # simplest case
@@ -282,50 +141,6 @@ class UrlUtilsTest(unittest.TestCase):
         # domains are case insensitive
         self.assertEqual(canonicalize_url("http://www.EXAMPLE.com"),
                                           "http://www.example.com")
-
-    def test_path_to_file_uri(self):
-        if os.name == 'nt':
-            self.assertEqual(path_to_file_uri("C:\\windows\clock.avi"),
-                             "file:///C:/windows/clock.avi")
-        else:
-            self.assertEqual(path_to_file_uri("/some/path.txt"),
-                             "file:///some/path.txt")
-
-        fn = "test.txt"
-        x = path_to_file_uri(fn)
-        self.assert_(x.startswith('file:///'))
-        self.assertEqual(file_uri_to_path(x).lower(), os.path.abspath(fn).lower())
-
-    def test_file_uri_to_path(self):
-        if os.name == 'nt':
-            self.assertEqual(file_uri_to_path("file:///C:/windows/clock.avi"),
-                             "C:\\windows\clock.avi")
-            uri = "file:///C:/windows/clock.avi"
-            uri2 = path_to_file_uri(file_uri_to_path(uri))
-            self.assertEqual(uri, uri2)
-        else:
-            self.assertEqual(file_uri_to_path("file:///path/to/test.txt"),
-                             "/path/to/test.txt")
-            self.assertEqual(file_uri_to_path("/path/to/test.txt"),
-                             "/path/to/test.txt")
-            uri = "file:///path/to/test.txt"
-            uri2 = path_to_file_uri(file_uri_to_path(uri))
-            self.assertEqual(uri, uri2)
-
-        self.assertEqual(file_uri_to_path("test.txt"),
-                         "test.txt")
-
-    def test_any_to_uri(self):
-        if os.name == 'nt':
-            self.assertEqual(any_to_uri("C:\\windows\clock.avi"),
-                             "file:///C:/windows/clock.avi")
-        else:
-            self.assertEqual(any_to_uri("/some/path.txt"),
-                             "file:///some/path.txt")
-        self.assertEqual(any_to_uri("file:///some/path.txt"),
-                         "file:///some/path.txt")
-        self.assertEqual(any_to_uri("http://www.example.com/some/path.txt"),
-                         "http://www.example.com/some/path.txt")
 
 
 if __name__ == "__main__":
--- scrapy/utils/url.py
+++ scrapy/utils/url.py
@@ -1,6 +1,9 @@
 """
 This module contains general purpose URL functions not found in the standard
 library.
+
+Some of the functions that used to be imported from this module have been moved
+to the w3lib.url module. Always import those from there instead.
 """
 
 import os
@@ -10,6 +13,7 @@ import urllib
 import posixpath
 import cgi
 
+from w3lib.url import *
 from scrapy.utils.python import unicode_to_str
 
 def url_is_from_any_domain(url, domains):
@@ -25,109 +29,6 @@ def url_is_from_spider(url, spider):
     """Return True if the url belongs to the given spider"""
     return url_is_from_any_domain(url, [spider.name] + \
         getattr(spider, 'allowed_domains', []))
-
-def urljoin_rfc(base, ref, encoding='utf-8'):
-    """Same as urlparse.urljoin but supports unicode values in base and ref
-    parameters (in which case they will be converted to str using the given
-    encoding).
-
-    Always returns a str.
-    """
-    return urlparse.urljoin(unicode_to_str(base, encoding), \
-        unicode_to_str(ref, encoding))
-
-_reserved = ';/?:@&=+$|,#' # RFC 3986 (Generic Syntax)
-_unreserved_marks = "-_.!~*'()" # RFC 3986 sec 2.3
-_safe_chars = urllib.always_safe + '%' + _reserved + _unreserved_marks
-
-def safe_url_string(url, encoding='utf8'):
-    """Convert the given url into a legal URL by escaping unsafe characters
-    according to RFC-3986.
-
-    If a unicode url is given, it is first converted to str using the given
-    encoding (which defaults to 'utf-8'). When passing a encoding, you should
-    use the encoding of the original page (the page from which the url was
-    extracted from).
-
-    Calling this function on an already "safe" url will return the url
-    unmodified.
-
-    Always returns a str.
-    """
-    s = unicode_to_str(url, encoding)
-    return urllib.quote(s,  _safe_chars)
-
-
-_parent_dirs = re.compile(r'/?(\.\./)+')
-
-def safe_download_url(url):
-    """ Make a url for download. This will call safe_url_string
-    and then strip the fragment, if one exists. The path will
-    be normalised.
-
-    If the path is outside the document root, it will be changed
-    to be within the document root.
-    """
-    safe_url = safe_url_string(url)
-    scheme, netloc, path, query, _ = urlparse.urlsplit(safe_url)
-    if path:
-        path = _parent_dirs.sub('', posixpath.normpath(path))
-        if url.endswith('/') and not path.endswith('/'):
-            path += '/'
-    else:
-        path = '/'
-    return urlparse.urlunsplit((scheme, netloc, path, query, ''))
-
-def is_url(text):
-    return text.partition("://")[0] in ('file', 'http', 'https')
-
-def url_query_parameter(url, parameter, default=None, keep_blank_values=0):
-    """Return the value of a url parameter, given the url and parameter name"""
-    queryparams = cgi.parse_qs(urlparse.urlsplit(str(url))[3], \
-        keep_blank_values=keep_blank_values)
-    return queryparams.get(parameter, [default])[0]
-
-def url_query_cleaner(url, parameterlist=(), sep='&', kvsep='=', remove=False, unique=True):
-    """Clean url arguments leaving only those passed in the parameterlist keeping order
-
-    If remove is True, leave only those not in parameterlist.
-    If unique is False, do not remove duplicated keys
-    """
-    url = urlparse.urldefrag(url)[0]
-    base, _, query = url.partition('?')
-    seen = set()
-    querylist = []
-    for ksv in query.split(sep):
-        k, _, _ = ksv.partition(kvsep)
-        if unique and k in seen:
-            continue
-        elif remove and k in parameterlist:
-            continue
-        elif not remove and k not in parameterlist:
-            continue
-        else:
-            querylist.append(ksv)
-            seen.add(k)
-    return '?'.join([base, sep.join(querylist)]) if querylist else base
-
-def add_or_replace_parameter(url, name, new_value, sep='&', url_is_quoted=False):
-    """Add or remove a parameter to a given url"""
-    def has_querystring(url):
-        _, _, _, query, _ = urlparse.urlsplit(url)
-        return bool(query)
-
-    parameter = url_query_parameter(url, name, keep_blank_values=1)
-    if url_is_quoted:
-        parameter = urllib.quote(parameter)
-    if parameter is None:
-        if has_querystring(url):
-            next_url = url + sep + name + '=' + new_value
-        else:
-            next_url = url + '?' + name + '=' + new_value
-    else:
-        next_url = url.replace(name+'='+parameter,
-                               name+'='+new_value)
-    return next_url
 
 def canonicalize_url(url, keep_blank_values=True, keep_fragments=False, \
         encoding=None):
@@ -155,27 +56,3 @@ def canonicalize_url(url, keep_blank_values=True, keep_fragments=False, \
     path = safe_url_string(urllib.unquote(path))
     fragment = '' if not keep_fragments else fragment
     return urlparse.urlunparse((scheme, netloc.lower(), path, params, query, fragment))
-
-def path_to_file_uri(path):
-    """Convert local filesystem path to legal File URIs as described in:
-    http://en.wikipedia.org/wiki/File_URI_scheme
-    """
-    x = urllib.pathname2url(os.path.abspath(path))
-    if os.name == 'nt':
-        x = x.replace('|', ':') # http://bugs.python.org/issue5861
-    return 'file:///%s' % x.lstrip('/')
-
-def file_uri_to_path(uri):
-    """Convert File URI to local filesystem path according to:
-    http://en.wikipedia.org/wiki/File_URI_scheme
-    """
-    return urllib.url2pathname(urlparse.urlparse(uri).path)
-
-def any_to_uri(uri_or_path):
-    """If given a path name, return its File URI, otherwise return it
-    unmodified
-    """
-    if os.path.splitdrive(uri_or_path)[0]:
-        return path_to_file_uri(uri_or_path)
-    u = urlparse.urlparse(uri_or_path)
-    return uri_or_path if u.scheme else path_to_file_uri(uri_or_path)
