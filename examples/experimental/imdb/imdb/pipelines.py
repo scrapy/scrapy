cf572bb6423691d9976b1113df1f174fc93ae5de
Pablo Hoffman <pablo@pablohoffman.com>
2011-04-28 18:07:23 -0300

    removed experimental examples

--- examples/experimental/imdb/imdb/pipelines.py
+++
@@ -1,8 +0,0 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: http://doc.scrapy.org/topics/item-pipeline.html
-
-class ImdbPipeline(object):
-    def process_item(self, item, spider):
-        return item
