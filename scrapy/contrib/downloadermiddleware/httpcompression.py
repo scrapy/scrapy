81fbe8c9a4b3940cd1cdf27ff0848a7ec5360512
Pablo Hoffman <pablo@pablohoffman.com>
2011-07-04 21:27:24 -0300

    added x-gzip to supported encoding declarations in httpcompression middleware

--- scrapy/contrib/downloadermiddleware/httpcompression.py
+++ scrapy/contrib/downloadermiddleware/httpcompression.py
@@ -10,7 +10,7 @@ class HttpCompressionMiddleware(object):
     sent/received from web sites"""
 
     def process_request(self, request, spider):
-        request.headers.setdefault('Accept-Encoding', 'gzip,deflate')
+        request.headers.setdefault('Accept-Encoding', 'x-gzip,gzip,deflate')
 
     def process_response(self, request, response, spider):
         if isinstance(response, Response):
@@ -32,7 +32,7 @@ class HttpCompressionMiddleware(object):
         return response
 
     def _decode(self, body, encoding):
-        if encoding == 'gzip':
+        if encoding == 'gzip' or encoding == 'x-gzip':
             body = gunzip(body)
 
         if encoding == 'deflate':
