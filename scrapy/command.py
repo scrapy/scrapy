46edfd4a9d47f765310555220e21c92f32d33a61
Pablo Hoffman <pablo@pablohoffman.com>
2011-09-01 14:29:11 -0300

    remove unneeded code to simplify

--- scrapy/command.py
+++ scrapy/command.py
@@ -64,20 +64,20 @@ class ScrapyCommand(object):
         Populate option parse with options available for this command
         """
         group = OptionGroup(parser, "Global Options")
-        group.add_option("--logfile", dest="logfile", metavar="FILE", \
+        group.add_option("--logfile", metavar="FILE", \
             help="log file. if omitted stderr will be used")
-        group.add_option("-L", "--loglevel", dest="loglevel", metavar="LEVEL", \
+        group.add_option("-L", "--loglevel", metavar="LEVEL", \
             default=None, \
             help="log level (default: %s)" % settings['LOGLEVEL'])
-        group.add_option("--nolog", action="store_true", dest="nolog", \
+        group.add_option("--nolog", action="store_true", \
             help="disable logging completely")
-        group.add_option("--profile", dest="profile", metavar="FILE", default=None, \
+        group.add_option("--profile", metavar="FILE", default=None, \
             help="write python cProfile stats to FILE")
-        group.add_option("--lsprof", dest="lsprof", metavar="FILE", default=None, \
+        group.add_option("--lsprof", metavar="FILE", default=None, \
             help="write lsprof profiling stats to FILE")
-        group.add_option("--pidfile", dest="pidfile", metavar="FILE", \
+        group.add_option("--pidfile", metavar="FILE", \
             help="write process ID to FILE")
-        group.add_option("-s", "--set", dest="set", action="append", default=[], metavar="NAME=VALUE", \
+        group.add_option("-s", "--set", action="append", default=[], metavar="NAME=VALUE", \
             help="set/override setting (may be repeated)")
         parser.add_option_group(group)
         
