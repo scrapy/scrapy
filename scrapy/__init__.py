db92bc8c40c5f56e965c9c7f165be82f9adee2e1
Pablo Hoffman <pablo@pablohoffman.com>
2012-01-02 13:07:15 -0200

    bumped version to 0.15.1, mainly to avoid package upgrade issues with new versioning based on git describe

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -2,8 +2,8 @@
 Scrapy - a screen scraping framework written in Python
 """
 
-version_info = (0, 15, 0)
-__version__ = "0.15.0"
+version_info = (0, 15, 1)
+__version__ = "0.15.1"
 
 import sys, os, warnings
 
