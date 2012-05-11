9584f5fa65cb5a4929bdcc3bfce6fa15b35118f2
dfdeshom <didier@cogtree.com>
2012-05-11 15:15:09 -0500

    allow to disable extension by setting its value to 0 or None. See https://github.com/scrapy/scrapy/issues/86#issuecomment-5392185 and subsequent comments

--- scrapy/utils/conf.py
+++ scrapy/utils/conf.py
@@ -13,7 +13,7 @@ def build_component_list(base, custom):
     compdict = base.copy()
     compdict.update(custom)
     return [k for k, v in sorted(compdict.items(), key=itemgetter(1)) \
-        if v is not None]
+        if v not in (0,None)]
 
 def arglist_to_dict(arglist):
     """Convert a list of arguments like ['arg1=val1', 'arg2=val2', ...] to a
