0a261700862c496dfabab8a5adc05d841bd95c05
Daniel Graña <dangra@gmail.com>
2012-05-15 11:27:04 -0300

    move ssl context factory to its own module and implement a non-ssl version that warns about pyopenssl support

--- scrapy/settings/default_settings.py
+++ scrapy/settings/default_settings.py
@@ -63,7 +63,7 @@ DOWNLOAD_TIMEOUT = 180      # 3mins
 DOWNLOADER_DEBUG = False
 
 DOWNLOADER_HTTPCLIENTFACTORY = 'scrapy.core.downloader.webclient.ScrapyHTTPClientFactory'
-DOWNLOADER_CLIENTCONTEXTFACTORY = 'scrapy.core.downloader.webclient.ScrapyClientContextFactory'
+DOWNLOADER_CLIENTCONTEXTFACTORY = 'scrapy.core.downloader.contextfactory.ScrapyClientContextFactory'
 
 DOWNLOADER_MIDDLEWARES = {}
 
