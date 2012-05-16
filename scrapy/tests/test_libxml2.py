7fc573a23086e83772ba3b1856529520626e6c2f
Daniel Graña <dangra@gmail.com>
2012-05-16 18:54:02 -0300

    fix libxml2 test

--- scrapy/tests/test_libxml2.py
+++ scrapy/tests/test_libxml2.py
@@ -11,8 +11,9 @@ class Libxml2Test(unittest.TestCase):
     @libxml2debug
     def test_libxml2_bug_2_6_27(self):
         # this test will fail in version 2.6.27 but passes on 2.6.29+
+        import libxml2
         html = "<td>1<b>2</b>3</td>"
-        node = self.libxml2.htmlParseDoc(html, 'utf-8')
+        node = libxml2.htmlParseDoc(html, 'utf-8')
         result = [str(r) for r in node.xpathEval('//text()')]
         self.assertEquals(result, ['1', '2', '3'])
         node.freeDoc()
