9d7106219a87519701e55641aa623a31a2ac021d
Pablo Hoffman <pablo@pablohoffman.com>
2012-10-12 17:24:25 -0200

    added deprecation warning when importing scrapy.conf module

--- scrapy/conf.py
+++ scrapy/conf.py
@@ -1,11 +1,10 @@
 # This module is kept for backwards compatibility, so users can import
 # scrapy.conf.settings and get the settings they expect
-#
-# TODO: Add deprecation warning once all scrapy.conf instances have been
-# removed from Scrapy codebase.
 
-import sys
+from scrapy.project import crawler
+settings = crawler.settings
 
-if 'scrapy.cmdline' not in sys.modules:
-    from scrapy.utils.project import get_project_settings
-    settings = get_project_settings()
+import warnings
+from scrapy.exceptions import ScrapyDeprecationWarning
+warnings.warn("Module `scrapy.conf` is deprecated, use `crawler.settings` attribute instead",
+    ScrapyDeprecationWarning, stacklevel=2)
