e8e5a62c20b3d217cc55038f30e495ca183bbba7
Pablo Hoffman <pablo@pablohoffman.com>
2012-10-25 11:40:09 -0200

    better backwards compatibility for scrapy.conf.settings
    Althought this backwards compatibility is more complex, it avoid modules
    from failing when importing scrapy.conf, if they are not run through
    "scrapy" command (such as when running tests on scrapy projects code).

--- scrapy/conf.py
+++ scrapy/conf.py
@@ -1,8 +1,11 @@
 # This module is kept for backwards compatibility, so users can import
 # scrapy.conf.settings and get the settings they expect
 
-from scrapy.project import crawler
-settings = crawler.settings
+import sys
+
+if 'scrapy.cmdline' not in sys.modules:
+    from scrapy.utils.project import get_project_settings
+    settings = get_project_settings()
 
 import warnings
 from scrapy.exceptions import ScrapyDeprecationWarning
