d5d944fa44f55159a11716c566d96f8c9dabb283
Pablo Hoffman <pablo@pablohoffman.com>
2013-02-28 11:35:11 -0200

    Log overriden Scrapy settings when Scrapy starts.
    This is useful for debugging, to quickly find out which settings where
    used for a specific spider run. dict settings are omitted for brevity.
    DEBUG level is used for consistency with "Enabled
    extensions/middlewares" lines (which share a similar purpose).

--- scrapy/settings/__init__.py
+++ scrapy/settings/__init__.py
@@ -57,3 +57,17 @@ class CrawlerSettings(Settings):
 
     def __str__(self):
         return "<CrawlerSettings module=%r>" % self.settings_module
+
+
+def iter_default_settings():
+    """Return the default settings as an iterator of (name, value) tuples"""
+    for name in dir(default_settings):
+        if name.isupper():
+            yield name, getattr(default_settings, name)
+
+def overridden_settings(settings):
+    """Return a dict of the settings that have been overridden"""
+    for name, defvalue in iter_default_settings():
+        value = settings[name]
+        if not isinstance(defvalue, dict) and value != defvalue:
+            yield name, value
