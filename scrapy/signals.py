b972c90bd64f8044a9423d44bbccd9bc64157c65
Jordi Llonch <llonchj@gmail.com>
2013-03-03 05:37:11 +1100

    Signal class. Allow receiver identify dispatched signal

--- scrapy/signals.py
+++ scrapy/signals.py
@@ -5,17 +5,21 @@ These signals are documented in docs/topics/signals.rst. Please don't add new
 signals here without documenting them there.
 """
 
-engine_started = object()
-engine_stopped = object()
-spider_opened = object()
-spider_idle = object()
-spider_closed = object()
-spider_error = object()
-request_received = object()
-response_received = object()
-response_downloaded = object()
-item_scraped = object()
-item_dropped = object()
+class Signal(object):
+    def __init__(self, name):
+        self.name = name
+
+engine_started = Signal("engine_started")
+engine_stopped = Signal("engine_stopped")
+spider_opened = Signal("spider_opened")
+spider_idle = Signal("spider_idle")
+spider_closed = Signal("spider_closed")
+spider_error = Signal("spider_error")
+request_received = Signal("request_received")
+response_received = Signal("response_received")
+response_downloaded = Signal("response_downloaded")
+item_scraped = Signal("item_scraped")
+item_dropped = Signal("item_dropped")
 
 stats_spider_opened = spider_opened
 stats_spider_closing = spider_closed
