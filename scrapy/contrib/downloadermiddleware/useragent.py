2e6a14477bb65fef9ea6c852191e08f599e5529b
nramirezuy <nramirez.uy@gmail.com>
2013-10-08 18:18:49 -0200

    useragent mid multi spider support removed

--- scrapy/contrib/downloadermiddleware/useragent.py
+++ scrapy/contrib/downloadermiddleware/useragent.py
@@ -1,25 +1,23 @@
 """Set User-Agent header per spider or use a default value from settings"""
 
-from scrapy.utils.python import WeakKeyCache
+from scrapy import signals
 
 
 class UserAgentMiddleware(object):
     """This middleware allows spiders to override the user_agent"""
 
     def __init__(self, user_agent='Scrapy'):
-        self.cache = WeakKeyCache(self._user_agent)
         self.user_agent = user_agent
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(crawler.settings['USER_AGENT'])
+        o = cls(crawler.settings['USER_AGENT'])
+        crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        return o
 
-    def _user_agent(self, spider):
-        if hasattr(spider, 'user_agent'):
-            return spider.user_agent
-        return self.user_agent
+    def spider_opened(self, spider):
+        self.user_agent = getattr(spider, 'user_agent', self.user_agent)
 
     def process_request(self, request, spider):
-        ua = self.cache[spider]
-        if ua:
-            request.headers.setdefault('User-Agent', ua)
+        if self.user_agent:
+            request.headers.setdefault('User-Agent', self.user_agent)
