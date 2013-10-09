da8c489be09e4c91813de49c61da14e7171908d7
nramirezuy <nramirez.uy@gmail.com>
2013-10-09 13:50:15 -0200

    defaultheaders multi spider support removed

--- scrapy/contrib/downloadermiddleware/defaultheaders.py
+++ scrapy/contrib/downloadermiddleware/defaultheaders.py
@@ -3,22 +3,17 @@ DefaultHeaders downloader middleware
 
 See documentation in docs/topics/downloader-middleware.rst
 """
-from scrapy.utils.python import WeakKeyCache
 
 
 class DefaultHeadersMiddleware(object):
 
-    def __init__(self, settings):
-        self._headers = WeakKeyCache(self._default_headers)
-        self._settings = settings
+    def __init__(self, headers):
+        self._headers = headers
 
     @classmethod
     def from_crawler(cls, crawler):
-        return cls(crawler.settings)
-
-    def _default_headers(self, spider):
-        return self._settings.get('DEFAULT_REQUEST_HEADERS').items()
+        return cls(crawler.settings.get('DEFAULT_REQUEST_HEADERS').items())
 
     def process_request(self, request, spider):
-        for k, v in self._headers[spider]:
+        for k, v in self._headers:
             request.headers.setdefault(k, v)
