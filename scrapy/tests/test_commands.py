2839483e0c116ce78bec2eda965c061993be2f9d
Nuno Maximiano <nuno.maximiano@leiribits.com>
2013-10-21 10:56:19 +0100

    add project name validation test

--- scrapy/tests/test_commands.py
+++ scrapy/tests/test_commands.py
@@ -63,6 +63,7 @@ class StartprojectTest(ProjectTest):
 
         self.assertEqual(1, self.call('startproject', self.project_name))
         self.assertEqual(1, self.call('startproject', 'wrong---project---name'))
+        self.assertEqual(1, self.call('startproject', 'sys'))
 
 
 class CommandTest(ProjectTest):
