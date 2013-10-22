67468e2a92c9299ddf6db7dec0b50f27108e1ad1
Nuno Maximiano <nuno.maximiano@leiribits.com>
2013-10-22 16:45:20 +0100

    add post option to fetch command

--- scrapy/commands/fetch.py
+++ scrapy/commands/fetch.py
@@ -5,6 +5,7 @@ from scrapy.http import Request
 from scrapy.spider import BaseSpider
 from scrapy.exceptions import UsageError
 from scrapy.utils.spider import create_spider_for_request
+from scrapy import settings
 
 class Command(ScrapyCommand):
 
@@ -26,6 +27,7 @@ class Command(ScrapyCommand):
             help="use this spider")
         parser.add_option("--headers", dest="headers", action="store_true", \
             help="print response HTTP headers instead of body")
+        parser.add_option("--post", dest="post", help="make a post request")
 
     def _print_headers(self, headers, prefix):
         for key, values in headers.items():
@@ -44,7 +46,15 @@ class Command(ScrapyCommand):
         if len(args) != 1 or not is_url(args[0]):
             raise UsageError()
         cb = lambda x: self._print_response(x, opts)
-        request = Request(args[0], callback=cb, dont_filter=True)
+        method_type = "Get"
+        header = settings.default_settings.DEFAULT_REQUEST_HEADERS
+        if opts.post:
+            #set post content Type
+            header['Content-Type'] = "application/x-www-form-urlencoded"
+            #set request type
+            method_type = "Post"
+        request = Request(args[0], headers=header, method=method_type, 
+                          body=opts.post, callback=cb, dont_filter=True)
         request.meta['handle_httpstatus_all'] = True
 
         crawler = self.crawler_process.create_crawler()
