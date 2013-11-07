d29791d7ab1d083be50ff48087b940eb3e082e37
Daniel Graña <dangra@gmail.com>
2013-11-07 09:54:04 -0200

    building Pillow with pypy requries dev headers

--- .travis-workarounds.sh
+++ .travis-workarounds.sh
@@ -5,7 +5,7 @@ set -x
 if [[ "${TOXENV}" == "pypy" ]]; then
     sudo add-apt-repository -y ppa:pypy/ppa
     sudo apt-get -qy update
-    sudo apt-get install -y pypy
+    sudo apt-get install -y pypy pypy-dev
     # This is required because we need to get rid of the Travis installed PyPy
     # or it'll take precedence over the PPA installed one.
     sudo rm -rf /usr/local/pypy/bin
