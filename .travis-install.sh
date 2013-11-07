ecfa743105120538d6bab498b2651b3cc8c31633
Daniel Graña <dangra@gmail.com>
2013-11-07 04:26:25 -0200

    install updated pypy from ppa

---
+++ .travis-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+set -x
+
+if [[ "${TOX_ENV}" == "pypy" ]]; then
+    sudo add-apt-repository -y ppa:pypy/ppa
+    sudo apt-get -qy update
+    sudo apt-get install -y pypy
+    # This is required because we need to get rid of the Travis installed PyPy
+    # or it'll take precedence over the PPA installed one.
+    sudo rm -rf /usr/local/pypy/bin
+fi
+
+pip install tox coveralls
