2dfeff7eca2e1482638595c48e04506ff002e3f7
Max Arnold <arnold.maxim@gmail.com>
2013-12-18 22:46:27 +0700

    rename ImagesPipelineTestCase image_path var to file_path for clarity

--- scrapy/tests/test_pipeline_images.py
+++ scrapy/tests/test_pipeline_images.py
@@ -39,19 +39,19 @@ class ImagesPipelineTestCase(unittest.TestCase):
     def tearDown(self):
         rmtree(self.tempdir)
 
-    def test_image_path(self):
-        image_path = self.pipeline.file_path
-        self.assertEqual(image_path(Request("https://dev.mydeco.com/mydeco.gif")),
+    def test_file_path(self):
+        file_path = self.pipeline.file_path
+        self.assertEqual(file_path(Request("https://dev.mydeco.com/mydeco.gif")),
                          'full/3fd165099d8e71b8a48b2683946e64dbfad8b52d.jpg')
-        self.assertEqual(image_path(Request("http://www.maddiebrown.co.uk///catalogue-items//image_54642_12175_95307.jpg")),
+        self.assertEqual(file_path(Request("http://www.maddiebrown.co.uk///catalogue-items//image_54642_12175_95307.jpg")),
                          'full/0ffcd85d563bca45e2f90becd0ca737bc58a00b2.jpg')
-        self.assertEqual(image_path(Request("https://dev.mydeco.com/two/dirs/with%20spaces%2Bsigns.gif")),
+        self.assertEqual(file_path(Request("https://dev.mydeco.com/two/dirs/with%20spaces%2Bsigns.gif")),
                          'full/b250e3a74fff2e4703e310048a5b13eba79379d2.jpg')
-        self.assertEqual(image_path(Request("http://www.dfsonline.co.uk/get_prod_image.php?img=status_0907_mdm.jpg")),
+        self.assertEqual(file_path(Request("http://www.dfsonline.co.uk/get_prod_image.php?img=status_0907_mdm.jpg")),
                          'full/4507be485f38b0da8a0be9eb2e1dfab8a19223f2.jpg')
-        self.assertEqual(image_path(Request("http://www.dorma.co.uk/images/product_details/2532/")),
+        self.assertEqual(file_path(Request("http://www.dorma.co.uk/images/product_details/2532/")),
                          'full/97ee6f8a46cbbb418ea91502fd24176865cf39b2.jpg')
-        self.assertEqual(image_path(Request("http://www.dorma.co.uk/images/product_details/2532")),
+        self.assertEqual(file_path(Request("http://www.dorma.co.uk/images/product_details/2532")),
                          'full/244e0dd7d96a3b7b01f54eded250c9e272577aa1.jpg')
 
     def test_thumbnail_name(self):
