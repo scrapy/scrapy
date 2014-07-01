137f80b8dea057604ec7458bdd5ae43d1051b98c
kyzi007 <kyzi007@rambler.ru>
2014-07-01 11:49:39 +0300

    add signal name

--- scrapy/signals.py
+++ scrapy/signals.py
@@ -5,17 +5,24 @@ These signals are documented in docs/topics/signals.rst. Please don't add new
 signals here without documenting them there.
 """
 
-engine_started = object()
-engine_stopped = object()
-spider_opened = object()
-spider_idle = object()
-spider_closed = object()
-spider_error = object()
-request_scheduled = object()
-response_received = object()
-response_downloaded = object()
-item_scraped = object()
-item_dropped = object()
+
+class Signal(object):
+    def __init__(self, name):
+        super(Signal, self).__init__()
+        self.name = name
+
+
+engine_started = Signal('engine_started')
+engine_stopped = Signal('engine_stopped')
+spider_opened = Signal('spider_opened')
+spider_idle = Signal('spider_idle')
+spider_closed = Signal('spider_closed')
+spider_error = Signal('spider_error')
+request_scheduled = Signal('request_scheduled')
+response_received = Signal('response_received')
+response_downloaded = Signal('response_downloaded')
+item_scraped = Signal('item_scraped')
+item_dropped = Signal('item_dropped')
 
 # for backwards compatibility
 stats_spider_opened = spider_opened
