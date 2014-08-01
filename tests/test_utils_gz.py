c564334d00fab76ecaadd15d7944bec6d59027ba
Daniel Graña <dangra@gmail.com>
2014-08-01 02:25:33 -0300

    PY3: port scrapy/utils/gz.py

--- tests/test_utils_gz.py
+++ tests/test_utils_gz.py
@@ -1,10 +1,11 @@
 import unittest
 from os.path import join
 
-from tests import tests_datadir
 from scrapy.utils.gz import gunzip
+from tests import tests_datadir
 
 SAMPLEDIR = join(tests_datadir, 'compressed')
+
 
 class GzTest(unittest.TestCase):
 
@@ -16,7 +17,7 @@ class GzTest(unittest.TestCase):
     def test_gunzip_truncated(self):
         with open(join(SAMPLEDIR, 'truncated-crc-error.gz'), 'rb') as f:
             text = gunzip(f.read())
-            assert text.endswith('</html')
+            assert text.endswith(b'</html')
 
     def test_gunzip_no_gzip_file_raises(self):
         with open(join(SAMPLEDIR, 'feed-sample1.xml'), 'rb') as f:
@@ -25,4 +26,4 @@ class GzTest(unittest.TestCase):
     def test_gunzip_truncated_short(self):
         with open(join(SAMPLEDIR, 'truncated-crc-error-short.gz'), 'rb') as f:
             text = gunzip(f.read())
-            assert text.endswith('</html>')
+            assert text.endswith(b'</html>')
