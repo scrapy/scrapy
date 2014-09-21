95100deb810b1ff2b1ca8a789e1d9c3590cd16be
Mikhail Korobov <kmike84@gmail.com>
2014-09-21 08:24:32 +0600

    DOC enable autodoc extension; prefer Scrapy from the checkout when building docs; corrected path to scrapy folder.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ from os import path
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 sys.path.append(path.join(path.dirname(__file__), "_ext"))
-sys.path.append(path.join(path.dirname(path.dirname(__file__)), "scrapy"))
+sys.path.insert(0, path.join(path.dirname(path.dirname(__file__))))
 
 
 # General configuration
@@ -26,7 +26,10 @@ sys.path.append(path.join(path.dirname(path.dirname(__file__)), "scrapy"))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['scrapydocs']
+extensions = [
+    'scrapydocs',
+    'sphinx.ext.autodoc',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
