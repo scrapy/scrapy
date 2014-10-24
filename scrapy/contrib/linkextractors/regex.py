5cbafe222dc52e2d470f0f82d84c9fdb40a9b8dd
Andrew Scorpil <webdev@scorpil.com>
2014-10-24 15:58:12 +0300

    Ignore bogus links

--- scrapy/contrib/linkextractors/regex.py
+++ scrapy/contrib/linkextractors/regex.py
@@ -10,21 +10,22 @@ linkre = re.compile(
         "<a\s.*?href=(\"[.#]+?\"|\'[.#]+?\'|[^\s]+?)(>|\s.*?>)(.*?)<[/ ]?a>",
         re.DOTALL | re.IGNORECASE)
 
-def clean_link(link_text):
-    """Remove leading and trailing whitespace and punctuation"""
-    return link_text.strip("\t\r\n '\"")
-
 class RegexLinkExtractor(SgmlLinkExtractor):
     """High performant link extractor"""
 
     def _extract_links(self, response_text, response_url, response_encoding, base_url=None):
         if base_url is None:
             base_url = urljoin(response_url, self.base_url) if self.base_url else response_url
-
-        clean_url = lambda u: urljoin(base_url, replace_entities(clean_link(u.decode(response_encoding))))
-        clean_text = lambda t: replace_escape_chars(remove_tags(t.decode(response_encoding))).strip()
-
         links_text = linkre.findall(response_text)
-        return [Link(clean_url(url).encode(response_encoding),
-                     clean_text(text))
-                for url, _, text in links_text]
+
+        ret = []
+        for url, _, text in links_text:
+            clean_link = url.decode(response_encoding).strip("\t\r\n '\"")
+            try:
+                clean_url = urljoin(base_url, replace_entities(clean_link))
+            except ValueError:
+                continue
+            clean_text = replace_escape_chars(remove_tags(text.decode(response_encoding))).strip()
+            ret.append(Link(clean_url.encode(response_encoding), clean_text))
+
+        return ret
