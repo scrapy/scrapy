633d9148fa51baeacee53483691ce86940bac8b1
Matt Fullerton <matt.fullerton@gmail.com>
2014-11-13 12:12:12 +0100

    Use only reppy parsing and not reppy cache

--- scrapy/contrib/downloadermiddleware/robotstxt.py
+++ scrapy/contrib/downloadermiddleware/robotstxt.py
@@ -4,7 +4,10 @@ enable this middleware and enable the ROBOTSTXT_OBEY setting.
 
 """
 
-from reppy.cache import RobotsCache
+import time
+
+from reppy.parser import Rules
+from reppy import Utility
 
 from scrapy import signals, log
 from scrapy.exceptions import NotConfigured, IgnoreRequest
@@ -14,6 +17,8 @@ from scrapy.utils.httpobj import urlparse_cached
 
 class RobotsTxtMiddleware(object):
     DOWNLOAD_PRIORITY = 1000
+    default_ttl = 3600
+    min_ttl = 60
 
     def __init__(self, crawler):
         if not crawler.settings.getbool('ROBOTSTXT_OBEY'):
@@ -22,15 +27,38 @@ class RobotsTxtMiddleware(object):
         self.crawler = crawler
         self._useragent = crawler.settings.get('USER_AGENT')
         self._parsers = {}
-        self._robots = RobotsCache()
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler)
 
     def process_request(self, request, spider):
-        if self._robots and not self._robots.allowed(request.url, self._useragent):
+        if request.meta.get('dont_obey_robotstxt'):
+            return
+        rp = self.robot_parser(request, spider)
+        if rp and not rp.allowed(request.url, self._useragent):
             log.msg(format="Forbidden by robots.txt: %(request)s",
                     level=log.DEBUG, request=request)
             raise IgnoreRequest
 
+    def robot_parser(self, request, spider):
+        url = urlparse_cached(request)
+        netloc = url.netloc
+        if netloc not in self._parsers:
+            self._parsers[netloc] = None
+            robotsurl = "%s://%s/robots.txt" % (url.scheme, url.netloc)
+            robotsreq = Request(
+                robotsurl,
+                priority=self.DOWNLOAD_PRIORITY,
+                meta={'dont_obey_robotstxt': True}
+            )
+            dfd = self.crawler.engine.download(robotsreq, spider)
+            dfd.addCallback(self._parse_robots)
+        return self._parsers[netloc]
+
+    def _parse_robots(self, response):
+        #A lot of work to provide the expire time which we don't actually use
+        ttl = max(self.min_ttl, Utility.get_ttl(response.headers, self.default_ttl))
+        rp = Rules(response.url, response.status, response.body, time.time() + ttl)
+        rp.parse(response.body)
+        self._parsers[urlparse_cached(response).netloc] = rp
