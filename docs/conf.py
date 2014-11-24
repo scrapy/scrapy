4353079c17336f09bc9467ec39cf86e451ea5fa6
Jouke Waleson <jouke.waleson@mendix.com>
2014-11-24 09:03:08 +0100

    undo changes to docs/conf.py for commented out lines

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-#  source_encoding = 'utf-8'
+#source_encoding = 'utf-8'
 
 # The master toctree document.
 master_doc = 'index'
@@ -63,30 +63,30 @@ language = 'en'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
-#  today = ''
+#today = ''
 # Else, today_fmt is used as the format for a strftime call.
-#  today_fmt = '%B %d, %Y'
+#today_fmt = '%B %d, %Y'
 
 # List of documents that shouldn't be included in the build.
-#  unused_docs = []
+#unused_docs = []
 
 # List of directories, relative to source directory, that shouldn't be searched
 # for source files.
 exclude_trees = ['.build']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#  default_role = None
+#default_role = None
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
-#  add_function_parentheses = True
+#add_function_parentheses = True
 
 # If true, the current module name will be prepended to all description
 # unit titles (such as .. function::).
-#  add_module_names = True
+#add_module_names = True
 
 # If true, sectionauthor and moduleauthor directives will be shown in the
 # output. They are ignored by default.
-#  show_authors = False
+#show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
@@ -102,19 +102,19 @@ html_style = 'scrapydoc.css'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#  html_title = None
+#html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#  html_short_title = None
+#html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#  html_logo = None
+#html_logo = None
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#  html_favicon = None
+#html_favicon = None
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -130,20 +130,20 @@ html_last_updated_fmt = '%b %d, %Y'
 html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#  html_sidebars = {}
+#html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
-#  html_additional_pages = {}
+#html_additional_pages = {}
 
 # If false, no module index is generated.
-#  html_use_modindex = True
+#html_use_modindex = True
 
 # If false, no index is generated.
-#  html_use_index = True
+#html_use_index = True
 
 # If true, the index is split into individual pages for each letter.
-#  html_split_index = False
+#html_split_index = False
 
 # If true, the reST sources are included in the HTML build as _sources/<name>.
 html_copy_source = True
@@ -151,10 +151,10 @@ html_copy_source = True
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
 # base URL from which the finished HTML is served.
-#  html_use_opensearch = ''
+#html_use_opensearch = ''
 
 # If nonempty, this is the file name suffix for HTML files (e.g. ".xhtml").
-#  html_file_suffix = ''
+#html_file_suffix = ''
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Scrapydoc'
@@ -164,10 +164,10 @@ htmlhelp_basename = 'Scrapydoc'
 # ------------------------
 
 # The paper size ('letter' or 'a4').
-#  latex_paper_size = 'letter'
+#latex_paper_size = 'letter'
 
 # The font size ('10pt', '11pt' or '12pt').
-#  latex_font_size = '10pt'
+#latex_font_size = '10pt'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
@@ -178,20 +178,20 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#  latex_logo = None
+#latex_logo = None
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#  latex_use_parts = False
+#latex_use_parts = False
 
 # Additional stuff for the LaTeX preamble.
-#  latex_preamble = ''
+#latex_preamble = ''
 
 # Documents to append as an appendix to all manuals.
-#  latex_appendices = []
+#latex_appendices = []
 
 # If false, no module index is generated.
-#  latex_use_modindex = True
+#latex_use_modindex = True
 
 
 # Options for the linkcheck builder
