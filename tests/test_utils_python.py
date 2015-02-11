f53a7417a60a266dad16e6de9029eee0357ebea6
klangner@gmail.com <klangner@gmail.com>
2015-02-11 18:57:05 +0100

    PY3: Fixed unit tests for test_utils_python.py

--- tests/test_utils_python.py
+++ tests/test_utils_python.py
@@ -12,6 +12,22 @@ from scrapy.utils.python import str_to_unicode, unicode_to_str, \
 __doctests__ = ['scrapy.utils.python']
 
 class UtilsPythonTestCase(unittest.TestCase):
+    def test_str_to_unicode(self):
+        # converting an utf-8 encoded string to unicode
+        self.assertEqual(str_to_unicode(b'lel\xc3\xb1e'), u'lel\xf1e')
+
+        # converting a latin-1 encoded string to unicode
+        self.assertEqual(str_to_unicode(b'lel\xf1e', 'latin-1'), u'lel\xf1e')
+
+        # converting a unicode to unicode should return the same object
+        self.assertEqual(str_to_unicode(u'\xf1e\xf1e\xf1e'), u'\xf1e\xf1e\xf1e')
+
+        # converting a strange object should raise TypeError
+        self.assertRaises(TypeError, str_to_unicode, 423)
+
+        # check errors argument works
+        assert u'\ufffd' in str_to_unicode(b'a\xedb', 'utf-8', errors='replace')
+
     def test_unicode_to_str(self):
         # converting a unicode object to an utf-8 encoded string
         self.assertEqual(unicode_to_str(u'\xa3 49'), b'\xc2\xa3 49')
