a302afe42061b14b3374930d9a5cd40e6ca29ca1
nyov <nyov@nexnode.net>
2015-03-24 16:54:49 +0000

    IPv6ThreadedResolver based on socket.getaddrinfo

--- scrapy/resolver.py
+++ scrapy/resolver.py
@@ -1,13 +1,57 @@
-from twisted.internet import defer
+import socket
+from twisted.python import failure
+from twisted.internet import defer, threads
+from twisted.internet.error import DNSLookupError
 from twisted.internet.base import ThreadedResolver
 
 from scrapy.utils.datatypes import LocalCache
+
+
+class IPv6ThreadedResolver(ThreadedResolver):
+    """ThreadedResolver calling socket.getaddrinfo for IPv6 capability.
+
+    Uses a threadpool to perform name lookups without blocking the
+    reactor thread. The underlying socket.getaddrinfo is a blocking call.
+    """
+
+    def _success(self, result, name):
+        # can only handle one result. pick the first one
+        # (we could sort results by family type to prefer either IPv4 or IPv6)
+        (family, socktype, proto, canonname, sockaddr) = result[0]
+        if family == socket.AF_INET:
+            (address, port) = sockaddr
+            return address
+        if family == socket.AF_INET6:
+            (address, port, flow_info, scope_id) = sockaddr
+            return address
+        msg = "address %r not found (invalid AF type returned)" % (name,)
+        err = DNSLookupError(msg)
+        return failure.Failure(err)
+
+    def getHostByName(self, name, timeout = (1, 3, 11, 45)):
+        if timeout:
+            timeoutDelay = sum(timeout)
+        else:
+            timeoutDelay = 60
+        userDeferred = defer.Deferred()
+        lookupDeferred = threads.deferToThreadPool(
+            self.reactor, self.reactor.getThreadPool(),
+            socket.getaddrinfo, name, None, 0, 0, socket.IPPROTO_TCP, 0)
+        cancelCall = self.reactor.callLater(
+            timeoutDelay, self._cleanup, name, lookupDeferred)
+        self._runningQueries[lookupDeferred] = (userDeferred, cancelCall)
+        lookupDeferred.addBoth(self._checkTimeout, name, lookupDeferred)
+
+        userDeferred.addCallback(self._success, name)
+        return userDeferred
+
 
 # TODO: cache misses
 
 dnscache = LocalCache(10000)
 
-class CachingThreadedResolver(ThreadedResolver):
+class CachingThreadedResolver(IPv6ThreadedResolver):
+
     def __init__(self, reactor, cache_size, timeout):
         super(CachingThreadedResolver, self).__init__(reactor)
         dnscache.limit = cache_size
---
+++ tests/test_resolver.py
@@ -0,0 +1,13 @@
+from twisted.internet.test.test_base import ThreadedResolverTests
+
+class IPv6ThreadedResolverTest(ThreadedResolverTests):
+    """
+    Tests for ThreadedResolver imported from twisted,
+    as we should support the same interface.
+    """
+
+class CachingThreadedResolverTest(ThreadedResolverTests):
+    """
+    Tests for ThreadedResolver imported from twisted,
+    as we should support the same interface.
+    """
