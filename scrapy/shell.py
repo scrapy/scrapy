c4f5cc93406ef11bbf823f2e8d91ea3f19e7d8b0
nyov <nyov@nexnode.net>
2015-03-25 10:24:07 +0000

    Add support for bpython console.
    Adds support for configuration of shells from scrapy.cfg
    and SCRAPY_PYTHON_SHELL.
    
    config snippet:
    
    cat <<EOF >> ~/.scrapy.cfg
    [settings]
    # shell can be one of ipython, bpython or python;
    # to be tried as the interactive python console
    # (in above order, unless set here).
    shell = python
    EOF
    
    (closes #270, #1100, #1301)

--- scrapy/shell.py
+++ scrapy/shell.py
@@ -5,6 +5,7 @@ See documentation in docs/topics/shell.rst
 """
 from __future__ import print_function
 
+import os
 import signal
 import warnings
 
@@ -21,6 +22,8 @@ from scrapy.spiders import Spider
 from scrapy.utils.console import start_python_console
 from scrapy.utils.misc import load_object
 from scrapy.utils.response import open_in_browser
+from scrapy.utils.conf import get_config
+from scrapy.utils.console import DEFAULT_PYTHON_SHELLS
 
 
 class Shell(object):
@@ -52,7 +55,28 @@ class Shell(object):
         if self.code:
             print(eval(self.code, globals(), self.vars))
         else:
-            start_python_console(self.vars)
+            """
+            Detect interactive shell setting in scrapy.cfg
+            e.g.: ~/.config/scrapy.cfg or ~/.scrapy.cfg
+            [settings]
+            # shell can be one of ipython, bpython or python;
+            # to be used as the interactive python console, if available.
+            # (default is ipython, fallbacks in the order listed above)
+            shell = python
+            """
+            cfg = get_config()
+            section, option = 'settings', 'shell'
+            env = os.environ.get('SCRAPY_PYTHON_SHELL')
+            shells = []
+            if env:
+                shells += env.strip().lower().split(',')
+            elif cfg.has_option(section, option):
+                shells += [cfg.get(section, option).strip().lower()]
+            else: # try all by default
+                shells += DEFAULT_PYTHON_SHELLS.keys()
+            # always add standard shell as fallback
+            shells += ['python']
+            start_python_console(self.vars, shells=shells)
 
     def _schedule(self, request, spider):
         spider = self._open_spider(request, spider)
