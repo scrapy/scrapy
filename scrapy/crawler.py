ed95ea88ffa8841e6dde4428bf2e361947558c8e
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-03-30 16:40:55 +0200

    Dns cache size and timeout options.

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -152,6 +152,8 @@ class CrawlerProcess(CrawlerRunner):
         cache_size = self.settings.getint('DNSCACHE_SIZE') if self.settings.getbool('DNSCACHE_ENABLED') else 0
         reactor.installResolver(CachingThreadedResolver(reactor, cache_size,
                                                             self.settings.getfloat('DNS_TIMEOUT')))
+        tp = reactor.getThreadPool()
+        tp.adjustPoolsize(maxthreads=self.settings.getint('REACTOR_THREADPOOL_MAXSIZE'))
         reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
         reactor.run(installSignalHandlers=False)  # blocking call
 
