9836dd1c226bd686d601f5f122871e737780e97b
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-03-30 16:40:55 +0200

    Enabling, size and timeout options in CachedThreadedResolver.

--- scrapy/resolver.py
+++ scrapy/resolver.py
@@ -4,17 +4,25 @@ from twisted.internet.base import ThreadedResolver
 from scrapy.utils.datatypes import LocalCache
 
 # TODO: cache misses
-# TODO: make cache size a setting
 
 dnscache = LocalCache(10000)
 
 class CachingThreadedResolver(ThreadedResolver):
+    def __init__(self, reactor, settings):
+        super(CachingThreadedResolver, self).__init__(reactor)
+        self.caching_enabled = settings.getbool('DNSCACHE_ENABLED')
+        if self.caching_enabled:
+            dnscache.limit = settings.getint('DNSCACHE_SIZE')
+        self.timeout = settings.getint('DNS_TIMEOUT')
 
-    def getHostByName(self, name, timeout = (1, 3, 11, 45)):
-        if name in dnscache:
+    def getHostByName(self, name, timeout=None):
+        if self.caching_enabled and name in dnscache:
             return defer.succeed(dnscache[name])
+        if not timeout:
+            timeout = self.timeout
         d = super(CachingThreadedResolver, self).getHostByName(name, timeout)
-        d.addCallback(self._cache_result, name)
+        if self.caching_enabled:
+            d.addCallback(self._cache_result, name)
         return d
 
     def _cache_result(self, result, name):
