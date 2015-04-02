8f52767c0425671609073857463b919848103d4f
Alexander Sibiryakov <sixty-one@yandex.ru>
2015-04-02 15:25:17 +0200

    getting float from setting instead of int.

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -151,7 +151,7 @@ class CrawlerProcess(CrawlerRunner):
 
         cache_size = self.settings.getint('DNSCACHE_SIZE') if self.settings.getbool('DNSCACHE_ENABLED') else 0
         reactor.installResolver(CachingThreadedResolver(reactor, cache_size,
-                                                            self.settings.getint('DNS_TIMEOUT')))
+                                                            self.settings.getfloat('DNS_TIMEOUT')))
         reactor.addSystemEventTrigger('before', 'shutdown', self.stop)
         reactor.run(installSignalHandlers=False)  # blocking call
 
