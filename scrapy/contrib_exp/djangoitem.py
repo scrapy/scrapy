6d48c19acd2eb9d5e9e7e8808d349b14c290bda4
nyov <nyov@nexnode.net>
2015-04-03 13:02:56 +0000

    dissolve `scrapy.contrib_exp`

--- scrapy/contrib_exp/djangoitem.py
+++
@@ -1,6 +0,0 @@
-import warnings
-from scrapy.exceptions import ScrapyDeprecationWarning
-warnings.warn("Module `scrapy.contrib_exp.djangoitem` is deprecated, use `scrapy.contrib.djangoitem` instead",
-    ScrapyDeprecationWarning, stacklevel=2)
-
-from scrapy.contrib.djangoitem import DjangoItem
