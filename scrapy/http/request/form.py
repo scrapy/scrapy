1eccd34a7032c3e50c56f61da34ef39d1aaf43ec
mrpandav <pandav.dharmesh@gmail.com>
2015-04-12 11:11:28 +0530

    adding feature improvement  for selecting form using form-id, in addition to formname , formnumer before we go for xpath.. making it more idiomatic in nature

--- scrapy/http/request/form.py
+++ scrapy/http/request/form.py
@@ -54,7 +54,7 @@ def _urlencode(seq, enc):
     return urlencode(values, doseq=1)
 
 
-def _get_form(response, formname, formnumber, formxpath):
+def _get_form(response, formname, formid, formnumber, formxpath):
     """Find the form element """
     from scrapy.selector.lxmldocument import LxmlDocument
     root = LxmlDocument(response, lxml.html.HTMLParser)
