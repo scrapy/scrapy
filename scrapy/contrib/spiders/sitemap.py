13297f6e426d4331f7fadd99bf389d135984897a
Alexandr N Zamaraev (aka tonal) <tonal@promsoft.ru>
2015-04-21 14:36:56 +0600

    incorrect var name from copy/paste

--- scrapy/contrib/spiders/sitemap.py
+++ scrapy/contrib/spiders/sitemap.py
@@ -28,7 +28,7 @@ class SitemapSpider(Spider):
     def _parse_sitemap(self, response):
         if response.url.endswith('/robots.txt'):
             for url in sitemap_urls_from_robots(response.body):
-                if any(x.search(loc) for x in self._follow):
+                if any(x.search(url) for x in self._follow):
                     yield Request(url, callback=self._parse_sitemap)
         else:
             body = self._get_sitemap_body(response)
