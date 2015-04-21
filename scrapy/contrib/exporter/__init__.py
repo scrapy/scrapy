152594ce990e0f5923d87f968e354c1a17bf463d
Julia Medina <who.knows.shia@gmail.com>
2015-04-20 22:46:23 -0300

    scrapy/contrib/exporter shims

---
+++ scrapy/contrib/exporter/__init__.py
@@ -0,0 +1,7 @@
+import warnings
+from scrapy.exceptions import ScrapyDeprecationWarning
+warnings.warn("Module `scrapy.contrib.exporter` is deprecated, "
+              "use `scrapy.exporters` instead",
+              ScrapyDeprecationWarning, stacklevel=2)
+
+from scrapy.exporters import *
