4f56c6c79bd29dbd2e139533179b2039d7054aa4
nyov <nyov@nexnode.net>
2015-07-11 11:27:33 +0000

    Unset environment proxies for tests
    because urllib doesn't handle $no_proxy correctly
    and the unittest webserver is always local.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,6 +6,13 @@ see http://doc.scrapy.org/en/latest/contributing.html#running-tests
 
 import os
 
+# ignore system-wide proxies for tests
+# which would send requests to a totally unsuspecting server
+# (e.g. because urllib does not fully understand the proxy spec)
+os.environ['http_proxy'] = ''
+os.environ['https_proxy'] = ''
+os.environ['ftp_proxy'] = ''
+
 try:
     import unittest.mock as mock
 except ImportError:
