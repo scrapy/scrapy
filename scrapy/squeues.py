8538c93fbb5f815983354f52fed630db23187814
Daniel Graña <dangra@gmail.com>
2015-08-06 01:58:44 -0300

    Code different implementations per python version

--- scrapy/squeues.py
+++ scrapy/squeues.py
@@ -2,10 +2,13 @@
 Scheduler queues
 """
 
+import types
 import marshal
-from six.moves import cPickle as pickle
 
+import six
+from six.moves import cPickle as pickle
 from queuelib import queue
+
 
 def _serializable_queue(queue_class, serialize, deserialize):
 
@@ -22,16 +25,48 @@ def _serializable_queue(queue_class, serialize, deserialize):
 
     return SerializableQueue
 
-def _pickle_serialize(obj):
+
+def _sane_pickle_serialize(obj):
     try:
         return pickle.dumps(obj, protocol=2)
     except pickle.PicklingError as e:
         raise ValueError(str(e))
-    # Workaround for Twisted bug #7989 present since Twisted 15.3.0
+
+
+# Workaround for Python2.7 + Twisted 15.3.0 bug, see #7989.
+def _py27_pickle_serialize(obj):
+    try:
+        return pickle.dumps(obj, protocol=2)
+    except pickle.PicklingError as e:
+        raise ValueError(str(e))
     except AttributeError as e:
         if '__qualname__' in str(e):
             raise ValueError("can't pickle function objects")
         raise
+
+# Workaround for Python3.3 bug serializing lambda objects
+def _py33_pickle_serialize(obj):
+    if isinstance(obj, types.FunctionType) \
+            and getattr(obj, "__qualname__", "") == "<lambda>":
+        raise ValueError("can't pickle function objects")
+
+    try:
+        return pickle.dumps(obj, protocol=2)
+    except pickle.PicklingError as e:
+        raise ValueError(str(e))
+
+
+if six.PY2:
+    _pickle_serialize = _py27_pickle_serialize
+else:
+    try:
+        ## Serializing lambdas must fail or it is a bug
+        pickle.dumps(lambda x: x)
+    except pickle.PicklingError:
+        _pickle_serialize = _sane_pickle_serialize
+    else:
+        _pickle_serialize = _py33_pickle_serialize
+
 
 PickleFifoDiskQueue = _serializable_queue(queue.FifoDiskQueue, \
     _pickle_serialize, pickle.loads)
