3cf1911a9237c8cf23f6bbb7cba35335a75623e2
Mikhail Korobov <kmike84@gmail.com>
2015-09-01 03:57:05 +0500

    PY3 fix DefaultHeadersMiddleware tests

--- tests/test_downloadermiddleware_defaultheaders.py
+++ tests/test_downloadermiddleware_defaultheaders.py
@@ -1,10 +1,10 @@
 from unittest import TestCase
-import six
 
 from scrapy.downloadermiddlewares.defaultheaders import DefaultHeadersMiddleware
 from scrapy.http import Request
 from scrapy.spiders import Spider
 from scrapy.utils.test import get_crawler
+from scrapy.utils.python import to_bytes
 
 
 class TestDefaultHeadersMiddleware(TestCase):
@@ -12,8 +12,10 @@ class TestDefaultHeadersMiddleware(TestCase):
     def get_defaults_spider_mw(self):
         crawler = get_crawler(Spider)
         spider = crawler._create_spider('foo')
-        defaults = dict([(k, [v]) for k, v in \
-            six.iteritems(crawler.settings.get('DEFAULT_REQUEST_HEADERS'))])
+        defaults = {
+            to_bytes(k): [to_bytes(v)]
+            for k, v in crawler.settings.get('DEFAULT_REQUEST_HEADERS').items()
+        }
         return defaults, spider, DefaultHeadersMiddleware.from_crawler(crawler)
 
     def test_process_request(self):
@@ -25,9 +27,10 @@ class TestDefaultHeadersMiddleware(TestCase):
     def test_update_headers(self):
         defaults, spider, mw = self.get_defaults_spider_mw()
         headers = {'Accept-Language': ['es'], 'Test-Header': ['test']}
+        bytes_headers = {b'Accept-Language': [b'es'], b'Test-Header': [b'test']}
         req = Request('http://www.scrapytest.org', headers=headers)
-        self.assertEquals(req.headers, headers)
+        self.assertEquals(req.headers, bytes_headers)
 
         mw.process_request(req, spider)
-        defaults.update(headers)
+        defaults.update(bytes_headers)
         self.assertEquals(req.headers, defaults)
