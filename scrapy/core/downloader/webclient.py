73ff87c1dc05e98104f854d16792e302625c5e98
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2016-01-14 12:03:08 +0300

    decode body from utf-8, as scrapy stores body as bytes, and twisted has already converted  to unicode

--- scrapy/core/downloader/webclient.py
+++ scrapy/core/downloader/webclient.py
@@ -132,6 +132,9 @@ class ScrapyHTTPClientFactory(HTTPClientFactory):
         status = int(self.status)
         headers = Headers(self.response_headers)
         respcls = responsetypes.from_args(headers=headers, url=self.url)
+        # XXX - scrapy response stores body as bytes,
+        # but maybe it makes sense to be able to store unicode?
+        body = to_bytes(body)
         return respcls(url=self.url, status=status, headers=headers, body=body)
 
     def _set_connection_attributes(self, request):
