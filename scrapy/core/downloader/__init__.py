f9dc02e23ae35e5ba9dce74dcc2571d02e90eb22
Mikhail Korobov <kmike84@gmail.com>
2016-01-28 02:59:23 +0500

    PY3 fix downloader slots GC

--- scrapy/core/downloader/__init__.py
+++ scrapy/core/downloader/__init__.py
@@ -194,6 +194,6 @@ class Downloader(object):
 
     def _slot_gc(self, age=60):
         mintime = time() - age
-        for key, slot in self.slots.items():
+        for key, slot in list(self.slots.items()):
             if not slot.active and slot.lastseen + slot.delay < mintime:
                 self.slots.pop(key).close()
