095495e99ce327d0fc6cf2064f8a6d871d2f7dd1
Paul Tremberth <paul.tremberth@gmail.com>
2016-02-24 01:24:58 +0100

    Backward-compatibility for common Scrapy context factory patterns

--- scrapy/core/downloader/contextfactory.py
+++ scrapy/core/downloader/contextfactory.py
@@ -30,7 +30,18 @@ try:
         def getCertificateOptions(self):
             # setting verify=True will require you to provide CAs
             # to verify against; in other words: it's not that simple
-            return CertificateOptions(verify=False, method=self._ssl_method)
+
+            # backward-compatible SSL/TLS method:
+            #
+            # * this will respect `method` attribute in often recommended
+            #   `ScrapyClientContextFactory` subclass
+            #   (https://github.com/scrapy/scrapy/issues/1429#issuecomment-131782133)
+            #
+            # * getattr() for `_ssl_method` attribute for context factories
+            #   not calling super(..., self).__init__
+            return CertificateOptions(verify=False,
+                        method=getattr(self, 'method',
+                                       getattr(self, '_ssl_method', None)))
 
         # kept for old-style HTTP/1.0 downloader context twisted calls,
         # e.g. connectSSL()
