adea80caa43dc13ddd542c58f654f2e4cc27e8f0
mgachhui <mgachhui@gmail.com>
2016-05-26 10:28:56 +0530

    ItemPipelineManager: Added check for single items

--- scrapy/pipelines/__init__.py
+++ scrapy/pipelines/__init__.py
@@ -7,6 +7,7 @@ See documentation in docs/item-pipeline.rst
 from scrapy.middleware import MiddlewareManager
 from scrapy.utils.conf import build_component_list
 from scrapy.utils.defer import process_chain
+from scrapy.item import BaseItem
 
 class ItemPipelineManager(MiddlewareManager):
 
@@ -24,7 +25,8 @@ class ItemPipelineManager(MiddlewareManager):
     def process_item(self, item, spider):
         def buildPipelineHandler(func):
             def pipelineHandler(output):
-                return func(output, spider)
+                if isinstance(output, (BaseItem, dict)):
+                    return func(output, spider)
             return pipelineHandler
         pipelines_list = [buildPipelineHandler(method) for method in self.methods['process_item']]
         return process_chain(pipelines_list, item)
