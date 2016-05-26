9f08fc9ca6fb285d09ab03c8c60ed7573f49a709
mgachhui <mgachhui@gmail.com>
2016-05-26 17:18:10 +0530

    Created code outline

--- scrapy/pipelines/__init__.py
+++ scrapy/pipelines/__init__.py
@@ -23,13 +23,38 @@ class ItemPipelineManager(MiddlewareManager):
             self.methods['process_item'].append(pipe.process_item)
 
     def process_item(self, item, spider):
+        pipelines_list = []
+        scraper = spider.crawler.engine.scraper
         def buildPipelineHandler(func):
             def pipelineHandler(output):
                 print pipelines_list.index(pipelineHandler)
                 if isinstance(output, (BaseItem, dict)):
                     return func(output, spider)
                 elif isinstance(output, collections.Iterable):
-                    pass
+                    if isinstance(output[0],tuple):
+                        '''
+                            Returned from a DeferredList, so raise a SplitItemError 
+                            & end callchain
+                        '''
+                        pass
+                    else:
+                        '''
+                            Process iterable of items
+                        '''
+                        scraper.slot.itemproc_size += len(output) - 1
+                        concurrent_items = scraper.concurrent_items
+                        doneIndex = pipelines_list.index(pipelineHandler)
+                        remainingList = pipelines_list[doneIndex: ]
+                        dfd = parallel(output, self.concurrent_items,
+            self._create_intermediate_chain, remainingList, errback, spider)
+                        return dfd
             return pipelineHandler
         pipelines_list = [buildPipelineHandler(method) for method in self.methods['process_item']]
         return process_chain(pipelines_list, item)
+    
+    def _create_intermediate_chain(self,output, methodList, errback, spider)
+        dfd = process_chain(methodList, output, spider)
+        dfd.addBoth(errback)
+        # Need to add scraper._itemproc_finished here, but not sure how
+        # to source the necessary arguments
+        return dfd
