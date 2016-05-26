98e17c70ac6aa43ee62e68211e29f716ea1396a5
mgachhui <mgachhui@gmail.com>
2016-05-26 09:10:22 +0530

    Initial changes to ItemPipelineManager

--- scrapy/pipelines/__init__.py
+++ scrapy/pipelines/__init__.py
@@ -6,6 +6,7 @@ See documentation in docs/item-pipeline.rst
 
 from scrapy.middleware import MiddlewareManager
 from scrapy.utils.conf import build_component_list
+from scrapy.utils.defer import process_chain
 
 class ItemPipelineManager(MiddlewareManager):
 
@@ -21,4 +22,8 @@ class ItemPipelineManager(MiddlewareManager):
             self.methods['process_item'].append(pipe.process_item)
 
     def process_item(self, item, spider):
-        return self._process_chain('process_item', item, spider)
+        def buildPipelineHandler(func):
+            def pipelineHandler(output):
+                return func(output, spider)
+        pipelines_list = [buildPipelineHandler(method) for method in self.methods['process_item']]
+        return process_chain(pipelines_list, item)
