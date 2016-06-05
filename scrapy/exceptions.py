d386b6904f482f6d33e15652f3d177a035feef33
mgachhui <mgachhui@gmail.com>
2016-06-05 10:44:53 +0530

    Added SplitItem exception

--- scrapy/exceptions.py
+++ scrapy/exceptions.py
@@ -33,6 +33,10 @@ class DropItem(Exception):
     """Drop item from the item pipeline"""
     pass
 
+class SplitItem(Exception):
+    """Split an item in the item pipeline"""
+    pass
+
 class NotSupported(Exception):
     """Indicates a feature or method is not supported"""
     pass
