d60642e1755b004e46b87e2011c1873f25895bb8
Artur Gaspar <artur.gaspar.00@gmail.com>
2016-08-12 00:45:42 -0300

    data URI download handler.

---
+++ scrapy/core/downloader/handlers/data.py
@@ -0,0 +1,93 @@
+import base64
+import re
+from six.moves.urllib.parse import unquote
+
+from scrapy.http import TextResponse
+from scrapy.responsetypes import responsetypes
+from scrapy.utils.datatypes import CaselessDict
+from scrapy.utils.decorators import defers
+
+
+# ASCII characters.
+_char = set(map(chr, range(127)))
+
+# RFC 2045 token.
+_token = r'[{}]+'.format(re.escape(''.join(_char -
+                                           # Control characters.
+                                           set(map(chr, range(0, 32))) -
+                                           # tspecials and space.
+                                           set('()<>@,;:\\"/[]?= '))))
+
+# RFC 822 quoted-string, without surrounding quotation marks.
+_quoted_string = r'(?:[{}]|(?:\\[{}]))*'.format(
+    re.escape(''.join(_char - {'"', '\\', '\r'})),
+    re.escape(''.join(_char))
+)
+
+# RFC 2397 mediatype.
+_mediatype_pattern = re.compile(r'{token}/{token}'.format(token=_token))
+
+_mediatype_parameter_pattern = re.compile(
+    r';({token})=(?:({token})|"({quoted})")'.format(token=_token,
+                                                    quoted=_quoted_string)
+)
+
+
+class DataURIDownloadHandler(object):
+    def __init__(self, settings):
+        super(DataURIDownloadHandler, self).__init__()
+
+    @defers
+    def download_request(self, request, spider):
+        url = request.url
+
+        scheme, url = url.split(':', 1)
+        if scheme != 'data':
+            raise ValueError("not a data URI")
+
+        # RFC 3986 section 2.1 allows percent encoding to escape characters
+        # that would be interpreted as delimiters, implying that actual
+        # delimiters should not be percent-encoded.
+        # Decoding before parsing will allow malformed URIs with
+        # percent-encoded delimiters, but it makes parsing easier and should
+        # not affect well-formed URIs, as the delimiters used in this URI
+        # scheme are not allowed, percent-encoded or not, in tokens.
+        url = unquote(url)
+
+        media_type = "text/plain"
+        media_type_params = CaselessDict()
+
+        m = _mediatype_pattern.match(url)
+        if m:
+            media_type = m.group()
+            url = url[m.end():]
+        else:
+            media_type_params['charset'] = "US-ASCII"
+
+        while True:
+            m = _mediatype_parameter_pattern.match(url)
+            if m:
+                attribute, value, value_quoted = m.groups()
+                if value_quoted:
+                    value = re.sub(r'\\(.)', '\1', value_quoted)
+                media_type_params[attribute] = value
+                url = url[m.end():]
+            else:
+                break
+
+        is_base64, data = url.split(',', 1)
+        if is_base64:
+            if is_base64 != ";base64":
+                raise ValueError("invalid data URI")
+            data = base64.b64decode(data)
+
+        respcls = responsetypes.from_mimetype(media_type)
+
+        resp_kwargs = {}
+
+        if media_type:
+            media_type = media_type.split('/')
+            if issubclass(respcls, TextResponse) and media_type[0] == 'text':
+                resp_kwargs['encoding'] = media_type_params.get('charset')
+
+        return respcls(url=request.url, body=data, **resp_kwargs)
