0ac0d3f4bd98602a8a66244e7798c5db0e9f2b50
Debosmit Ray <debo@uber.com>
2016-08-18 19:05:06 -0700

    adding sys import

--- scrapy/commands/crawl.py
+++ scrapy/commands/crawl.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import sys
 from scrapy.commands import ScrapyCommand
 from scrapy.utils.conf import arglist_to_dict
 from scrapy.utils.python import without_none_values
@@ -36,7 +37,8 @@ class Command(ScrapyCommand):
         try:
             opts.spargs = arglist_to_dict(opts.spargs)
         except ValueError:
-            raise UsageError("Invalid -a value, use -a NAME=VALUE", print_help=False)
+            raise UsageError(
+                "Invalid -a value, use -a NAME=VALUE", print_help=False)
         if opts.output:
             if opts.output == '-':
                 self.settings.set('FEED_URI', 'stdout:', priority='cmdline')
@@ -46,19 +48,22 @@ class Command(ScrapyCommand):
                 self.settings.getwithbase('FEED_EXPORTERS'))
             valid_output_formats = feed_exporters.keys()
             if not opts.output_format:
-                opts.output_format = os.path.splitext(opts.output)[1].replace(".", "")
+                opts.output_format = os.path.splitext(
+                    opts.output)[1].replace(".", "")
             if opts.output_format not in valid_output_formats:
                 raise UsageError("Unrecognized output format '%s', set one"
                                  " using the '-t' switch or as a file extension"
                                  " from the supported list %s" % (opts.output_format,
                                                                   tuple(valid_output_formats)))
-            self.settings.set('FEED_FORMAT', opts.output_format, priority='cmdline')
+            self.settings.set(
+                'FEED_FORMAT', opts.output_format, priority='cmdline')
 
     def run(self, args, opts):
         if len(args) < 1:
             raise UsageError()
         elif len(args) > 1:
-            raise UsageError("running 'scrapy crawl' with more than one spider is no longer supported")
+            raise UsageError(
+                "running 'scrapy crawl' with more than one spider is no longer supported")
         spname = args[0]
 
         d = self.crawler_process.crawl(spname, **opts.spargs)
@@ -66,7 +71,7 @@ class Command(ScrapyCommand):
         def _crawl_error(_exc):
             global exception
             exception = _exc
-            
+
         d.addErrback(_crawl_error)
         self.crawler_process.start()
         if exception is not None:
