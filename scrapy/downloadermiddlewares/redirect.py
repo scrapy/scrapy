9dcad94273990aad8626be197197892350bd797b
Paul Tremberth <paul.tremberth@gmail.com>
2016-10-12 12:50:17 +0200

    Do not interpret non-ASCII bytes in "Location" and percent-encode them
    Fixes GH-2321
    
    The idea is to not guess the encoding of "Location" header value
    and simply percent-encode non-ASCII bytes,
    which should then be re-interpreted correctly by the remote website
    in whatever encoding was used originally.
    
    See https://tools.ietf.org/html/rfc3987#section-3.2
    
    This is similar to the changes to safe_url_string in
    https://github.com/scrapy/w3lib/pull/45

--- scrapy/downloadermiddlewares/redirect.py
+++ scrapy/downloadermiddlewares/redirect.py
@@ -1,6 +1,8 @@
 import logging
 from six.moves.urllib.parse import urljoin
 
+from w3lib.url import safe_url_string
+
 from scrapy.http import HtmlResponse
 from scrapy.utils.response import get_meta_refresh
 from scrapy.utils.python import to_native_str
@@ -65,8 +67,7 @@ class RedirectMiddleware(BaseRedirectMiddleware):
         if 'Location' not in response.headers or response.status not in allowed_status:
             return response
 
-        # HTTP header is ascii or latin1, redirected url will be percent-encoded utf-8
-        location = to_native_str(response.headers['location'].decode('latin1'))
+        location = safe_url_string(response.headers['location'])
 
         redirected_url = urljoin(request.url, location)
 
