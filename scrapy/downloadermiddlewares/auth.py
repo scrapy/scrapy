fb57c71ab49e4797e276791711f714171fc64bd4
Paul Tremberth <paul.tremberth@gmail.com>
2016-10-18 12:45:02 +0200

    Refactor http/ftp URL tests, use urlpase_cached + add more tests

--- scrapy/downloadermiddlewares/auth.py
+++ scrapy/downloadermiddlewares/auth.py
@@ -1,19 +1,38 @@
 """
-HTTP basic auth downloader middleware
+HTTP/FTP Authorization downloader middleware
 
 See documentation in docs/topics/downloader-middleware.rst
 """
+from six.moves.urllib.parse import urlunparse
 
 from w3lib.http import basic_auth_header
 
 from scrapy import signals
+from scrapy.utils.httpobj import urlparse_cached
 
-from six.moves.urllib.parse import urlparse
+
+def credstrip_url(parsed_url):
+    """Strip username and password from an urlparse'd URL"""
+    return urlunparse((
+        parsed_url.scheme,
+        parsed_url.netloc.split('@')[-1],
+        parsed_url.path,
+        parsed_url.params,
+        parsed_url.query,
+        parsed_url.fragment))
 
 
 class AuthMiddleware(object):
-    """Set Basic HTTP Authorization header
-    (http_user and http_pass spider class attributes)"""
+    """
+    Populate authorization credentials for HTTP and FTP requests.
+
+    For http(s):// requests, set Basic HTTP Authorization header,
+    either from http_user and http_pass spider attributes,
+    or from URL netloc parsing.
+
+    Also handle FTP credentials from ftp://user:password@... URLs,
+    populating request's meta accordingly.
+    """
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -28,19 +47,33 @@ class AuthMiddleware(object):
             self.auth = basic_auth_header(usr, pwd)
 
     def process_request(self, request, spider):
-        auth = getattr(self, 'auth', None)
-        if auth and 'Authorization' not in request.headers:
-            request.headers['Authorization'] = auth
+        url = urlparse_cached(request)
+        if url.scheme.startswith('http'):
+            # do not override Auth header set priorly
+            if 'Authorization' in request.headers:
+                return
 
-        # credentials from url are supposed to override spider settings
-        url = urlparse(request.url)
-        if url.username and url.password:
-            if url.scheme.startswith('ftp'):
-                request.meta['ftp_user'] = url.username
-                request.meta['ftp_password'] = url.password
-            elif url.scheme.startswith('http'):
-                request.headers['Authorization'] = basic_auth_header(url.username, url.password)
+            new_url = None
+            # credentials from URL override spider attributes
+            if url.username or url.password:
+                auth = basic_auth_header(url.username, url.password)
 
-            # no credentials in new url
-            new_url = url.scheme + '://' + request.url.split('@')[-1]
-            return request.replace(url=new_url)
+                # no credentials in new url
+                new_url = credstrip_url(url)
+
+            else:
+                auth = getattr(self, 'auth', None)
+
+            if auth:
+                request.headers['Authorization'] = auth
+                if new_url:
+                    return request.replace(url=new_url)
+
+        elif url.scheme.startswith('ftp'):
+            if url.username or url.password:
+                # priorly set credentials take precedence
+                request.meta.setdefault('ftp_user', url.username)
+                request.meta.setdefault('ftp_password', url.password)
+
+                # no credentials in new url
+                return request.replace(url=credstrip_url(url))
