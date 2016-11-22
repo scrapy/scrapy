4adfaced6ae82db8ba38c33daefa06d5ffafdf11
Daniel Graña <dangra@gmail.com>
2016-11-22 09:41:20 -0300

    Use Twisted modules for client and endpoints

--- scrapy/xlib/tx/__init__.py
+++ scrapy/xlib/tx/__init__.py
@@ -1,14 +1,16 @@
 from scrapy import twisted_version
-if twisted_version > (13, 0, 0):
+if twisted_version >= (13, 1, 0):
     from twisted.web import client
     from twisted.internet import endpoints
-if twisted_version >= (11, 1, 0):
+elif twisted_version >= (11, 1, 0):
     from . import client, endpoints
 else:
     from scrapy.exceptions import NotSupported
+
     class _Mocked(object):
         def __init__(self, *args, **kw):
             raise NotSupported('HTTP1.1 not supported')
+
     class _Mock(object):
         def __getattr__(self, name):
             return _Mocked
