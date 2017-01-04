b5ae8666e8282be1191911b80a7db5f8805c901e
Paul Tremberth <paul.tremberth@gmail.com>
2017-01-04 14:59:38 +0100

    Testing with Twisted 16.7 RC

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=[
-        'Twisted>=13.1.0',
+        'Twisted==16.7.0rc1',
         'w3lib>=1.15.0',
         'queuelib',
         'lxml',
