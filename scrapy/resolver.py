c32f7c12d5ceb1b3047232516297c628905630fe
Paul Tremberth <paul.tremberth@gmail.com>
2017-01-05 13:41:12 +0100

    Experimental caching GAIResolver

--- scrapy/resolver.py
+++ scrapy/resolver.py
@@ -1,5 +1,11 @@
+from functools import wraps
+from socket import getaddrinfo
+from zope.interface import implementer
+
 from twisted.internet import defer
 from twisted.internet.base import ThreadedResolver
+from twisted.internet.interfaces import IHostnameResolver
+from twisted.internet._resolver import GAIResolver
 
 from scrapy.utils.datatypes import LocalCache
 
@@ -25,3 +31,26 @@ class CachingThreadedResolver(ThreadedResolver):
     def _cache_result(self, result, name):
         dnscache[name] = result
         return result
+
+
+# Borrowed from https://wiki.python.org/moin/PythonDecoratorLibrary#Alternate_memoize_as_nested_functions
+# note that this decorator ignores **kwargs
+def memoize(obj, cache):
+    cache = cache
+
+    @wraps(obj)
+    def memoizer(*args, **kwargs):
+        if args not in cache:
+            cache[args] = obj(*args, **kwargs)
+        return cache[args]
+    return memoizer
+
+
+@implementer(IHostnameResolver)
+class CachingGAIResolver(GAIResolver):
+    def __init__(self, reactor, getThreadPool=None, cache_size=0):
+        dnscache.limit = cache_size
+        super(CachingGAIResolver, self).__init__(reactor,
+            getThreadPool=getThreadPool,
+            getaddrinfo=memoize(getaddrinfo, dnscache) if cache_size
+                else getaddrinfo)
