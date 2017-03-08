3cd9185aa12430708b17eb8c02a6bdc3709ed5f9
jorenham <jhammudoglu@gmail.com>
2017-03-08 20:44:39 +0100

    Fixed the FIXME; more specific exception catching

--- scrapy/pipelines/files.py
+++ scrapy/pipelines/files.py
@@ -58,7 +58,7 @@ class FSFilesStore(object):
         absolute_path = self._get_filesystem_path(path)
         try:
             last_modified = os.path.getmtime(absolute_path)
-        except:  # FIXME: catching everything!
+        except os.error:
             return {}
 
         with open(absolute_path, 'rb') as f:
