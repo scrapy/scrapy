67e1a1e051f099a0d2fe98e17e9185a2a4017adc
jorenham <jhammudoglu@gmail.com>
2017-03-16 21:47:20 +0100

    Simplified middleware template fixed failing test

--- scrapy/spidermiddlewares/__init__.py
+++ scrapy/spidermiddlewares/__init__.py
@@ -1,16 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
 class SpiderMiddleware(object):
+
     def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
         pass
 
     def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
         return result
 
-    def process_spider_exception(self, repsonse, exception, spider):
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Response, dict
+        # or Item objects.
         pass
 
     def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
         return start_requests
 
     @classmethod
     def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
         pass
