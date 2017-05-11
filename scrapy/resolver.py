a21942fde58366199bdf02fd05031e00d03bef2c
Paul Tremberth <paul.tremberth@gmail.com>
2017-05-11 17:49:12 +0200

    Handle Unicode domain name in Scrapy's name resolver
    Fixes GH-2728

--- scrapy/resolver.py
+++ scrapy/resolver.py
@@ -1,3 +1,5 @@
+from six import text_type
+
 from twisted.internet import defer
 from twisted.internet.base import ThreadedResolver
 
@@ -14,15 +16,23 @@ class CachingThreadedResolver(ThreadedResolver):
         self.timeout = timeout
 
     def getHostByName(self, name, timeout=None):
-        if name in dnscache:
-            return defer.succeed(dnscache[name])
+        if isinstance(name, text_type):
+            try:
+                safe_name = name.encode('idna')
+            except UnicodeError:
+                # we did our best, let it fail later.
+                safe_name = name
+        else:
+            safe_name = name
+        if safe_name in dnscache:
+            return defer.succeed(dnscache[safe_name])
         # in Twisted<=16.6, getHostByName() is always called with
         # a default timeout of 60s (actually passed as (1, 3, 11, 45) tuple),
         # so the input argument above is simply overridden
         # to enforce Scrapy's DNS_TIMEOUT setting's value
         timeout = (self.timeout,)
-        d = super(CachingThreadedResolver, self).getHostByName(name, timeout)
-        d.addCallback(self._cache_result, name)
+        d = super(CachingThreadedResolver, self).getHostByName(safe_name, timeout)
+        d.addCallback(self._cache_result, safe_name)
         return d
 
     def _cache_result(self, result, name):
