1a452c038cc0547924051a7cd0786215ec7c2104
Bernardas <bernardas.alisauskas@gmail.com>
2017-05-18 16:57:13 +0000

    increase ptpython priority since it can use other shells as backend

--- scrapy/utils/console.py
+++ scrapy/utils/console.py
@@ -56,9 +56,9 @@ def _embed_standard_shell(namespace={}, banner=''):
     return wrapper
 
 DEFAULT_PYTHON_SHELLS = OrderedDict([
+    ('ptpython', _embed_ptpython_shell),
     ('ipython', _embed_ipython_shell),
     ('bpython', _embed_bpython_shell),
-    ('ptpython', _embed_ptpython_shell),
     ('python', _embed_standard_shell),
 ])
 
