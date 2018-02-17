af97f17357cf0344901f2a6ada0f60c8312250ad
nctl144 <nctl144@gmail.com>
2018-02-16 19:02:27 -0500

    restore request's callback after calling custom callback

--- scrapy/commands/parse.py
+++ scrapy/commands/parse.py
@@ -205,12 +205,14 @@ class Command(ScrapyCommand):
                     req.meta['_depth'] = depth + 1
                     req.meta['_callback'] = req.callback
                     req.callback = callback
+                    req.callback = original_callback
                 return requests
 
         #update request meta if any extra meta was passed through the --meta/-m opts.
         if opts.meta:
             request.meta.update(opts.meta)
 
+        original_callback = request.callback
         request.meta['_depth'] = 1
         request.meta['_callback'] = request.callback
         request.callback = callback
