71dbbda2e0c5f9acd4212b6b87801fc95fa6d634
nctl144 <nctl144@gmail.com>
2018-03-10 21:21:51 -0500

    use response.selector right away

--- scrapy/loader/__init__.py
+++ scrapy/loader/__init__.py
@@ -26,10 +26,8 @@ class ItemLoader(object):
 
     def __init__(self, item=None, selector=None, response=None, parent=None, **context):
         if selector is None and response is not None:
-            if response.selector.__class__ == Selector:
-                selector = response.selector
-            else:
-                selector = self.default_selector_class(response)
+            self.default_selector_class = response.selector
+            selector = self.default_selector_class
         self.selector = selector
         context.update(selector=selector, response=response)
         if item is None:
