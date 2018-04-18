446fc48cce2360b9b83d40d21a1e6e192819f306
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2018-04-18 14:56:24 +0000

    pass mockserver address as variable

--- tests/test_feedexport.py
+++ tests/test_feedexport.py
@@ -179,7 +179,7 @@ class FeedExportTest(unittest.TestCase):
         try:
             with MockServer() as s:
                 runner = CrawlerRunner(Settings(defaults))
-                spider_cls.start_urls = [MockServer.url('/')]
+                spider_cls.start_urls = [s.address().url('/')]
                 yield runner.crawl(spider_cls)
 
             with open(res_name, 'rb') as f:
