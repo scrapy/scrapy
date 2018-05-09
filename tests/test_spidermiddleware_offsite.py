fc27fb52fbe72785020bcf69e095b677bfab8ce4
Ryan P Kilby <rpkilby@ncsu.edu>
2018-05-09 06:15:47 -0400

    Refactor offsite tests to prevent unintended successes

--- tests/test_spidermiddleware_offsite.py
+++ tests/test_spidermiddleware_offsite.py
@@ -10,71 +10,86 @@ from scrapy.utils.test import get_crawler
 import warnings
 
 
-class TestOffsiteMiddleware(TestCase):
+class OffsiteMiddlewareTestCase:
+    spider_kwargs = None
+    onsite_reqs = []
+    offsite_reqs = []
 
     def setUp(self):
         crawler = get_crawler(Spider)
-        self.spider = crawler._create_spider(**self._get_spiderargs())
+        self.spider = crawler._create_spider(**self.spider_kwargs)
         self.mw = OffsiteMiddleware.from_crawler(crawler)
         self.mw.spider_opened(self.spider)
 
-    def _get_spiderargs(self):
-        return dict(name='foo', allowed_domains=['scrapytest.org', 'scrapy.org', 'scrapy.test.org'])
-
     def test_process_spider_output(self):
-        res = Response('http://scrapytest.org')
+        """
+        Test that the processed spider's output only matches the onsite_reqs.
+        """
+        resp = Response('http://scrapytest.org')
+        reqs = self.onsite_reqs + self.offsite_reqs
 
-        onsite_reqs = [Request('http://scrapytest.org/1'),
-                       Request('http://scrapy.org/1'),
-                       Request('http://sub.scrapy.org/1'),
-                       Request('http://offsite.tld/letmepass', dont_filter=True),
-                       Request('http://scrapy.test.org/')]
-        offsite_reqs = [Request('http://scrapy2.org'),
-                        Request('http://offsite.tld/'),
-                        Request('http://offsite.tld/scrapytest.org'),
-                        Request('http://offsite.tld/rogue.scrapytest.org'),
-                        Request('http://rogue.scrapytest.org.haha.com'),
-                        Request('http://roguescrapytest.org'),
-                        Request('http://test.org/'),
-                        Request('http://notscrapy.test.org/')]
-        reqs = onsite_reqs + offsite_reqs
-
-        out = list(self.mw.process_spider_output(res, reqs, self.spider))
-        self.assertEqual(out, onsite_reqs)
+        out = list(self.mw.process_spider_output(resp, reqs, self.spider))
+        self.assertEqual(out, self.onsite_reqs)
 
 
-class TestOffsiteMiddleware2(TestOffsiteMiddleware):
+class TestOffsiteMiddleware(OffsiteMiddlewareTestCase, TestCase):
+    spider_kwargs = dict(
+        name='foo',
+        allowed_domains=['scrapytest.org', 'scrapy.org', 'scrapy.test.org'],
+    )
 
-    def _get_spiderargs(self):
-        return dict(name='foo', allowed_domains=None)
-
-    def test_process_spider_output(self):
-        res = Response('http://scrapytest.org')
-        reqs = [Request('http://a.com/b.html'), Request('http://b.com/1')]
-        out = list(self.mw.process_spider_output(res, reqs, self.spider))
-        self.assertEqual(out, reqs)
-
-
-class TestOffsiteMiddleware3(TestOffsiteMiddleware2):
-
-    def _get_spiderargs(self):
-        return dict(name='foo')
-
-
-class TestOffsiteMiddleware4(TestOffsiteMiddleware3):
-
-    def _get_spiderargs(self):
-        bad_hostname = urlparse('http:////scrapytest.org').hostname
-        return dict(name='foo', allowed_domains=['scrapytest.org', None, bad_hostname])
-
-    def test_process_spider_output(self):
-        res = Response('http://scrapytest.org')
-        reqs = [Request('http://scrapytest.org/1')]
-        out = list(self.mw.process_spider_output(res, reqs, self.spider))
-        self.assertEqual(out, reqs)
+    onsite_reqs = [
+        Request('http://scrapytest.org/1'),
+        Request('http://scrapy.org/1'),
+        Request('http://sub.scrapy.org/1'),
+        Request('http://offsite.tld/letmepass', dont_filter=True),
+        Request('http://scrapy.test.org/'),
+    ]
+    offsite_reqs = [
+        Request('http://scrapy2.org'),
+        Request('http://offsite.tld/'),
+        Request('http://offsite.tld/scrapytest.org'),
+        Request('http://offsite.tld/rogue.scrapytest.org'),
+        Request('http://rogue.scrapytest.org.haha.com'),
+        Request('http://roguescrapytest.org'),
+        Request('http://test.org/'),
+        Request('http://notscrapy.test.org/'),
+    ]
 
 
-class TestOffsiteMiddleware5(TestOffsiteMiddleware4):
+class TestEmptyDomains(OffsiteMiddlewareTestCase, TestCase):
+    spider_kwargs = dict(name='foo', allowed_domains=None)
+
+    onsite_reqs = [
+        Request('http://a.com/b.html'),
+        Request('http://b.com/1'),
+    ]
+    offsite_reqs = []
+
+
+class TestNoAllowedDomainsArgument(TestEmptyDomains):
+    spider_kwargs = dict(name='foo')
+
+
+class TestBadHostnames(OffsiteMiddlewareTestCase, TestCase):
+    bad_hostname = urlparse('http:////scrapytest.org').hostname
+    spider_kwargs = dict(
+        name='foo',
+        allowed_domains=['scrapytest.org', None, bad_hostname],
+    )
+
+    onsite_reqs = [Request('http://scrapytest.org/1')]
+    offsite_reqs = []
+
+
+class TestURLWarnings(OffsiteMiddlewareTestCase, TestCase):
+    spider_kwargs = dict(
+        name='foo',
+        allowed_domains=['http://scrapytest.org', 'scrapy.org'],
+    )
+
+    onsite_reqs = [Request('http://scrapy.org/1')]
+    offsite_reqs = [Request('http://scrapytest.org/1')]
 
     def test_get_host_regex(self):
         self.spider.allowed_domains = ['http://scrapytest.org', 'scrapy.org', 'scrapy.test.org']
