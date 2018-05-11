c6ef9016ef9d8958691679c6e253129e2587b225
Erik Dominguez <erik.dominguez1003@gmail.com>
2018-05-11 14:52:33 +0200

    Fix test_urlparse_cached_strip()

--- tests/test_utils_httpobj.py
+++ tests/test_utils_httpobj.py
@@ -21,7 +21,7 @@ class HttpobjUtilsTest(unittest.TestCase):
         assert req1a is not req2
         assert req1a is not req2
 
-    def test_urlparse_cached_strip():
+    def test_urlparse_cached_strip(self):
 
         #Test out that urlparse_cached strips out any \n
         # or \t from its url
@@ -31,16 +31,12 @@ class HttpobjUtilsTest(unittest.TestCase):
 
         request1 = Request(normal_url)
         request2 = Request(dirty_url)
-        req1a = urlparse_cached(request1)
-        req1b = urlparse_cached(request1)
+        req1 = urlparse_cached(request1)
         req2 = urlparse_cached(request2)
         urlp = urlparse(url)
 
-        assert req1a == req2
-        assert req1a == urlp
-        assert req1a is req1b
-        assert req1a is not req2
-        assert req1a is not req2
+        assert req1 == req2
+        assert req1 != urlp
 
 
 
