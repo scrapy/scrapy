65a88e7738834246cc7f93e2d6675fb809f67407
Grammy Jiang <grammy.jiang@gmail.com>
2018-07-04 16:54:00 +0800

    move the unnecessary functions out of the middleware

--- scrapy/downloadermiddlewares/httpproxy.py
+++ scrapy/downloadermiddlewares/httpproxy.py
@@ -11,13 +11,32 @@ from scrapy.utils.httpobj import urlparse_cached
 from scrapy.utils.python import to_bytes
 
 
+def basic_auth_header(username, password, auth_encoding):
+    user_pass = to_bytes(
+        '%s:%s' % (unquote(username), unquote(password)),
+        encoding=auth_encoding)
+    return base64.b64encode(user_pass).strip()
+
+
+def get_proxy(url, orig_type, auth_encoding):
+    proxy_type, user, password, hostport = _parse_proxy(url)
+    proxy_url = urlunparse((proxy_type or orig_type, hostport, '', '', '', ''))
+
+    if user:
+        creds = basic_auth_header(user, password, auth_encoding)
+    else:
+        creds = None
+
+    return creds, proxy_url
+
+
 class HttpProxyMiddleware(object):
 
     def __init__(self, auth_encoding='latin-1'):
         self.auth_encoding = auth_encoding
         self.proxies = {}
         for type_, url in getproxies().items():
-            self.proxies[type_] = self._get_proxy(url, type_)
+            self.proxies[type_] = get_proxy(url, type_, self.auth_encoding)
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -27,21 +46,23 @@ class HttpProxyMiddleware(object):
         return cls(auth_encoding)
 
     def _basic_auth_header(self, username, password):
-        user_pass = to_bytes(
-            '%s:%s' % (unquote(username), unquote(password)),
-            encoding=self.auth_encoding)
-        return base64.b64encode(user_pass)
+        warnings.warn(
+            "Method `scrapy.downloadermiddleware.httpproxy.HttpProxyMiddleware._basic_auth_header` "
+            "is deprecated and will be removed in future releases. Please use "
+            "`w3lib.http.basic_auth_header` instead",
+            ScrapyDeprecationWarning, stacklevel=2,
+        )
+        return basic_auth_header(username, password, self.auth_encoding)
 
     def _get_proxy(self, url, orig_type):
-        proxy_type, user, password, hostport = _parse_proxy(url)
-        proxy_url = urlunparse((proxy_type or orig_type, hostport, '', '', '', ''))
-
-        if user:
-            creds = self._basic_auth_header(user, password)
-        else:
-            creds = None
-
-        return creds, proxy_url
+        warnings.warn(
+            "Method 'scrapy.downloadermiddleware.httpproxy.HttpProxyMiddleware._get_proxy "
+            "is deprecated and will be removed in future releases. Please use "
+            "`scrapy.downloadermiddleware.httpproxy.HttpProxyMiddleware.get_proxy` "
+            "instead",
+            ScrapyDeprecationWarning, stacklevel=2,
+        )
+        return get_proxy(url, orig_type, self.auth_encoding)
 
     def process_request(self, request, spider):
         # ignore if proxy is already set
@@ -49,7 +70,7 @@ class HttpProxyMiddleware(object):
             if request.meta['proxy'] is None:
                 return
             # extract credentials if present
-            creds, proxy_url = self._get_proxy(request.meta['proxy'], '')
+            creds, proxy_url = get_proxy(request.meta['proxy'], '', self.auth_encoding)
             request.meta['proxy'] = proxy_url
             if creds and not request.headers.get('Proxy-Authorization'):
                 request.headers['Proxy-Authorization'] = b'Basic ' + creds
