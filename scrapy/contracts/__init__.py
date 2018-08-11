fb7d4cbce379c4e3fac2ca89b6e0772d0c690935
Stas Glubokiy <glubokiy.stas@gmail.com>
2018-08-11 16:08:26 +0300

    Add error handling in contracts

--- scrapy/contracts/__init__.py
+++ scrapy/contracts/__init__.py
@@ -41,7 +41,11 @@ class ContractsManager(object):
         requests = []
         for method in self.tested_methods_from_spidercls(type(spider)):
             bound_method = spider.__getattribute__(method)
-            requests.append(self.from_method(bound_method, results))
+            try:
+                requests.append(self.from_method(bound_method, results))
+            except:
+                case = _create_testcase(bound_method, 'contract')
+                results.addError(case, sys.exc_info())
 
         return requests
 
