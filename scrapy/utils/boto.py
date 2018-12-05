f96e809fcd74c87e5c72e04dded194f01ad98229
Zhuoyun Wei <wzyboy@wzyboy.org>
2018-12-05 08:34:08 -0500

    Warn about the deprecated usage of boto

--- scrapy/utils/boto.py
+++ scrapy/utils/boto.py
@@ -1,9 +1,11 @@
 """Boto/botocore helpers"""
 
 from __future__ import absolute_import
+import warnings
 import six
 
 from scrapy.exceptions import NotConfigured
+from scrapy.exceptions import ScrapyDeprecationWarning
 
 
 def is_botocore():
@@ -14,6 +16,11 @@ def is_botocore():
         if six.PY2:
             try:
                 import boto
+                message = (
+                    'Usage of boto in Scrapy is deprecated. '
+                    'Consider installing botocore for a stable and recommended way to access AWS.'
+                )
+                warnings.warn(message, category=ScrapyDeprecationWarning, stacklevel=2)
                 return False
             except ImportError:
                 raise NotConfigured('missing botocore or boto library')
