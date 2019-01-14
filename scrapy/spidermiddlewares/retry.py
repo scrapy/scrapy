8e3d3d8c2672762dc9666cff296106ed598cd5a0
kasun Herath <kasunh01@gmail.com>
2019-01-14 12:55:05 +0530

    retry spidermiddleware check retrying enabled

--- scrapy/spidermiddlewares/retry.py
+++ scrapy/spidermiddlewares/retry.py
@@ -4,9 +4,13 @@ processed by spider before been retried.
 
 You can issue a retry request by raising a `scrapy.exceptions.RetryRequest` exception inside a spider callback
 """
+import logging
 
 from scrapy.exceptions import RetryRequest
-from scrapy.utils.retry import RetryHandler
+from scrapy.utils.retry import RetryHandler, is_retrying_enabled_on_request
+
+
+logger = logging.getLogger(__name__)
 
 
 class RetryMiddleware(object):
@@ -20,9 +24,6 @@ class RetryMiddleware(object):
 
     def process_spider_output(self, response, result, spider):
         # work around until this is fixed https://github.com/scrapy/scrapy/issues/220
-        if not self.enabled:
-            return
-
         try:
             for x in result:
                 yield x
@@ -34,10 +35,23 @@ class RetryMiddleware(object):
 
     def process_spider_exception(self, response, exception, spider):
         if isinstance(exception, RetryRequest):
-            if not self.enabled:
-                return []
-
             request = response.request
+            if not self.enabled:
+                logger.debug(
+                    "Found a retry request but request retrying is disabled: %(request)s",
+                    {'request': request},
+                    extra={'spider': spider}
+                )
+                return None
+
+            if not is_retrying_enabled_on_request(request):
+                logger.debug(
+                    "Found a retry request on a request that has retrying disabled: %(request)s",
+                    {'request': request},
+                    extra={'spider': spider}
+                )
+                return None
+
             retry_handler = RetryHandler(spider, request)
             if retry_handler.is_exhausted():
                 retry_handler.record_retry_failure(exception)
