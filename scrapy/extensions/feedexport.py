cfd183a9d19563f09487af942c9a635d665a1905
Victor Torres <vpaivatorres@gmail.com>
2019-02-08 14:49:26 -0200

    no need to use get here since we're defining a default value in default_settings.py

--- scrapy/extensions/feedexport.py
+++ scrapy/extensions/feedexport.py
@@ -133,7 +133,7 @@ class S3FeedStorage(BlockingFeedStorage):
     def from_crawler(cls, crawler, uri):
         return cls(uri, crawler.settings['AWS_ACCESS_KEY_ID'],
                    crawler.settings['AWS_SECRET_ACCESS_KEY'],
-                   crawler.settings.get('FEED_STORAGE_S3_ACL'))
+                   crawler.settings['FEED_STORAGE_S3_ACL'])
 
     def _store_in_thread(self, file):
         file.seek(0)
