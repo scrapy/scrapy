773efd58a463894e1365fce066df1d3df30956b8
Felix <felixlil@kth.se>
2019-02-15 16:46:46 +0100

    refactor: finish script for annotating src files with branch flags

--- adhoc_tool/annotate.py
+++ adhoc_tool/annotate.py
@@ -1,39 +1,46 @@
-""" Annotate a source file with flags used by code coverage tool.
+""" Annotate an entire source file with flags used by code coverage tool.
     The flags indicate whether a branch was taken or not.
-    Note: This annotation doesn't handle ternary operators
+
+    NOTE: This script DOES NOT handle ternary operators and list comprehensions
     Info about token types: https://www.asmeurer.com/brown-water-python/tokens.html
 """
 __author__ = "Felix Liljefors"
 
 import sys
-import token
-from token import NAME, INDENT, DEDENT
 import tokenize
+import token
+import strip_comments
+import os
+from token import NAME, INDENT, DEDENT
+from adhoc_cov_config import BRANCH_COUNT, BRANCH_LIST_NAME
 
-DEBUG = False
+# Keep track of which lines contain conditional statements
+conditional_line_nums = []
 
 
 def annotate(fname):
-    src = open(fname)
-    annotated_src = open("annotated_" + fname, "w")
+    global BRANCH_COUNT
+    global conditional_line_nums
 
-    # prev_toktype = token.INDENT
+    # Strip source file from comments and docstrings
+    strip_comments.do_file(fname)
+
+    src = open('stripped_' + fname)
+    annotated_src = open('annotated_' + fname, 'w')
+    annotated_src.write('from adhoc_cov_config import BRANCHES\n\n')
+
     indent_lvl = 0
     prev_logical_line = ''
+    prev_logical_line_num = -1
     last_condition_type = None
 
     token_gen = tokenize.generate_tokens(src.readline)
-    for t in token_gen:
-        type, string, (srow, scol), (erow, _), logical_line = t
-
+    for type, string, (srow, _), (_, _), logical_line in token_gen:
         # Keep track of indentation. One indent = 4 spaces
         if type == INDENT:
             indent_lvl += 4
         elif type == DEDENT:
             indent_lvl -= 4
-
-        debug(
-            f'type: {type}\nexact type: {t.exact_type}\nstring: {string}\nlogical line: {logical_line}')
 
         if logical_line != prev_logical_line:
             # Write previous logical line
@@ -42,10 +49,14 @@ def annotate(fname):
             # Check if previous logical line contained a conditional
             # If so, a boolean flag should be inserted on between previous and current logical line
             if last_condition_type != None:
-                debug(f'INSERT FLAG ON LINE: {srow}')
-                debug(f'TOKEN {type} OCCURS AT COL: {scol}')
-                # Match indentation to current logical line
-                annotated_src.write(' ' * indent_lvl + 'flag = False\n')
+                # Match indentation to current logical line and add branch flag variable
+                annotated_src.write(
+                    ' ' * indent_lvl + f'{BRANCH_LIST_NAME}[{BRANCH_COUNT}] = True\n')
+                BRANCH_COUNT += 1
+
+                # Keep track of which lines contain conditional statements
+                conditional_line_nums += [prev_logical_line_num]
+
                 # Only process a condition once, although a line can have many tokens
                 last_condition_type = None
 
@@ -63,15 +74,27 @@ def annotate(fname):
            string == 'catch' or \
            string == 'finally':
 
-            # Record where this logical line ends (next line will set boolean flag)
+            # Record conditional type (boolean flag will be inserted on logical line below)
             last_condition_type = string
-            debug(f'CONDITIONAL ENDS ON LINE: {erow}')
 
+            # Record line number where current logical line started
+            prev_logical_line_num = srow
 
-def debug(str):
-    if DEBUG:
-        print(str)
+    # Clean up
+    try:
+        annotated_src.close()
+        src.close()
+        os.remove('stripped_' + fname)
+    except OSError:
+        pass
 
 
 if __name__ == "__main__":
-    annotate('test.py')
+    # Pass filename provided as cmd line arg
+    annotate(sys.argv[1])
+
+    # Print some details of the annotation
+    print(f'Summary:\n \
+    Branches detected: {BRANCH_COUNT} \n \
+    On lines: {conditional_line_nums}'
+          )
