b941bcbe84bc0efc925ec2e9008c0d5759464683
Maram Sumanth <maram.sumanth@gmail.com>
2019-03-17 16:16:44 +0530

    Update statscollectors.py

--- scrapy/statscollectors.py
+++ scrapy/statscollectors.py
@@ -43,10 +43,6 @@ class StatsCollector(object):
 
     def close_spider(self, spider, reason):
         if self._dump:
-            if self.get_value('log_count/INFO'):
-                self.set_value('log_count/INFO', self.get_value('log_count/INFO') + 5)
-            else:
-                self.set_value('log_count/INFO', 5)
             logger.info("Dumping Scrapy stats:\n" + pprint.pformat(self._stats),
                         extra={'spider': spider})
         self._persist_stats(self._stats, spider)
