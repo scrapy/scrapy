a4f1629ede223da24dd3ce3e311a9e540f007ee0
Adrián Chaves <adrian@chaves.io>
2019-03-24 00:31:05 +0530

    Update scrapy/extensions/corestats.py
    Co-Authored-By: maramsumanth <maram.sumanth@gmail.com>

--- scrapy/extensions/corestats.py
+++ scrapy/extensions/corestats.py
@@ -26,7 +26,7 @@ class CoreStats(object):
     def spider_closed(self, spider, reason):
         finish_time = datetime.datetime.utcnow()
         elapsed_time = finish_time - self.stats.get_value('start_time')
-        elapsed_time_seconds = elapsed_time.seconds + elapsed_time.microseconds/1000000
+        elapsed_time_seconds = elapsed_time.total_seconds()
         self.stats.set_value('elapsed_time_seconds', elapsed_time_seconds, spider=spider)
         self.stats.set_value('finish_time', finish_time, spider=spider)
         self.stats.set_value('finish_reason', reason, spider=spider)
