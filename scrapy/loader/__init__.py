754f52b02781097c8ca6835e057815c7653062d4
Sortafreel <woutut@gmail.com>
2019-06-07 03:20:45 +0300

    Preprocess values if item built from dict.
    https://github.com/scrapy/scrapy/issues/3804

--- scrapy/loader/__init__.py
+++ scrapy/loader/__init__.py
@@ -35,8 +35,9 @@ class ItemLoader(object):
         self.parent = parent
         self._local_item = context['item'] = item
         self._local_values = defaultdict(list)
+        # Preprocess values if item built from dict
         for field_name, value in item.items():
-            self.add_value(field_name, value)
+            self._values[field_name] = self._process_input_value(field_name, value)
 
     @property
     def _values(self):
