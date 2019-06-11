cdeccac6d6ccd0034a5f007ed371c1d481b32c26
sortafreel <woutut@gmail.com>
2019-06-11 17:38:06 +0300

    Linting (return previous indentation).

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -746,28 +746,28 @@ class SubselectorLoaderTest(unittest.TestCase):
 
 
 class SelectJmesTestCase(unittest.TestCase):
-    test_list_equals = {
-        'simple': ('foo.bar', {"foo": {"bar": "baz"}}, "baz"),
-        'invalid': ('foo.bar.baz', {"foo": {"bar": "baz"}}, None),
-        'top_level': ('foo', {"foo": {"bar": "baz"}}, {"bar": "baz"}),
-        'double_vs_single_quote_string': ('foo.bar', {"foo": {"bar": "baz"}}, "baz"),
-        'dict': (
-            'foo.bar[*].name',
-            {"foo": {"bar": [{"name": "one"}, {"name": "two"}]}},
-            ['one', 'two']
-        ),
-        'list': ('[1]', [1, 2], 2)
-    }
+        test_list_equals = {
+            'simple': ('foo.bar', {"foo": {"bar": "baz"}}, "baz"),
+            'invalid': ('foo.bar.baz', {"foo": {"bar": "baz"}}, None),
+            'top_level': ('foo', {"foo": {"bar": "baz"}}, {"bar": "baz"}),
+            'double_vs_single_quote_string': ('foo.bar', {"foo": {"bar": "baz"}}, "baz"),
+            'dict': (
+                'foo.bar[*].name',
+                {"foo": {"bar": [{"name": "one"}, {"name": "two"}]}},
+                ['one', 'two']
+            ),
+            'list': ('[1]', [1, 2], 2)
+        }
 
-    def test_output(self):
-        for l in self.test_list_equals:
-            expr, test_list, expected = self.test_list_equals[l]
-            test = SelectJmes(expr)(test_list)
-            self.assertEqual(
-                test,
-                expected,
-                msg='test "{}" got {} expected {}'.format(l, test, expected)
-            )
+        def test_output(self):
+            for l in self.test_list_equals:
+                expr, test_list, expected = self.test_list_equals[l]
+                test = SelectJmes(expr)(test_list)
+                self.assertEqual(
+                    test,
+                    expected,
+                    msg='test "{}" got {} expected {}'.format(l, test, expected)
+                )
 
 
 if __name__ == "__main__":
