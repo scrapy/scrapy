a4badba1ee87c59a6c153e7edca17cff2079152d
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2019-08-02 17:00:40 -0300

    Bump minimum Twisted version in py27; Envs with pinned versions for py27 and py35

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     install_requires=[
-        'Twisted>=13.1.0;python_version=="2.7"',
+        'Twisted>=16.0.0;python_version=="2.7"',
         'Twisted>=17.9.0;python_version>="3.5"',
         'cssselect>=0.9.1',
         'lxml>=3.5.0',
