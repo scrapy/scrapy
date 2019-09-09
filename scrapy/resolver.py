735c0ceb7890dbea607dd6e4c0a14a4ce2b0afd2
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2019-09-09 16:20:58 -0300

    Custom name resolver implementing twisted.internet.interfaces.IHostnameResolver

--- scrapy/resolver.py
+++ scrapy/resolver.py
@@ -1,32 +1,39 @@
-from twisted.internet import defer
-from twisted.internet.base import ThreadedResolver
+from twisted.internet.interfaces import IHostnameResolver, IResolutionReceiver
+from zope.interface.declarations import implementer, provider
 
 from scrapy.utils.datatypes import LocalCache
 
-# TODO: cache misses
 
+# TODO: cache misses
 dnscache = LocalCache(10000)
 
 
-class CachingThreadedResolver(ThreadedResolver):
-    def __init__(self, reactor, cache_size, timeout):
-        super(CachingThreadedResolver, self).__init__(reactor)
-        dnscache.limit = cache_size
+@implementer(IHostnameResolver)
+class CachingHostnameResolver(object):
+
+    def __init__(self, resolver, cache_size, timeout):
+        self.resolver = resolver
         self.timeout = timeout
+        dnscache.limit = cache_size
 
-    def getHostByName(self, name, timeout=None):
-        if name in dnscache:
-            return defer.succeed(dnscache[name])
-        # in Twisted<=16.6, getHostByName() is always called with
-        # a default timeout of 60s (actually passed as (1, 3, 11, 45) tuple),
-        # so the input argument above is simply overridden
-        # to enforce Scrapy's DNS_TIMEOUT setting's value
-        timeout = (self.timeout,)
-        d = super(CachingThreadedResolver, self).getHostByName(name, timeout)
-        if dnscache.limit:
-            d.addCallback(self._cache_result, name)
-        return d
+    def resolveHostName(self, resolutionReceiver, hostName, portNumber=0,
+                        addressTypes=None, transportSemantics='TCP'):
 
-    def _cache_result(self, result, name):
-        dnscache[name] = result
-        return result
+        @provider(IResolutionReceiver)
+        class CachingResolutionReceiver(resolutionReceiver):
+            def resolutionBegan(self, resolution):
+                super(CachingResolutionReceiver, self).resolutionBegan(resolution)
+                self.resolution = resolution
+
+            def resolutionComplete(self):
+                super(CachingResolutionReceiver, self).resolutionComplete()
+                dnscache[hostName] = self.resolution
+
+        try:
+            result = dnscache[hostName]
+        except KeyError:
+            result = self.resolver.resolveHostName(
+                CachingResolutionReceiver(), hostName, portNumber, addressTypes, transportSemantics
+            )
+        finally:
+            return result
