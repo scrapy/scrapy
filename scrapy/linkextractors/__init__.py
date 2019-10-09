877ef4269e5683bf87832e816cf20a3cac352440
akhter wahab <akhter.wahab@gmail.com>
2019-10-09 16:03:44 +0500

    Add .webm to ignored video extensions

--- scrapy/linkextractors/__init__.py
+++ scrapy/linkextractors/__init__.py
@@ -28,7 +28,7 @@ IGNORED_EXTENSIONS = [
 
     # video
     '3gp', 'asf', 'asx', 'avi', 'mov', 'mp4', 'mpg', 'qt', 'rm', 'swf', 'wmv',
-    'm4a', 'm4v', 'flv',
+    'm4a', 'm4v', 'flv', 'webm',
 
     # office suites
     'xls', 'xlsx', 'ppt', 'pptx', 'pps', 'doc', 'docx', 'odt', 'ods', 'odg',
