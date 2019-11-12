8a6a063778d45e8ba68a59558d2930332c1e9a83
Adrián Chaves <adrian@chaves.io>
2019-11-12 10:23:19 +0100

    Allow opening the source code from the API documentation

--- docs/conf.py
+++ docs/conf.py
@@ -31,6 +31,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
