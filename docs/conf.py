048cd74ae594f449ba97d07c927d7640f32a6770
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2019-11-27 19:16:18 -0300

    Add separate mapping for Twisted API docs

--- docs/conf.py
+++ docs/conf.py
@@ -279,7 +279,8 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master', None),
     'tox': ('https://tox.readthedocs.io/en/latest', None),
-    'twisted': ('https://twistedmatrix.com/documents/current/api', None),
+    'twisted': ('https://twistedmatrix.com/documents/current', None),
+    'twistedapi': ('https://twistedmatrix.com/documents/current/api', None),
 }
 
 
