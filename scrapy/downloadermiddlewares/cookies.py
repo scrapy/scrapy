ea0e0562e74bbb30dcafffeee8f10d40e11f873f
Grammy Jiang <grammy.jiang@gmail.com>
2019-12-05 23:42:56 +1100

    Move staticmethods out of CookiesMiddleware class
    This commit moves the staticmethods out of the CookiesMiddleware class:
    * format_cookie
    * get_request_cookies

--- scrapy/downloadermiddlewares/cookies.py
+++ scrapy/downloadermiddlewares/cookies.py
@@ -11,6 +11,32 @@ from scrapy.utils.python import to_unicode
 logger = logging.getLogger(__name__)
 
 
+def format_cookie(cookie):
+    # build cookie string
+    cookie_str = '%s=%s' % (cookie['name'], cookie['value'])
+
+    if cookie.get('path', None):
+        cookie_str += '; Path=%s' % cookie['path']
+    if cookie.get('domain', None):
+        cookie_str += '; Domain=%s' % cookie['domain']
+
+    return cookie_str
+
+
+def get_request_cookies(jar, request):
+    if isinstance(request.cookies, dict):
+        cookie_list = [{'name': k, 'value': v} for k, v in
+                       six.iteritems(request.cookies)]
+    else:
+        cookie_list = request.cookies
+
+    cookies = [format_cookie(x) for x in cookie_list]
+    headers = {'Set-Cookie': cookies}
+    response = Response(request.url, headers=headers)
+
+    return jar.make_cookies(response, request)
+
+
 class CookiesMiddleware(object):
     """This middleware enables working with sites that need cookies"""
 
@@ -30,7 +56,7 @@ class CookiesMiddleware(object):
 
         cookiejarkey = request.meta.get("cookiejar")
         jar = self.jars[cookiejarkey]
-        cookies = self._get_request_cookies(jar, request)
+        cookies = get_request_cookies(jar, request)
         for cookie in cookies:
             jar.set_cookie_if_ok(cookie, request)
 
@@ -68,27 +94,3 @@ class CookiesMiddleware(object):
                 cookies = "\n".join("Set-Cookie: {}\n".format(c) for c in cl)
                 msg = "Received cookies from: {}\n{}".format(response, cookies)
                 logger.debug(msg, extra={'spider': spider})
-
-    def _format_cookie(self, cookie):
-        # build cookie string
-        cookie_str = '%s=%s' % (cookie['name'], cookie['value'])
-
-        if cookie.get('path', None):
-            cookie_str += '; Path=%s' % cookie['path']
-        if cookie.get('domain', None):
-            cookie_str += '; Domain=%s' % cookie['domain']
-
-        return cookie_str
-
-    def _get_request_cookies(self, jar, request):
-        if isinstance(request.cookies, dict):
-            cookie_list = [{'name': k, 'value': v} for k, v in \
-                    six.iteritems(request.cookies)]
-        else:
-            cookie_list = request.cookies
-
-        cookies = [self._format_cookie(x) for x in cookie_list]
-        headers = {'Set-Cookie': cookies}
-        response = Response(request.url, headers=headers)
-
-        return jar.make_cookies(response, request)
