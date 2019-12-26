87ece066ca320b07acda57c99aee8a62992ec144
Andrey Rakhmatullin <wrar@wrar.name>
2019-12-26 20:41:06 +0500

    Remove conditional asyncio imports.

--- scrapy/utils/asyncio.py
+++ scrapy/utils/asyncio.py
@@ -1,25 +1,17 @@
+import asyncio
 from contextlib import suppress
 
+from twisted.internet import asyncioreactor
 from twisted.internet.error import ReactorAlreadyInstalledError
 
 
 def install_asyncio_reactor():
     """ Tries to install AsyncioSelectorReactor
     """
-    try:
-        import asyncio
-        from twisted.internet import asyncioreactor
-    except ImportError:
-        return
-
     with suppress(ReactorAlreadyInstalledError):
         asyncioreactor.install(asyncio.get_event_loop())
 
 
 def is_asyncio_reactor_installed():
-    try:
-        import twisted.internet.reactor
-        from twisted.internet import asyncioreactor
-        return isinstance(twisted.internet.reactor, asyncioreactor.AsyncioSelectorReactor)
-    except ImportError:
-        return False
+    from twisted.internet import reactor
+    return isinstance(reactor, asyncioreactor.AsyncioSelectorReactor)
