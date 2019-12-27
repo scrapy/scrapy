7d0237f2d55c602f7914785982a5221c5d5af38c
1um0s <abitha95@gmail.com>
2019-12-27 11:05:37 +0530

    scrapy#4034 rebase master

--- scrapy/exporters.py
+++ scrapy/exporters.py
@@ -263,7 +263,6 @@ class PickleItemExporter(BaseItemExporter):
 class MarshalItemExporter(BaseItemExporter):
     """Exports items in a Python-specific binary format (see
     :mod:`marshal`).
-
     :param file: The file-like object to use for exporting the data. Its
                  ``write`` method should accept :class:`bytes` (a disk file
                  opened in binary mode, a :class:`~io.BytesIO` object, etc)
@@ -291,10 +290,8 @@ class PprintItemExporter(BaseItemExporter):
 class PythonItemExporter(BaseItemExporter):
     """This is a base class for item exporters that extends
     :class:`BaseItemExporter` with support for nested items.
-
     It serializes items to built-in Python types, so that any serialization
     library (e.g. :mod:`json` or msgpack_) can be used on top of it.
-
     .. _msgpack: https://pypi.org/project/msgpack/
     """
     def _configure(self, options, dont_fail=False):
