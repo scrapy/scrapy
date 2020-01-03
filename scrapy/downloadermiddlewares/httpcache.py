e187cd9f29fe8b428cbf3cfdc5348c517a8629e8
Grammy Jiang <grammy.jiang@gmail.com>
2020-01-03 19:52:50 +1100

    Remove typing hint for the variables

--- scrapy/downloadermiddlewares/httpcache.py
+++ scrapy/downloadermiddlewares/httpcache.py
@@ -43,7 +43,7 @@ class HttpCacheMiddleware(object):
 
     @classmethod
     def from_crawler(cls, crawler: Crawler) -> HttpCacheMiddleware:
-        o: HttpCacheMiddleware = cls(crawler.settings, crawler.stats)
+        o = cls(crawler.settings, crawler.stats)
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         crawler.signals.connect(o.spider_closed, signal=signals.spider_closed)
         return o
