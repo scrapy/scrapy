c544c0d2b8356125d1a5465b44617aaaaeab0ea1
OmarFarrag <omar.alaa.farrag@gmail.com>
2020-01-24 14:36:16 +0200

    Use context management with `FTP`

--- scrapy/utils/ftp.py
+++ scrapy/utils/ftp.py
@@ -22,13 +22,12 @@ def ftp_store_file(
     """Opens a FTP connection with passed credentials,sets current directory
     to the directory extracted from given path, then uploads the file to server
     """
-    ftp = FTP()
-    ftp.connect(host, port)
-    ftp.login(username, password)
-    if use_active_mode:
-        ftp.set_pasv(False)
-    file.seek(0)
-    dirname, filename = posixpath.split(path)
-    ftp_makedirs_cwd(ftp, dirname)
-    ftp.storbinary('STOR %s' % filename, file)
-    ftp.quit()
+    with FTP() as ftp:
+        ftp.connect(host, port)
+        ftp.login(username, password)
+        if use_active_mode:
+            ftp.set_pasv(False)
+        file.seek(0)
+        dirname, filename = posixpath.split(path)
+        ftp_makedirs_cwd(ftp, dirname)
+        ftp.storbinary('STOR %s' % filename, file)
