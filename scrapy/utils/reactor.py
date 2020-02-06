35dafef7f106dbf1c022d997b4e29e3eee84de7c
elacuesta <elacuesta@users.noreply.github.com>
2020-02-06 14:42:34 -0300

    Specify Twisted reactor (TWISTED_REACTOR setting) (#4294)
    * Add the ability to install a specific reactor
    
    * Add docs for the TWISTED_REACTOR setting
    
    * Add tests for the TWISTED_REACTOR setting
    
    * Update asyncio reactor test
    
    * Ignore W503 globally
    
    W503 is not PEP8-compliant:
    https://github.com/python/peps/commit/c59c4376ad233a62ca4b3a6060c81368bd21e85b
    
    * Line length adjustment
    
    * Adjust asyncio reactor tests
    
    * Merge ASYNCIO_ENABLED and TWISTED_REACTOR settings
    
    * More docs about TWISTED_REACTOR
    
    * Fix asyncio reactor test
    
    * Docs: fix title
    
    * Reword docs
    
    * Check the TWISTED_REACTOR setting outside of the installing function
    
    * Remove unrelated change
    
    * Update scrapy/utils/log.py
    
    Co-Authored-By: Adrián Chaves <adrian@chaves.io>
    
    * Update docs/topics/settings.rst
    
    Co-Authored-By: Adrián Chaves <adrian@chaves.io>
    
    * Update docs/topics/settings.rst
    
    Co-Authored-By: Adrián Chaves <adrian@chaves.io>
    
    Co-authored-by: Adrián Chaves <adrian@chaves.io>

--- scrapy/utils/reactor.py
+++ scrapy/utils/reactor.py
@@ -1,4 +1,9 @@
-from twisted.internet import error
+import asyncio
+from contextlib import suppress
+
+from twisted.internet import asyncioreactor, error
+
+from scrapy.utils.misc import load_object
 
 
 def listen_tcp(portrange, host, factory):
@@ -42,3 +47,31 @@ class CallLaterOnce(object):
     def __call__(self):
         self._call = None
         return self._func(*self._a, **self._kw)
+
+
+def install_reactor(reactor_path):
+    reactor_class = load_object(reactor_path)
+    if reactor_class is asyncioreactor.AsyncioSelectorReactor:
+        with suppress(error.ReactorAlreadyInstalledError):
+            asyncioreactor.install(asyncio.get_event_loop())
+    else:
+        *module, _ = reactor_path.split(".")
+        installer_path = module + ["install"]
+        installer = load_object(".".join(installer_path))
+        with suppress(error.ReactorAlreadyInstalledError):
+            installer()
+
+
+def verify_installed_reactor(reactor_path):
+    from twisted.internet import reactor
+    reactor_class = load_object(reactor_path)
+    if not isinstance(reactor, reactor_class):
+        msg = "The installed reactor ({}.{}) does not match the requested one ({})".format(
+            reactor.__module__, reactor.__class__.__name__, reactor_path
+        )
+        raise Exception(msg)
+
+
+def is_asyncio_reactor_installed():
+    from twisted.internet import reactor
+    return isinstance(reactor, asyncioreactor.AsyncioSelectorReactor)
