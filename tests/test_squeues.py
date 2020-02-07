c4b9c8cd568121a2dc5912f57f6514c6bf5282a9
Vostretsov Nikita <whalebot.helmsman@gmail.com>
2020-02-07 06:47:21 +0000

    4 spaces indentation

--- tests/test_squeues.py
+++ tests/test_squeues.py
@@ -2,11 +2,11 @@ import pickle
 
 from queuelib.tests import test_queue as t
 from scrapy.squeues import (
-        MarshalFifoDiskQueueNonRequest as MarshalFifoDiskQueue,
-        MarshalLifoDiskQueueNonRequest as MarshalLifoDiskQueue,
-        PickleFifoDiskQueueNonRequest as PickleFifoDiskQueue,
-        PickleLifoDiskQueueNonRequest as PickleLifoDiskQueue
-        )
+    MarshalFifoDiskQueueNonRequest as MarshalFifoDiskQueue,
+    MarshalLifoDiskQueueNonRequest as MarshalLifoDiskQueue,
+    PickleFifoDiskQueueNonRequest as PickleFifoDiskQueue,
+    PickleLifoDiskQueueNonRequest as PickleLifoDiskQueue
+    )
 from scrapy.item import Item, Field
 from scrapy.http import Request
 from scrapy.loader import ItemLoader
