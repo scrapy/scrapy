61d33e1a818c1ad6fb4df8c91331c22586e52a4c
MaliCN <40772522+MaliYudina@users.noreply.github.com>
2020-02-08 16:39:35 +0300

    updated macOS name

--- scrapy/extensions/memusage.py
+++ scrapy/extensions/memusage.py
@@ -47,7 +47,7 @@ class MemoryUsage(object):
     def get_virtual_size(self):
         size = self.resource.getrusage(self.resource.RUSAGE_SELF).ru_maxrss
         if sys.platform != 'darwin':
-            # on Mac OS X ru_maxrss is in bytes, on Linux it is in KB
+            # on macOS ru_maxrss is in bytes, on Linux it is in KB
             size *= 1024
         return size
 
