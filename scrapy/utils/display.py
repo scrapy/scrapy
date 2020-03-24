825210f005a46226fa24ac6e561ddd5fbcde3303
Akshay Sharma <Akshaysharmajs@gmail.com>
2020-03-25 03:57:17 +0530

    changed orientation, removed unwanted spaces (pr #4403)

--- scrapy/utils/display.py
+++ scrapy/utils/display.py
@@ -8,45 +8,38 @@ from pprint import pformat as pformat_
 
 
 def _colorize(text, colorize=True):
-
-
     if not colorize or not sys.stdout.isatty():
         return text
     try:
-        if sys.platform == "win32" and platform.release() == "10":
-            if platform.version() >= "10.0.14393":
-                import ctypes
-                kernel32 = ctypes.windll.kernel32
-                kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
-
-        colors = color_support_info()
-        style = 'default'
-
-        if colors == 256:
-            format_options = {'style': style}
-        elif style in ('light', 'dark'):
-            format_options = {'bg': style}
-        else:
-            format_options = {'bg': 'dark'}
-        
-        from pygments.formatters import get_formatter_by_name
         import pygments.util
-        
-        format_alias = 'terminal256' if colors == 256 else 'terminal'
-        
-        try:
-            formatter = get_formatter_by_name(format_alias, **format_options)
-        except pygments.util.ClassNotFound as err:
-            sys.stderr.write(str(err) + "\n")
-            formatter = get_formatter_by_name(format_alias)
-        
+        from pygments.formatters import get_formatter_by_name        
         from pygments import highlight
-        from pygments.formatters import TerminalFormatter
-        from pygments.lexers import PythonLexer
-        return highlight(text, PythonLexer(), formatter)
-    
+        from pygments.lexers import PythonLexer    
     except ImportError:
         return text
+
+    if sys.platform == "win32" and platform.release() == "10":
+        if platform.version() >= "10.0.14393":
+            import ctypes
+            kernel32 = ctypes.windll.kernel32
+            kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+    colors = color_support_info()
+    style = 'default'
+    if colors == 256:
+        format_options = {'style': style}
+    elif style in ('light', 'dark'):
+        format_options = {'bg': style}
+    else:
+        format_options = {'bg': 'dark'}    
+    format_alias = 'terminal256' if colors == 256 else 'terminal' 
+    try:
+        formatter = get_formatter_by_name(format_alias, **format_options)
+    except pygments.util.ClassNotFound as err:
+        sys.stderr.write(str(err) + "\n")
+        formatter = get_formatter_by_name(format_alias)
+
+    return highlight(text, PythonLexer(), formatter)    
 
 
 def pformat(obj, *args, **kwargs):
@@ -56,6 +49,7 @@ def pformat(obj, *args, **kwargs):
 def pprint(obj, *args, **kwargs):
     print(pformat(obj, *args, **kwargs))
 
+
 def color_support_info():
     try:
         import curses
@@ -64,3 +58,4 @@ def color_support_info():
         return 16
     curses.setupterm()
     return curses.tigetnum('colors')
+    
