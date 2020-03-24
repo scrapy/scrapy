2d416d778672e16d84dbfb26a771591cccc04d2e
Akshay Sharma <Akshaysharmajs@gmail.com>
2020-03-25 03:13:38 +0530

    added error handling for terminal info (pr #4403)

--- scrapy/utils/display.py
+++ scrapy/utils/display.py
@@ -8,19 +8,44 @@ from pprint import pformat as pformat_
 
 
 def _colorize(text, colorize=True):
-    if sys.platform == "win32" and platform.release() == "10":
-        if platform.version() >= "10.0.14393":
-            import ctypes
-            kernel32 = ctypes.windll.kernel32
-            kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
 
     if not colorize or not sys.stdout.isatty():
         return text
     try:
+        if sys.platform == "win32" and platform.release() == "10":
+            if platform.version() >= "10.0.14393":
+                import ctypes
+                kernel32 = ctypes.windll.kernel32
+                kernel32.SetConsoleMode(kernel32.GetStdHandle(-11), 7)
+
+        colors = color_support_info()
+        style = 'default'
+
+        if colors == 256:
+            format_options = {'style': style}
+        elif style in ('light', 'dark'):
+            format_options = {'bg': style}
+        else:
+            format_options = {'bg': 'dark'}
+        
+        from pygments.formatters import get_formatter_by_name
+        import pygments.util
+        
+        format_alias = 'terminal256' if colors == 256 else 'terminal'
+        
+        try:
+            formatter = get_formatter_by_name(format_alias, **format_options)
+        except pygments.util.ClassNotFound as err:
+            if self.debug:
+                sys.stderr.write(str(err) + "\n")
+            formatter = get_formatter_by_name(format_alias)
+        
         from pygments import highlight
         from pygments.formatters import TerminalFormatter
         from pygments.lexers import PythonLexer
-        return highlight(text, PythonLexer(), TerminalFormatter())
+        return highlight(text, PythonLexer(), formatter)
+    
     except ImportError:
         return text
 
@@ -31,3 +56,12 @@ def pformat(obj, *args, **kwargs):
 
 def pprint(obj, *args, **kwargs):
     print(pformat(obj, *args, **kwargs))
+
+def color_support_info():
+    try:
+        import curses
+    except ImportError:
+        # Usually Windows, which doesn't have great curses support
+        return 16
+    curses.setupterm()
+    return curses.tigetnum('colors')
