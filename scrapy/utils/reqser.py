94c95020b391c3298f4a7fd7608d48d74117bf43
Victor Torres <vpaivatorres@gmail.com>
2020-04-16 11:37:03 -0300

    add comment to explain the use of __func__ instead of instance method objects

--- scrapy/utils/reqser.py
+++ scrapy/utils/reqser.py
@@ -80,6 +80,13 @@ def _find_method(obj, func):
             if func_self is obj:
                 members = inspect.getmembers(obj, predicate=inspect.ismethod)
                 for name, obj_func in members:
+                    # We need to use __func__ to access the original
+                    # function object because instance method objects
+                    # are generated each time attribute is retrieved from
+                    # instance.
+                    #
+                    # Reference: The standard type hierarchy
+                    # https://docs.python.org/3/reference/datamodel.html
                     if obj_func.__func__ is func.__func__:
                         return name
     raise ValueError("Function %s is not a method of: %s" % (func, obj))
