7634b0ff4c1d7eb397b053cc53822f3e8cffa76f
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-04-29 09:18:53 -0300

    Update regex usage in contracts

--- scrapy/contracts/__init__.py
+++ scrapy/contracts/__init__.py
@@ -20,7 +20,7 @@ class ContractsManager:
         is_method = re.compile(r"^\s*@", re.MULTILINE).search
         methods = []
         for key, value in getmembers(spidercls):
-            if callable(value) and value.__doc__ and method_re.search(value.__doc__):
+            if callable(value) and value.__doc__ and is_method(value.__doc__):
                 methods.append(key)
 
         return methods
