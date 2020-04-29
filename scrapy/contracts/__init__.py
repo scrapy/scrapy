a11e8241b7eb6b0725e217db182e4b3e92becc00
Eugenio Lacuesta <1731933+elacuesta@users.noreply.github.com>
2020-04-29 09:17:42 -0300

    Update scrapy/contracts/__init__.py
    Co-Authored-By: Mikhail Korobov <kmike84@gmail.com>

--- scrapy/contracts/__init__.py
+++ scrapy/contracts/__init__.py
@@ -17,7 +17,7 @@ class ContractsManager:
             self.contracts[contract.name] = contract
 
     def tested_methods_from_spidercls(self, spidercls):
-        method_re = re.compile(r"^\s*@", re.MULTILINE)
+        is_method = re.compile(r"^\s*@", re.MULTILINE).search
         methods = []
         for key, value in getmembers(spidercls):
             if callable(value) and value.__doc__ and method_re.search(value.__doc__):
