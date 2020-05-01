f622cdef60746d681bcbab188be5cb2902599b4b
Adrián Chaves <adrian@chaves.io>
2020-05-01 19:07:11 +0200

    Change the minimum Python version from 3.5 to 3.5.1

--- scrapy/squeues.py
+++ scrapy/squeues.py
@@ -82,10 +82,9 @@ def _scrapy_non_serialization_queue(queue_class):
 def _pickle_serialize(obj):
     try:
         return pickle.dumps(obj, protocol=2)
-    # Python <= 3.4 raises pickle.PicklingError here while
     # 3.5 <= Python < 3.6 raises AttributeError and
     # Python >= 3.6 raises TypeError
-    except (pickle.PicklingError, AttributeError, TypeError) as e:
+    except (AttributeError, TypeError) as e:
         raise ValueError(str(e))
 
 
