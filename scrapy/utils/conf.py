4605c66a80dabd64924e397580224a667cd73ec8
Adrián Chaves <adrian@chaves.io>
2020-05-07 12:38:51 +0200

    Fix AttributeError

--- scrapy/utils/conf.py
+++ scrapy/utils/conf.py
@@ -114,7 +114,7 @@ def get_sources(use_closest=True):
 def feed_complete_default_values_from_settings(feed, settings):
     out = feed.copy()
     out.setdefault("encoding", settings["FEED_EXPORT_ENCODING"])
-    out.setdefault("fields", settings.settings.getdictorlist("FEED_EXPORT_FIELDS") or None)
+    out.setdefault("fields", settings.getdictorlist("FEED_EXPORT_FIELDS") or None)
     out.setdefault("store_empty", settings.getbool("FEED_STORE_EMPTY"))
     out.setdefault("uri_params", settings["FEED_URI_PARAMS"])
     if settings["FEED_EXPORT_INDENT"] is None:
