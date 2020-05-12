9806f4fc85a139294b6fa52df9b9c9f75646a062
Júlio César Batista <jcb.1611@gmail.com>
2020-05-12 15:43:25 -0300

    Add docstring to loader/common.py

--- scrapy/loader/common.py
+++ scrapy/loader/common.py
@@ -10,6 +10,9 @@ from scrapy.utils.python import get_func_args
 
 
 def wrap_loader_context(function, context):
+    """Wrap functions that receive loader_context to contain the context
+    "pre-loaded" and expose a interface that receives only one argument
+    """
     warnings.warn(
         "scrapy.loader.common.wrap_loader_context has moved to a new library."
         "Please update your reference to itemloaders.common.wrap_loader_context",
