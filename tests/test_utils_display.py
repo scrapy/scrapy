35812fbc8c6b01153a0ba0914ba4df6c1afc565f
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-05-14 20:55:19 -0300

    Add simple test for scrapy.utils.display._colorize

---
+++ tests/test_utils_display.py
@@ -0,0 +1,20 @@
+from unittest import mock, TestCase
+
+from scrapy.utils.display import _colorize, _color_support_info
+
+
+class TestDisplay(TestCase):
+
+    @mock.patch('sys.stdout.isatty', autospec=True)
+    def test_color(self, mock_isatty):
+        mock_isatty.return_value = True
+        if _color_support_info() == 256:
+            self.assertEqual(
+                _colorize('{"a": 1}'),
+                '{\x1b[38;5;124m"\x1b[39m\x1b[38;5;124ma\x1b[39m\x1b[38;5;124m"\x1b[39m: \x1b[38;5;241m1\x1b[39m}\n'
+            )
+        else:
+            self.assertEquals(
+                _colorize('{"a": 1}'),
+                '{\x1b[33m"\x1b[39;49;00m\x1b[33ma\x1b[39;49;00m\x1b[33m"\x1b[39;49;00m: \x1b[94m1\x1b[39;49;00m}\n'
+            )
