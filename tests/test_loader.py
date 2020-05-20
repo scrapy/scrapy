be3a9fd45891090f500e6dbf0464b4ded1c8aa4c
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-05-20 17:37:15 -0300

    Remove more unnecessary ItemAdapter.items()

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -475,7 +475,7 @@ class InitializationTestMixin:
         il = ItemLoader(item=input_item)
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo']})
 
     def test_keep_list(self):
         """Loaded item should contain values from the initial item"""
@@ -483,7 +483,7 @@ class InitializationTestMixin:
         il = ItemLoader(item=input_item)
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo', 'bar']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo', 'bar']})
 
     def test_add_value_singlevalue_singlevalue(self):
         """Values added after initialization should be appended"""
@@ -492,7 +492,7 @@ class InitializationTestMixin:
         il.add_value('name', 'bar')
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo', 'bar']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo', 'bar']})
 
     def test_add_value_singlevalue_list(self):
         """Values added after initialization should be appended"""
@@ -501,7 +501,7 @@ class InitializationTestMixin:
         il.add_value('name', ['item', 'loader'])
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo', 'item', 'loader']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo', 'item', 'loader']})
 
     def test_add_value_list_singlevalue(self):
         """Values added after initialization should be appended"""
@@ -510,7 +510,7 @@ class InitializationTestMixin:
         il.add_value('name', 'qwerty')
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo', 'bar', 'qwerty']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo', 'bar', 'qwerty']})
 
     def test_add_value_list_list(self):
         """Values added after initialization should be appended"""
@@ -519,7 +519,7 @@ class InitializationTestMixin:
         il.add_value('name', ['item', 'loader'])
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), {'name': ['foo', 'bar', 'item', 'loader']})
+        self.assertEqual(dict(ItemAdapter(loaded_item)), {'name': ['foo', 'bar', 'item', 'loader']})
 
     def test_get_output_value_singlevalue(self):
         """Getting output value must not remove value from item"""
@@ -528,7 +528,7 @@ class InitializationTestMixin:
         self.assertEqual(il.get_output_value('name'), ['foo'])
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), dict({'name': ['foo']}))
+        self.assertEqual(dict(ItemAdapter(loaded_item)), dict({'name': ['foo']}))
 
     def test_get_output_value_list(self):
         """Getting output value must not remove value from item"""
@@ -537,7 +537,7 @@ class InitializationTestMixin:
         self.assertEqual(il.get_output_value('name'), ['foo', 'bar'])
         loaded_item = il.load_item()
         self.assertIsInstance(loaded_item, self.item_class)
-        self.assertEqual(dict(ItemAdapter(loaded_item).items()), dict({'name': ['foo', 'bar']}))
+        self.assertEqual(dict(ItemAdapter(loaded_item)), dict({'name': ['foo', 'bar']}))
 
     def test_values_single(self):
         """Values from initial item must be added to loader._values"""
