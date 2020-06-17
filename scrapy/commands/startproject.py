9e99be982a2916559219693e6665ec7e9319f0e9
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-06-17 15:52:57 -0300

    Remove backslash

--- scrapy/commands/startproject.py
+++ scrapy/commands/startproject.py
@@ -137,6 +137,7 @@ class Command(ScrapyCommand):
 
     @property
     def templates_dir(self):
-        _templates_base_dir = self.settings['TEMPLATES_DIR'] or \
-            join(scrapy.__path__[0], 'templates')
-        return join(_templates_base_dir, 'project')
+        return join(
+            self.settings['TEMPLATES_DIR'] or join(scrapy.__path__[0], 'templates'),
+            'project'
+        )
