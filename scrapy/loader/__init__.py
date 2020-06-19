42b1d75f236c21cc5f1f8e756203af8745a831ff
Júlio César Batista <jcb.1611@gmail.com>
2020-06-19 15:15:03 -0300

    Update itemloaders version

--- scrapy/loader/__init__.py
+++ scrapy/loader/__init__.py
@@ -86,20 +86,3 @@ class ItemLoader(itemloaders.ItemLoader):
             selector = self.default_selector_class(response)
         context.update(response=response)
         super().__init__(item=item, selector=selector, parent=parent, **context)
-
-    def get_default_input_processor_for_field(self, field_name):
-        proc = self._get_item_field_attr(field_name, 'input_processor')
-        if not proc:
-            proc = super().get_default_input_processor_for_field(field_name)
-        return proc
-
-    def get_default_output_processor_for_field(self, field_name):
-        proc = self._get_item_field_attr(field_name, 'output_processor')
-        if not proc:
-            proc = super().get_default_output_processor_for_field(field_name)
-        return proc
-
-    def _get_item_field_attr(self, field_name, key):
-        if isinstance(self.item, Item):
-            return self.item.fields[field_name].get(key)
-        return None
