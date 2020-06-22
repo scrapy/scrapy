d9b1d53ab8310d82f355c0a3907b4b73a6516adc
Akshay Sharma <Akshaysharmajs@gmail.com>
2020-06-22 19:26:52 +0530

    sample changes

--- scrapy/downloadermiddlewares/httpauth.py
+++ scrapy/downloadermiddlewares/httpauth.py
@@ -6,6 +6,7 @@ See documentation in docs/topics/downloader-middleware.rst
 
 from w3lib.http import basic_auth_header
 
+from scrapy.http.response import Response
 from scrapy import signals
 
 
@@ -15,17 +16,28 @@ class HttpAuthMiddleware:
 
     @classmethod
     def from_crawler(cls, crawler):
-        o = cls()
+        o = cls()   
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         return o
 
     def spider_opened(self, spider):
-        usr = getattr(spider, 'http_user', '')
-        pwd = getattr(spider, 'http_pass', '')
-        if usr or pwd:
-            self.auth = basic_auth_header(usr, pwd)
+        pass
+
+    def _authenticated(self, response):
+        if response.status == "401":
+            return False
+
+        return True
 
     def process_request(self, request, spider):
-        auth = getattr(self, 'auth', None)
-        if auth and b'Authorization' not in request.headers:
+        if _authenticated(self, Response(request.url)):
+            if request.url in spider.url_user_pwd.keys():
+                usr = spider.url_user_pwd[request.url][0] #url_user_pwd is a dictionary of urls with their corresponding username, password
+                pwd = spider.url_user_pwd[request.url][1]
+                if usr or pwd:
+                    self.auth = basic_auth_header(usr, pwd)
+
+            auth = getattr(self, 'auth', None)
             request.headers[b'Authorization'] = auth
+
+        # here redirect or retry middleware can be used if request needs authentication
