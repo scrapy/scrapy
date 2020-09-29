e832070d7177bb0f481632a945f132863bcbeeef
Yifei Wu <yiferic@gmail.com>
2020-09-29 17:24:07 +1000

    Remove todo

--- scrapy/pipelines/media.py
+++ scrapy/pipelines/media.py
@@ -165,12 +165,6 @@ class MediaPipeline:
     def _check_media_to_download(self, result, request, info, item):
         if result is not None:
             return result
-        if self.download_func:
-            # this ugly code was left only to support tests. TODO: remove
-            dfd = mustbe_deferred(self.download_func, request, info.spider)
-            dfd.addCallbacks(
-                callback=self.media_downloaded, callbackArgs=(request, info), callbackKeywords={'item': item},
-                errback=self.media_failed, errbackArgs=(request, info))
         else:
             self._modify_media_request(request)
             dfd = self.crawler.engine.download(request, info.spider)
