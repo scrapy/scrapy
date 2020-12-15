b261f52ebfad9f1b99e0c2f9ab9b94494259d1d3
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-12-15 16:54:18 -0300

    Restore minimum cryptography version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def has_environment_marker_platform_impl_support():
 
 install_requires = [
     'Twisted>=17.9.0',
-    'cryptography>=2.1.4',
+    'cryptography>=2.0',
     'cssselect>=0.9.1',
     'itemloaders>=1.0.1',
     'parsel>=1.5.0',
