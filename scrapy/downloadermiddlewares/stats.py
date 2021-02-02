7544ee7d8ae4ecc78f83f45fa12b2b7305ac3ca4
GeorgeA92 <george.zatserklyany@gmail.com>
2021-02-02 09:35:44 +0200

    response_httprepr replaced by response.body

--- scrapy/downloadermiddlewares/stats.py
+++ scrapy/downloadermiddlewares/stats.py
@@ -24,7 +24,7 @@ class DownloaderStats:
     def process_response(self, request, response, spider):
         self.stats.inc_value('downloader/response_count', spider=spider)
         self.stats.inc_value(f'downloader/response_status_count/{response.status}', spider=spider)
-        reslen = len(response_httprepr(response))
+        reslen = len(response.body)
         self.stats.inc_value('downloader/response_bytes', reslen, spider=spider)
         return response
 
