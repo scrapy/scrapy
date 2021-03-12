0f254a6afbc3ad4a42048ea67acafdd035ba690a
Dmitriy Pomazunovskiy <dmitriy.pom0@gmail.com>
2021-03-12 17:11:50 +0600

    Test from_settings

--- tests/test_spidermiddleware_urllength.py
+++ tests/test_spidermiddleware_urllength.py
@@ -6,17 +6,19 @@ from scrapy.spidermiddlewares.urllength import UrlLengthMiddleware
 from scrapy.http import Response, Request
 from scrapy.spiders import Spider
 from scrapy.utils.test import get_crawler
+from scrapy.settings import Settings
 
 
 class TestUrlLengthMiddleware(TestCase):
 
     def setUp(self):
+        self.maxlength = 25
+        settings = Settings({'URLLENGTH_LIMIT': self.maxlength})
+
         crawler = get_crawler(Spider)
         self.spider = crawler._create_spider('foo')
         self.stats = self.spider.crawler.stats
-
-        self.maxlength = 25
-        self.mw = UrlLengthMiddleware(maxlength=self.maxlength)
+        self.mw = UrlLengthMiddleware.from_settings(settings)
 
         self.response = Response('http://scrapytest.org')
         self.short_url_req = Request('http://scrapytest.org/')
