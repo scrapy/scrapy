cb8eafcc4560234c9d06c2997ab7aa4e5fb3f0c3
Amar Hodzic <amar_hodzic@hotmail.com>
2021-03-22 15:15:04 +0100

    fix: Removed conversion from bytes to kilobytes

--- scrapy/extensions/logstats.py
+++ scrapy/extensions/logstats.py
@@ -30,14 +30,14 @@ class LogStats:
     def spider_opened(self, spider):
         self.pagesprev = 0
         self.itemsprev = 0
-        self.responsebytesprev = 0.0
+        self.responsebytesprev = 0
         self.task = task.LoopingCall(self.log, spider)
         self.task.start(self.interval)
 
     def log(self, spider):
         items = self.stats.get_value('item_scraped_count', 0)
         pages = self.stats.get_value('response_received_count', 0)
-        response_bytes = self.stats.get_value('downloader/response_bytes', 0.0) / 1000
+        response_bytes = self.stats.get_value('downloader/response_bytes', 0)
         irate = (items - self.itemsprev) * self.multiplier
         prate = (pages - self.pagesprev) * self.multiplier
         brate = (response_bytes - self.responsebytesprev) * self.multiplier
@@ -45,7 +45,7 @@ class LogStats:
         msg = ("Crawled %(pages)d pages (at %(pagerate)d pages/min), "
                "scraped %(items)d items (at %(itemrate)d items/min)")
         if self.stats.get_value('downloader/response_bytes') is not None:
-            msg = msg + ", received %(response_bytes)d KB of response bytes (at %(brate)d KB/min)"
+            msg = msg + ", received %(response_bytes)d bytes (at %(brate)d bytes/min)"
         log_args = {'pages': pages, 'pagerate': prate,
                     'items': items, 'itemrate': irate,
                     'response_bytes': response_bytes, 'brate': brate}
