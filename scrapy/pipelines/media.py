e0c9def4869acedee0beba36c208d31f99d4438b
Adrián Chaves <adrian@chaves.io>
2021-03-26 15:29:31 +0100

    MediaPipeline: log media_to_download errors before striping them

--- scrapy/pipelines/media.py
+++ scrapy/pipelines/media.py
@@ -112,11 +112,17 @@ class MediaPipeline:
         info.downloading.add(fp)
         dfd = mustbe_deferred(self.media_to_download, request, info, item=item)
         dfd.addCallback(self._check_media_to_download, request, info, item=item)
+        dfd.addErrback(self._log_error, info)
         dfd.addBoth(self._cache_result_and_execute_waiters, fp, info)
-        dfd.addErrback(lambda f: logger.error(
-            f.value, exc_info=failure_to_exc_info(f), extra={'spider': info.spider})
-        )
         return dfd.addBoth(lambda _: wad)  # it must return wad at last
+
+    def _log_error(self, result, info):
+        logger.error(
+            result.value,
+            exc_info=failure_to_exc_info(result),
+            extra={'spider': info.spider}
+        )
+        return result
 
     def _make_compatible(self):
         """Make overridable methods of MediaPipeline and subclasses backwards compatible"""
