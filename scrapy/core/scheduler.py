537ef6893fd15b31e1dd5b76045f2e70eeef2471
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-04-22 12:19:29 -0300

    Update scheduler docstrings

--- scrapy/core/scheduler.py
+++ scrapy/core/scheduler.py
@@ -87,7 +87,12 @@ class BaseScheduler(metaclass=BaseSchedulerMeta):
         """
         Process a request received by the engine.
 
-        Return ``True`` if the request is to be considered scheduled, ``False`` otherwise.
+        Return ``True`` if the request is stored correctly, ``False`` otherwise.
+
+        If ``False``, the engine will fire a ``request_dropped`` signal, and
+        will not make further attempts to schedule the request at a later time.
+        For reference, the default Scrapy scheduler returns ``False`` when the
+        request is rejected by the dupefilter.
         """
         raise NotImplementedError()
 
@@ -96,6 +101,10 @@ class BaseScheduler(metaclass=BaseSchedulerMeta):
         """
         Return the next :class:`~scrapy.http.Request` to be processed, or ``None``
         to indicate that there are no requests to be considered ready at the moment.
+
+        Returning ``None`` implies that no request from the scheduler will be sent
+        to the downloader in the current reactor cycle. The engine will continue
+        calling ``next_request`` until ``has_pending_requests`` is ``False``.
         """
         raise NotImplementedError()
 
