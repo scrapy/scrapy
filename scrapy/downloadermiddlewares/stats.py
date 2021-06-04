3c73cbaaa1fe68767c6c358d42d66931cc25d76d
GeorgeA92 <george.zatserklyany@gmail.com>
2021-06-05 01:08:21 +0300

    response size calculation updated

--- scrapy/downloadermiddlewares/stats.py
+++ scrapy/downloadermiddlewares/stats.py
@@ -34,7 +34,7 @@ class DownloaderStats:
     def process_response(self, request, response, spider):
         self.stats.inc_value('downloader/response_count', spider=spider)
         self.stats.inc_value(f'downloader/response_status_count/{response.status}', spider=spider)
-        reslen = len(response.body)
+        reslen = len(response.body) + get_header_size(response.headers)
         self.stats.inc_value('downloader/response_bytes', reslen, spider=spider)
         return response
 
