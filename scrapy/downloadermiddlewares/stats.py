77071088e8abbb8a38b573eaf84e5de5ec61b0fb
GeorgeA92 <george.zatserklyany@gmail.com>
2021-08-13 13:36:24 +0300

    added counting status code, line breaks to response size

--- scrapy/downloadermiddlewares/stats.py
+++ scrapy/downloadermiddlewares/stats.py
@@ -1,6 +1,8 @@
 from scrapy.exceptions import NotConfigured
-from scrapy.utils.python import global_object_name
+from scrapy.utils.python import global_object_name, to_bytes
 from scrapy.utils.request import request_httprepr
+
+from twisted.web import http
 
 
 def get_header_size(headers):
@@ -14,6 +16,17 @@ def get_header_size(headers):
             for v in value:
                 size += len(b": ") + len(key) + len(v)
     return size + len(b'\r\n') * (len(headers.keys()) - 1)
+
+
+def get_status_size(response_status):
+    values = [
+        9,
+        len(to_bytes(str(response_status))),
+        1,
+        len(to_bytes(http.RESPONSES.get(response_status, b''))),
+        2,
+    ]
+    return sum(values)
 
 
 class DownloaderStats:
@@ -36,7 +49,7 @@ class DownloaderStats:
     def process_response(self, request, response, spider):
         self.stats.inc_value('downloader/response_count', spider=spider)
         self.stats.inc_value(f'downloader/response_status_count/{response.status}', spider=spider)
-        reslen = len(response.body) + get_header_size(response.headers)
+        reslen = len(response.body) + get_header_size(response.headers) + get_status_size(response.status) + 4
         self.stats.inc_value('downloader/response_bytes', reslen, spider=spider)
         return response
 
