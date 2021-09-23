7f2990eaf8a9deceaa88ff0c7220be9a3ce3ddb5
GeorgeA92 <george.zatserklyany@gmail.com>
2021-09-24 00:43:12 +0300

    scrapy.extensions.feedexport: logger message formatting fixed

--- scrapy/extensions/feedexport.py
+++ scrapy/extensions/feedexport.py
@@ -469,7 +469,7 @@ class FeedExporter:
         for uri_template, values in self.feeds.items():
             if values['batch_item_count'] and not re.search(r'%\(batch_time\)s|%\(batch_id\)', uri_template):
                 logger.error(
-                    '%(batch_time)s or %(batch_id)d must be in the feed URI ({%s}) if FEED_EXPORT_BATCH_ITEM_COUNT '
+                    '%%(batch_time)s or %%(batch_id)d must be in the feed URI (%s) if FEED_EXPORT_BATCH_ITEM_COUNT '
                     'setting or FEEDS.batch_item_count is specified and greater than 0. For more info see: '
                     'https://docs.scrapy.org/en/latest/topics/feed-exports.html#feed-export-batch-item-count',
                     uri_template
