915bd9cca933f3050250186b0479c9c71e475751
GeorgeA92 <george.zatserklyany@gmail.com>
2021-10-01 18:55:45 +0300

    comment added

--- scrapy/downloadermiddlewares/stats.py
+++ scrapy/downloadermiddlewares/stats.py
@@ -40,6 +40,7 @@ class DownloaderStats:
         self.stats.inc_value('downloader/response_count', spider=spider)
         self.stats.inc_value(f'downloader/response_status_count/{response.status}', spider=spider)
         reslen = len(response.body) + get_header_size(response.headers) + get_status_size(response.status) + 4
+        # response.body + b"\r\n"+ response.header + b"\r\n" + response.status
         self.stats.inc_value('downloader/response_bytes', reslen, spider=spider)
         return response
 
