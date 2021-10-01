d3be17b87f29d91f402884bf4bf96da8af93d225
GeorgeA92 <george.zatserklyany@gmail.com>
2021-10-01 19:18:58 +0300

    tests.test_http_request: typo fixed

--- tests/test_http_request.py
+++ tests/test_http_request.py
@@ -1218,7 +1218,7 @@ class FormRequestTest(RequestTest):
 
     def test_from_response_valid_form_methods(self):
         form_methods = [[method, method] for method in self.request_class.valid_form_methods]
-        form_methods.append([['UNKNOWN', 'GET']])
+        form_methods.append(['UNKNOWN', 'GET'])
 
         for method, expected in form_methods:
             response = _buildresponse(
