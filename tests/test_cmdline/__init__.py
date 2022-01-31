537360108b67de6f304efe0a4c1b6fcb73c2ec5f
Laerte <5853172+Laerte@users.noreply.github.com>
2022-01-31 12:44:39 -0300

    Fix lint

--- tests/test_cmdline/__init__.py
+++ tests/test_cmdline/__init__.py
@@ -66,4 +66,5 @@ class CmdlineTest(unittest.TestCase):
         self.assertEqual(200, settingsdict[EXT_PATH])
 
     def test_pathlib_path_as_feeds_key(self):
-        self.assertEqual(self._execute('settings', '--get', 'FEEDS'), json.dumps({"items.csv": {"format": "csv", "fields": ["price", "name"]}}))
+        self.assertEqual(self._execute('settings', '--get', 'FEEDS'),
+                         json.dumps({"items.csv": {"format": "csv", "fields": ["price", "name"]}}))
