86ef6d5a1a58af52fd5f0ed3c765e94c27af4445
Laerte Pereira <laertefbk@gmail.com>
2022-06-03 06:59:18 -0300

    chore: Drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -84,7 +84,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
@@ -95,7 +94,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Application Frameworks',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=install_requires,
     extras_require=extras_require,
 )
