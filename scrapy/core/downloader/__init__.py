12d52a4f089798f266cbc6f86df3cc9c1cd58257
GeorgeA92 <george.zatserklyany@gmail.com>
2022-11-18 14:16:18 +0200

    per slot settings: code optimized

--- scrapy/core/downloader/__init__.py
+++ scrapy/core/downloader/__init__.py
@@ -100,12 +100,13 @@ class Downloader:
     def _get_slot(self, request, spider):
         key = self._get_slot_key(request, spider)
         if key not in self.slots:
-            conc = self.per_slot_settings.get(key, {}).get(
+            slot_settings = self.per_slot_settings.get(key, {})
+            conc = slot_settings.get(
                 'concurrency', self.ip_concurrency if self.ip_concurrency else self.domain_concurrency
             )
             conc, delay = _get_concurrency_delay(conc, spider, self.settings)
-            delay = self.per_slot_settings.get(key, {}).get('delay', delay)
-            randomize_delay = self.per_slot_settings.get(key, {}).get('randomize_delay', self.randomize_delay)
+            delay = slot_settings.get('delay', delay)
+            randomize_delay = slot_settings.get('randomize_delay', self.randomize_delay)
             new_slot = Slot(conc, delay, randomize_delay)
             self.slots[key] = new_slot
 
