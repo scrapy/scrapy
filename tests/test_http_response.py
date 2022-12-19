a26b6b0607026e4ea266ed3e30d2643d5990bf8a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-12-18 20:26:13 -0500

    spelling: indistinct
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/test_http_response.py
+++ tests/test_http_response.py
@@ -425,7 +425,7 @@ class TextResponseTest(BaseResponseTest):
     def test_bom_is_removed_from_body(self):
         # Inferring encoding from body also cache decoded body as sideeffect,
         # this test tries to ensure that calling response.encoding and
-        # response.text in indistint order doesn't affect final
+        # response.text in indistinct order doesn't affect final
         # values for encoding and decoded body.
         url = 'http://example.com'
         body = b"\xef\xbb\xbfWORD"
