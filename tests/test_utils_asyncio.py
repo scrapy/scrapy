67e95ac1b1db6ca1c25e6d64b435ff13bea71ec4
Adrián Chaves <adrian@chaves.io>
2023-01-02 16:38:48 +0100

    test_install_asyncio_reactor: verify non-action if a reactor has been already installed

--- tests/test_utils_asyncio.py
+++ tests/test_utils_asyncio.py
@@ -14,14 +14,8 @@ class AsyncioTest(TestCase):
         self.assertEqual(is_asyncio_reactor_installed(), self.reactor_pytest == 'asyncio')
 
     def test_install_asyncio_reactor(self):
+        from twisted.internet import reactor as original_reactor
+        with warnings.catch_warnings(record=True) as w:
+            install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
         from twisted.internet import reactor
-        original_class = type(reactor)
-        original_import_path = f"{original_class.__module__}.{original_class.__qualname__}"
-        asyncio_import_path = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-        try:
-            with warnings.catch_warnings(record=True) as w:
-                install_reactor(asyncio_import_path)
-                self.assertEqual(len(w), 0)
-        finally:
-            if original_import_path != asyncio_import_path:
-                install_reactor(original_import_path)
+        assert original_reactor == reactor
