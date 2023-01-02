4e365d6a7e210368233cc78616533878ad357cbb
Adrián Chaves <adrian@chaves.io>
2023-01-02 15:18:14 +0100

    test_install_asyncio_reactor: restore the original reactor when done

--- tests/test_utils_asyncio.py
+++ tests/test_utils_asyncio.py
@@ -14,6 +14,14 @@ class AsyncioTest(TestCase):
         self.assertEqual(is_asyncio_reactor_installed(), self.reactor_pytest == 'asyncio')
 
     def test_install_asyncio_reactor(self):
-        with warnings.catch_warnings(record=True) as w:
-            install_reactor("twisted.internet.asyncioreactor.AsyncioSelectorReactor")
-            self.assertEqual(len(w), 0)
+        from twisted.internet import reactor
+        original_class = type(reactor)
+        original_import_path = f"{original_class.__module__}.{original_class.__qualname__}"
+        asyncio_import_path = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+        try:
+            with warnings.catch_warnings(record=True) as w:
+                install_reactor(asyncio_import_path)
+                self.assertEqual(len(w), 0)
+        finally:
+            if original_import_path != asyncio_import_path:
+                install_reactor(original_import_path)
