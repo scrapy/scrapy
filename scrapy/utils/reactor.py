66417d85d02e5ff4ae95a98d9c283dfd70f956a9
Adrián Chaves <adrian@chaves.io>
2023-01-05 20:38:24 +0100

    Clarify things further

--- scrapy/utils/reactor.py
+++ scrapy/utils/reactor.py
@@ -96,8 +96,12 @@ def set_asyncio_event_loop(event_loop_path):
     else:
         try:
             with catch_warnings():
-                # Prevent logging a DeprecationWarning due to a known issue
-                # https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.get_event_loop
+                # In Python 3.10.9, 3.11.1, 3.12 and 3.13, a DeprecationWarning
+                # is emitted about the lack of a current event loop, because in
+                # Python 3.14 and later `get_event_loop` will raise a
+                # RuntimeError in that event. Because our code is already
+                # prepared for that future behavior, we ignore the deprecation
+                # warning.
                 filterwarnings(
                     "ignore",
                     message="There is no current event loop",
@@ -105,9 +109,12 @@ def set_asyncio_event_loop(event_loop_path):
                 )
                 event_loop = policy.get_event_loop()
         except RuntimeError:
-            # `get_event_loop` is expected to fail when called from a new thread
-            # with no asyncio event loop yet installed. Such is the case when
-            # called from `scrapy shell`
+            # `get_event_loop` raises RuntimeError when called with no asyncio
+            # event loop yet installed in the following scenarios:
+            # - From a thread other than the main thread. For example, when
+            #   using ``scrapy shell``.
+            # - Previsibly on Python 3.14 and later.
+            #   https://github.com/python/cpython/issues/100160#issuecomment-1345581902
             event_loop = policy.new_event_loop()
             asyncio.set_event_loop(event_loop)
     return event_loop
