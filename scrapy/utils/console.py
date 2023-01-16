a58bf02fa97c3065f97d2d79303339ccba39497a
Serhii Abarovskyi <aserhii@protonmail.com>
2023-01-16 17:25:03 +0200

    change to {}

--- scrapy/utils/console.py
+++ scrapy/utils/console.py
@@ -62,12 +62,12 @@ def _embed_standard_shell(namespace={}, banner=''):
     return wrapper
 
 
-DEFAULT_PYTHON_SHELLS = dict(
-    ptpython=_embed_ptpython_shell,
-    ipython=_embed_ipython_shell,
-    bpython=_embed_bpython_shell,
-    python=_embed_standard_shell,
-)
+DEFAULT_PYTHON_SHELLS = {
+    'ptpython': _embed_ptpython_shell,
+    'ipython': _embed_ipython_shell,
+    'bpython': _embed_bpython_shell,
+    'python': _embed_standard_shell,
+}
 
 
 def get_shell_embed_func(shells=None, known_shells=None):
