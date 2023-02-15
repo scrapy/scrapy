60bf56b715e443951125a10ff91ad1699270a82c
Yegor <yahor.statkevich@godeltech.com>
2023-02-15 12:15:24 +0100

    Add boto3 availability util method

--- scrapy/utils/boto.py
+++ scrapy/utils/boto.py
@@ -8,3 +8,12 @@ def is_botocore_available():
         return True
     except ImportError:
         return False
+
+
+def is_boto3_available():
+    try:
+        import boto3  # noqa: F401
+
+        return True
+    except ImportError:
+        return False
