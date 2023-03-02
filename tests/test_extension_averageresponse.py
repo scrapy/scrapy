655db5a41021d5cff143e28853e08e8e26a87398
hansstammler <stammlerhans@gmail.com>
2023-03-02 10:59:35 +0100

    feat/#5: Added test file
    Added test file that runs autmatically as part of test suite with basic assertion to check that it works

---
+++ tests/test_extension_averageresponse.py
@@ -0,0 +1,7 @@
+from twisted.trial import unittest
+
+class AverageResponseTestClass(unittest.TestCase):
+    
+    #test function t osee if this runs
+    def test_crawler_itemerror(self):
+        self.assertEqual(True, True)
