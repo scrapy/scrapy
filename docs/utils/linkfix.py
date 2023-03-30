0a0e3da808ad3d39e282c7e286491fefc26985d3
Will Jessup <will@willjessup.com>
2023-03-30 19:09:00 -0400

    After refactoring, the code should look like this:

--- docs/utils/linkfix.py
+++ docs/utils/linkfix.py
@@ -1,202 +1,200 @@
 """
-Images Pipeline
+Mail sending helpers
 
-See documentation in topics/media-pipeline.rst
+See documentation in docs/topics/email.rst
 """
-from hashlib import sha1
-from io import BytesIO
-import warnings
 
-from PIL import Image
-from itemadapter import ItemAdapter
-from scrapy.exceptions import DropItem, NotConfigured, ScrapyDeprecationWarning
-from scrapy.http import Request
-from scrapy.http.request import NO_CALLBACK
-from scrapy.pipelines.files import FileException, FilesPipeline
-from scrapy.settings import Settings
-from scrapy.utils.misc import md5sum
+import logging
+
+from email import encoders as Encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+from io import BytesIO
+
+from twisted.internet import defer, reactor, ssl
+from twisted.mail.smtp import ESMTPSenderFactory
+from twisted.python.versions import Version
+
+from scrapy.utils.misc import arg_to_iter
 from scrapy.utils.python import to_bytes
 
 
-class NoImagesDrop(DropItem):
-    """Raise this exception if the product doesn't have images"""
-
-    def __init__(self, *args, **kwargs):
-        warnings.warn(
-            "The NoImagesDrop class is deprecated",
-            category=ScrapyDeprecationWarning,
-            stacklevel=2,
-        )
-        super().__init__(*args, **kwargs)
+logger = logging.getLogger(__name__)
 
 
-class ImagesPipeline(FilesPipeline):
-    """
-    Abstract pipeline that implements the image thumbnail generation logic
-    """
-
-    # Uppercase attributes kept for backward compatibility with code that subclasses
-    # ImagesPipeline. They may be overridden by settings.
-    MIN_WIDTH = 0
-    MIN_HEIGHT = 0
-    EXPIRES = 90
-    DEFAULT_IMAGES_URLS_FIELD = "image_urls"
-    DEFAULT_IMAGES_RESULT_FIELD = "images"
-    THUMBS = {}
-
-    MEDIA_NAME = "image"
-
-    def __init__(self, store_uri, download_func=None, settings=None):
-        super().__init__(store_uri, settings=settings, download_func=download_func)
-
-        if isinstance(settings, dict) or settings is None:
-            settings = Settings(settings)
-
-        self.expires = settings.getint("IMAGES_EXPIRES", self.EXPIRES)
-        self.images_urls_field = settings.get("IMAGES_URLS_FIELD",
-            self.DEFAULT_IMAGES_URLS_FIELD)
-        self.images_result_field = settings.get("IMAGES_RESULT_FIELD",
-            self.DEFAULT_IMAGES_RESULT_FIELD)
-        self.min_width = settings.getint("IMAGES_MIN_WIDTH",
-            self.MIN_WIDTH)
-        self.min_height = settings.getint("IMAGES_MIN_HEIGHT",
-            self.MIN_HEIGHT)
-        self.thumbs = settings.get("IMAGES_THUMBS", self.THUMBS)
-
-        self._deprecated_convert_image = None
+class MailSender:
+    def __init__(
+        self,
+        smtphost="localhost",
+        mailfrom="scrapy@localhost",
+        smtpuser=None,
+        smtppass=None,
+        smtpport=25,
+        smtptls=False,
+        smtpssl=False,
+        debug=False,
+    ):
+        self.smtphost = smtphost
+        self.smtpport = smtpport
+        self.smtpuser = to_bytes(smtpuser) if smtpuser else None
+        self.smtppass = to_bytes(smtppass) if smtppass else None
+        self.smtptls = smtptls
+        self.smtpssl = smtpssl
+        self.mailfrom = mailfrom
+        self.debug = debug
 
     @classmethod
     def from_settings(cls, settings):
-        s3store = cls.STORE_SCHEMES["s3"]
-        s3store.AWS_ACCESS_KEY_ID = settings["AWS_ACCESS_KEY_ID"]
-        s3store.AWS_SECRET_ACCESS_KEY = settings["AWS_SECRET_ACCESS_KEY"]
-        s3store.AWS_SESSION_TOKEN = settings["AWS_SESSION_TOKEN"]
-        s3store.AWS_ENDPOINT_URL = settings["AWS_ENDPOINT_URL"]
-        s3store.AWS_REGION_NAME = settings["AWS_REGION_NAME"]
-        s3store.AWS_USE_SSL = settings["AWS_USE_SSL"]
-        s3store.AWS_VERIFY = settings["AWS_VERIFY"]
-        s3store.POLICY = settings["IMAGES_STORE_S3_ACL"]
+        return cls(
+            smtphost=settings["MAIL_HOST"],
+            mailfrom=settings["MAIL_FROM"],
+            smtpuser=settings["MAIL_USER"],
+            smtppass=settings["MAIL_PASS"],
+            smtpport=settings.getint("MAIL_PORT"),
+            smtptls=settings.getbool("MAIL_TLS"),
+            smtpssl=settings.getbool("MAIL_SSL"),
+        )
 
-        gcs_store = cls.STORE_SCHEMES["gs"]
-        gcs_store.GCS_PROJECT_ID = settings["GCS_PROJECT_ID"]
-        gcs_store.POLICY = settings.get("IMAGES_STORE_GCS_ACL", None)
+    def send(
+        self,
+        to,
+        subject,
+        body,
+        cc=None,
+        attachs=(),
+        mimetype="text/plain",
+        charset=None,
+        _callback=None,
+    ):
 
-        ftp_store = cls.STORE_SCHEMES["ftp"]
-        ftp_store.FTP_USERNAME = settings["FTP_USER"]
-        ftp_store.FTP_PASSWORD = settings["FTP_PASSWORD"]
-        ftp_store.USE_ACTIVE_MODE = settings.getbool("FEED_STORAGE_FTP_ACTIVE")
-
-        store_uri = settings["IMAGES_STORE"]
-        return cls(store_uri, settings=settings)
-
-    def file_downloaded(self, response, request, info, *, item=None):
-        return self.image_downloaded(response, request, info, item=item)
-
-    def image_downloaded(self, response, request, info, *, item=None):
-        checksum = None
-        for path, image, buf in self.get_images(response, request, info, item=item):
-            if checksum is None:
-                buf.seek(0)
-                checksum = md5sum(buf)
-            width, height = image.size
-            self.store.persist_file(
-                path,
-                buf,
-                info,
-                meta={"width": width, "height": height},
-                headers={"Content-Type": "image/jpeg"},
-            )
-        return checksum
-
-    def get_images(self, response, request, info, *, item=None):
-        path = self.file_path(request, response=response, info=info, item=item)
-        orig_image = Image.open(BytesIO(response.body))
-
-        width, height = orig_image.size
-        if width < self.min_width or height < self.min_height:
-            raise FileException(
-                f"Image too small ({width}x{height} < "
-                f"{self.min_width}x{self.min_height})"
-            )
-
-        if self._deprecated_convert_image is None:
-            self._deprecated_convert_image = "response_body" not in get_func_args(
-                self.convert_image
-            )
-            if self._deprecated_convert_image:
-                warnings.warn(
-                    f"{self.__class__.__name__}.convert_image() method overridden in a deprecated way, "
-                    "overridden method does not accept response_body argument.",
-                    category=ScrapyDeprecationWarning,
-                )
-
-        if self._deprecated_convert_image:
-            image, buf = self.convert_image(orig_image)
+        if attachs:
+            msg = MIMEMultipart()
         else:
-            image, buf = self.convert_image(
-                orig_image, response_body=BytesIO(response.body)
+            msg = MIMEText(body, mimetype, charset)
+
+        to = arg_to_iter(to)
+        cc = arg_to_iter(cc)
+
+        msg["From"] = self.mailfrom
+        msg["To"] = COMMASPACE.join(to)
+        msg["Date"] = formatdate(localtime=True)
+        msg["Subject"] = subject
+
+        rcpts = list(to)
+        if cc:
+            rcpts.extend(list(cc))
+            msg["Cc"] = COMMASPACE.join(cc)
+
+        if charset:
+            msg.set_charset(charset)
+
+        if attachs:
+            msg.attach(MIMEText(body, mimetype, charset))
+            for attachment in attachs:
+                attach_name, mimetype, f = attachment
+                part = MIMEBase(*mimetype.split("/"))
+                part.set_payload(f.read())
+                Encoders.encode_base64(part)
+                part.add_header(
+                    "Content-Disposition", "attachment", filename=attach_name
+                )
+                msg.attach(part)
+
+        if _callback:
+            _callback(to=to, subject=subject, body=body, cc=cc, attach=attachs, msg=msg)
+
+        if self.debug:
+            logger.debug(
+                "Debug mail sent OK: To=%(mailto)s Cc=%(mailcc)s "
+                'Subject="%(mailsubject)s" Attachs=%(mailattachs)d',
+                {
+                    "mailto": to,
+                    "mailcc": cc,
+                    "mailsubject": subject,
+                    "mailattachs": len(attachs),
+                },
             )
-        yield path, image, buf
+            return
 
-        for thumb_id, size in self.thumbs.items():
-            thumb_path = self.thumb_path(
-                request, thumb_id, response=response, info=info, item=item
-            )
-            if self._deprecated_convert_image:
-                thumb_image, thumb_buf = self.convert_image(image, size)
-            else:
-                thumb_image, thumb_buf = self.convert_image(image, size, buf)
-            yield thumb_path, thumb_image, thumb_buf
+        d = self._sendmail(rcpts, msg.as_string().encode(charset or "utf-8"))
+        d.addCallbacks(
+            callback=self._sent_ok,
+            errback=self._sent_failed,
+            callbackArgs=[to, cc, subject, len(attachs)],
+            errbackArgs=[to, cc, subject, len(attachs)],
+        )
+        reactor.addSystemEventTrigger("before", "shutdown", lambda: d)
+        return d
 
-    def convert_image(self, image, size=None, response_body=None):
-        if response_body is None:
-            warnings.warn(
-                f"{self.__class__.__name__}.convert_image() method called in a deprecated way, "
-                "method called without response_body argument.",
-                category=ScrapyDeprecationWarning,
-                stacklevel=2,
-            )
+    def _sent_ok(self, result, to, cc, subject, nattachs):
+        logger.info(
+            "Mail sent OK: To=%(mailto)s Cc=%(mailcc)s "
+            'Subject="%(mailsubject)s" Attachs=%(mailattachs)d',
+            {
+                "mailto": to,
+                "mailcc": cc,
+                "mailsubject": subject,
+                "mailattachs": nattachs,
+            },
+        )
 
-        if image.format in ("PNG", "WEBP") and image.mode == "RGBA":
-            background = Image.new("RGBA", image.size, (255, 255, 255))
-            background.paste(image, image)
-            image = background.convert("RGB")
-        elif image.mode == "P":
-            image = image.convert("RGBA")
-            background = Image.new("RGBA", image.size, (255, 255, 255))
-            background.paste(image, image)
-            image = background.convert("RGB")
-        elif image.mode != "RGB":
-            image = image.convert("RGB")
+    def _sent_failed(self, failure, to, cc, subject, nattachs):
+        errstr = str(failure.value)
+        logger.error(
+            "Unable to send mail: To=%(mailto)s Cc=%(mailcc)s "
+            'Subject="%(mailsubject)s" Attachs=%(mailattachs)d'
+            "- %(mailerr)s",
+            {
+                "mailto": to,
+                "mailcc": cc,
+                "mailsubject": subject,
+                "mailattachs": nattachs,
+                "mailerr": errstr,
+            },
+        )
 
-        if size:
-            image = image.copy()
-            try:
-                resampling_filter = Image.Resampling.LANCZOS
-            except AttributeError:
-                resampling_filter = Image.ANTIALIAS
-            image.thumbnail(size, resampling_filter)
-        elif response_body is not None and image.format == "JPEG":
-            return image, response_body
+    def _sendmail(self, to_addrs, msg):
+        d = defer.Deferred()
 
-        buf = BytesIO()
-        image.save(buf, "JPEG")
-        return image, buf
+        factory = ESMTPSenderFactory(
+            self.smtphost,
+            self.smtpuser,
+            to_addrs,
+            msg,
+            self.smtppass,
+            self.smtpport,
+            contextFactory=(ssl.ClientContextFactory() if self.smtpssl else None),
+            requireAuthentication=False,
+            requireTransportSecurity=self.smtptls,
+            heloFallback=True,
+            hostname=self.smtphost if Version("twisted", 21, 2, 0) <= twisted_version else None,
+        )
 
-    def get_media_requests(self, item, info):
-        urls = ItemAdapter(item).get(self.images_urls_field, [])
-        return [Request(u, callback=NO_CALLBACK) for u in urls]
+        factory.protocol = lambda: CustomSMTPClient(factory)
+        reactor.connectTCP(self.smtphost, self.smtpport, factory)
 
-    def item_completed(self, results, item, info):
-        with warnings.catch_warnings():
-            warnings.simplefilter("ignore")
-            ItemAdapter(item)[self.images_result_field] = [x for ok, x in results if ok]
-        return item
+        return d
 
-    def file_path(self, request, response=None, info=None, *, item=None):
-        image_guid = sha1(to_bytes(request.url)).hexdigest()
-        return f"full/{image_guid}.jpg"
 
-    def thumb_path(self, request, thumb_id, response=None, info=None, *, item=None):
-        thumb_guid = sha1(to_bytes(request.url)).hexdigest()
-        return f"thumbs/{thumb_id}/{thumb_guid}.jpg"
+class CustomSMTPClient(object):
+    """
+    Custom SMTP client class to handle a message sent event.
+    """
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def send_message(self, from_addr, to_addrs, msg):
+        self.factory.resetDelay()
+        d = self.factory.getMailSender(to_addrs, from_addr, msg)
+        d.addCallbacks(self._message_sent, self._message_failed)
+
+    def _message_sent(self, response):
+        self.factory.protocol = lambda: CustomSMTPClient(self.factory)
+        self.factory.deferred.callback(response)
+
+    def _message_failed(self, err):
+        self.factory.protocol = lambda: CustomSMTPClient(self.factory)
+        self.factory.deferred.errback(err)
