a042f0602f0db2458552ea72607a16dfd474628d
guillermo-bondonno <guillermo.bondonno@zyte.com>
2023-04-12 19:24:52 -0300

    use f-strings in utils.request.request_to_curl

--- scrapy/utils/request.py
+++ scrapy/utils/request.py
@@ -337,26 +337,24 @@ def request_to_curl(request: Request) -> str:
     :return: string containing the curl command
     """
     method = request.method
-    data = (
-        "--data-raw '{}'".format(request.body.decode("utf-8")) if request.body else ""
-    )
+
+    data = f"--data-raw '{request.body.decode('utf-8')}'" if request.body else ""
+
     headers = " ".join(
-        "-H '{}: {}'".format(k.decode(), v[0].decode())
-        for k, v in request.headers.items()
+        f"-H '{k.decode()}: {v[0].decode()}'" for k, v in request.headers.items()
     )
+
     url = request.url
     cookies = ""
     if request.cookies:
         if isinstance(request.cookies, dict):
-            cookies = "--cookie '{}'".format(
-                "; ".join("{}={}".format(k, v) for k, v in request.cookies.items())
-            )
+            cookie = "; ".join(f"{k}={v}" for k, v in request.cookies.items())
+            cookies = f"--cookie '{cookie}'"
         elif isinstance(request.cookies, list):
-            cookies = "--cookie '{}'".format(
-                "; ".join(
-                    "{}={}".format(list(c.keys())[0], list(c.values())[0])
-                    for c in request.cookies
-                )
+            cookie = "; ".join(
+                f"{list(c.keys())[0]}={list(c.values())[0]}" for c in request.cookies
             )
+            cookies = f"--cookie '{cookie}'"
+
     curl_cmd = f"curl -X {method} {url} {data} {headers} {cookies}".strip()
     return " ".join(curl_cmd.split())
