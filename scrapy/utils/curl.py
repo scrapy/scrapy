b7ecec18099ace6ba77161302cca85c3e58a2ae7
Jalil SA <ssergio.jalil@gmail.com>
2023-04-19 01:04:03 -0600

    Update scrapy/utils/curl.py
    Co-authored-by: Adrián Chaves <adrian@chaves.io>

--- scrapy/utils/curl.py
+++ scrapy/utils/curl.py
@@ -10,8 +10,9 @@ from w3lib.http import basic_auth_header
 
 class DataAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
-        value = str(values).encode("utf-8").decode("utf-8")
-        value = value[1::] if re.match(r"^\$(.+)", value) else value
+        value = str(values)
+        if value.startswith("$"):
+            value = value[1:]
         setattr(namespace, self.dest, value)
 
 
