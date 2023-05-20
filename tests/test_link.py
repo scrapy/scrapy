8e3b1a2f36cba9c9345d1714afc3bbdf8e21a1ae
Konstantinos Drosos <dean.drossos@gmail.com>
2023-05-20 14:09:18 -0400

    Improved commenting in the test_link file for testing the Link instances

--- tests/test_link.py
+++ tests/test_link.py
@@ -13,38 +13,72 @@ class LinkTest(unittest.TestCase):
         self.assertNotEqual(hash(link1), hash(link2))
 
     def test_eq_and_hash_1(self):
+        """
+        Tests if two instances of Link with the 
+        same url recognize they have the same url
+        """
         l1 = Link("http://www.example.com")
 
         self._assert_same_links(l1, l1)
 
     def test_eq_and_hash_2(self):
+        """
+        Tests if two instances of Link with different 
+        url address recognize they are different even 
+        if the initial part is the same.
+        """
         l1 = Link("http://www.example.com")
         l2 = Link("http://www.example.com/other")
 
         self._assert_different_links(l1, l2)
 
     def test_eq_and_hash_3(self):
+        """
+        Checks if two instances of Link successufully 
+        capture the address they are point two if it is 
+        the same address. 
+        """
         l1 = Link("http://www.example.com")
         l3 = Link("http://www.example.com")
 
         self._assert_same_links(l1, l3)
 
     def test_eq_and_hash_4(self):
+        """
+        Tests if an instance of Link successfully recognizes
+        it points to the same url if it is compared with itself
+        """
         l4 = Link("http://www.example.com", text="test")
         self._assert_same_links(l4, l4)
 
     def test_eq_and_hash_5(self):
+        """
+        Tests if two instances of Link that point to the same url 
+        are not evaluating that they point to the same url if they
+        have different text variable values.
+        """
         l4 = Link("http://www.example.com", text="test")
         l5 = Link("http://www.example.com", text="test2")
 
         self._assert_different_links(l4, l5)
 
     def test_eq_and_hash_6(self):
+        """
+        Tests if two instances of Link that point to the same url 
+        are evaluated that they point to the same url if they
+        have the same text variable values.
+        """
         l4 = Link("http://www.example.com", text="test")
         l6 = Link("http://www.example.com", text="test")
         self._assert_same_links(l4, l6)
 
     def test_eq_and_hash_7(self):
+        """
+        Tests if two instances of Link that point to the same url 
+        are evaluated that they point to the same url if they
+        have the same text and fragment variable values and they both 
+        have the nofollow option set to False.
+        """
         l7 = Link(
             "http://www.example.com", text="test", fragment="something", nofollow=False
         )
@@ -54,6 +88,12 @@ class LinkTest(unittest.TestCase):
         self._assert_same_links(l7, l8)
 
     def test_eq_and_hash_8(self):
+        """
+        Tests if two instances of Link that point to the same url 
+        are evaluated as different if they point to the same url and 
+        have the same text and fragment variable values but one is set  
+        to have the nofollow option set to False while the other to True.
+        """
         l7 = Link(
             "http://www.example.com", text="test", fragment="something", nofollow=False
         )
@@ -63,6 +103,12 @@ class LinkTest(unittest.TestCase):
         self._assert_different_links(l7, l9)
 
     def test_eq_and_hash_9(self):
+        """
+        Tests if two instances of Link that point to the same url 
+        are evaluated as different if they point to the same url and 
+        have the same text variable values and both haves set the 
+        nofollow option to False but they have different fragment values.
+        """
         l7 = Link(
             "http://www.example.com", text="test", fragment="something", nofollow=False
         )
@@ -72,6 +118,10 @@ class LinkTest(unittest.TestCase):
         self._assert_different_links(l7, l10)
 
     def test_repr(self):
+        """
+        Tests if the repr function successfully creates a similar copy of a Link
+        instance.
+        """
         l1 = Link(
             "http://www.example.com", text="test", fragment="something", nofollow=True
         )
@@ -79,6 +129,10 @@ class LinkTest(unittest.TestCase):
         self._assert_same_links(l1, l2)
 
     def test_bytes_url(self):
+        """
+        Tests if a wrong argument is passed in the initialization of 
+        a Link instance successfully raises a TypeError.
+        """
         with self.assertRaises(TypeError):
             Link(b"http://www.example.com/\xc2\xa3")
 
