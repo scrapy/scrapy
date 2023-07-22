21b6dc5f9fbd2607d6f4df20256dcdd9d5b9dae4
Andrey Rakhmatullin <wrar@wrar.name>
2023-07-22 17:55:32 +0400

    Add 3.12 CI jobs.

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Internet :: WWW/HTTP",
