4ad727f0b566f2e50587e7bd2c9c691f10700daf
Laerte Pereira <laertefbk@gmail.com>
2023-10-17 19:47:01 -0300

    Remove _check_signature

--- scrapy/pipelines/media.py
+++ scrapy/pipelines/media.py
@@ -1,8 +1,6 @@
 import functools
 import logging
 from collections import defaultdict
-from inspect import signature
-from warnings import warn
 
 from twisted.internet.defer import Deferred, DeferredList
 from twisted.python.failure import Failure
@@ -11,7 +9,6 @@ from scrapy.http.request import NO_CALLBACK
 from scrapy.settings import Settings
 from scrapy.utils.datatypes import SequenceExclude
 from scrapy.utils.defer import defer_result, mustbe_deferred
-from scrapy.utils.deprecate import ScrapyDeprecationWarning
 from scrapy.utils.log import failure_to_exc_info
 from scrapy.utils.misc import arg_to_iter
 
@@ -122,21 +119,6 @@ class MediaPipeline:
             )
         )
         return dfd.addBoth(lambda _: wad)  # it must return wad at last
-
-    def _check_signature(self, func):
-        sig = signature(func)
-        self._expects_item[func.__name__] = True
-
-        if "item" not in sig.parameters:
-            old_params = str(sig)[1:-1]
-            new_params = old_params + ", *, item=None"
-            warn(
-                f"{func.__name__}(self, {old_params}) is deprecated, "
-                f"please use {func.__name__}(self, {new_params})",
-                ScrapyDeprecationWarning,
-                stacklevel=2,
-            )
-            self._expects_item[func.__name__] = False
 
     def _modify_media_request(self, request):
         if self.handle_httpstatus_list:
