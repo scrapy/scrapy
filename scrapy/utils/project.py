4000174852e1ed0a0c99755d17846e350857524c
Heorhii Korzh <georgiy.korzh@tgadigital.com>
2023-11-01 22:35:23 +0200

    scrapy.utils.data_path type hint change

--- scrapy/utils/project.py
+++ scrapy/utils/project.py
@@ -2,6 +2,8 @@ import os
 import warnings
 from importlib import import_module
 from pathlib import Path
+from typing import Union
+from os import PathLike
 
 from scrapy.exceptions import NotConfigured
 from scrapy.settings import Settings
@@ -44,7 +46,7 @@ def project_data_dir(project: str = "default") -> str:
     return str(d)
 
 
-def data_path(path: str, createdir: bool = False) -> str:
+def data_path(path: Union[str, PathLike, Path], createdir: bool = False) -> str:
     """
     Return the given path joined with the .scrapy data directory.
     If given an absolute path, return it unmodified.
