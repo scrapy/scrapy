b2524b5f8ab2b4f90324bec5243572d7fb3fa01c
Swati Anshu <97234193+sa2415@users.noreply.github.com>
2023-12-04 20:24:52 -0500

    Update misc.py
    Separates the create_instance into 2 separate functions 
    1) build_from_settings
    2)build_from_crawler

--- scrapy/utils/misc.py
+++ scrapy/utils/misc.py
@@ -137,31 +137,67 @@ def rel_has_nofollow(rel: Optional[str]) -> bool:
     return rel is not None and "nofollow" in rel.replace(",", " ").split()
 
 
-def create_instance(objcls, settings, crawler, *args, **kwargs):
-    """Construct a class instance using its ``from_crawler`` or
-    ``from_settings`` constructors, if available.
+# def create_instance(objcls, settings, crawler, *args, **kwargs):
+#     """Construct a class instance using its ``from_crawler`` or
+#     ``from_settings`` constructors, if available.
 
-    At least one of ``settings`` and ``crawler`` needs to be different from
-    ``None``. If ``settings `` is ``None``, ``crawler.settings`` will be used.
-    If ``crawler`` is ``None``, only the ``from_settings`` constructor will be
-    tried.
+#     At least one of ``settings`` and ``crawler`` needs to be different from
+#     ``None``. If ``settings `` is ``None``, ``crawler.settings`` will be used.
+#     If ``crawler`` is ``None``, only the ``from_settings`` constructor will be
+#     tried.
 
-    ``*args`` and ``**kwargs`` are forwarded to the constructors.
+#     ``*args`` and ``**kwargs`` are forwarded to the constructors.
 
-    Raises ``ValueError`` if both ``settings`` and ``crawler`` are ``None``.
+#     Raises ``ValueError`` if both ``settings`` and ``crawler`` are ``None``.
 
-    .. versionchanged:: 2.2
-       Raises ``TypeError`` if the resulting instance is ``None`` (e.g. if an
-       extension has not been implemented correctly).
-    """
-    if settings is None:
-        if crawler is None:
-            raise ValueError("Specify at least one of settings and crawler.")
-        settings = crawler.settings
+#     .. versionchanged:: 2.2
+#        Raises ``TypeError`` if the resulting instance is ``None`` (e.g. if an
+#        extension has not been implemented correctly).
+#     """
+#     if settings is None:
+#         if crawler is None:
+#             raise ValueError("Specify at least one of settings and crawler.")
+#         settings = crawler.settings
+#     if crawler and hasattr(objcls, "from_crawler"):
+#         instance = objcls.from_crawler(crawler, *args, **kwargs)
+#         method_name = "from_crawler"
+#     elif hasattr(objcls, "from_settings"):
+#         instance = objcls.from_settings(settings, *args, **kwargs)
+#         method_name = "from_settings"
+#     else:
+#         instance = objcls(*args, **kwargs)
+#         method_name = "__new__"
+#     if instance is None:
+#         raise TypeError(f"{objcls.__qualname__}.{method_name} returned None")
+#     return instance
+
+
+# ``*args`` and ``**kwargs`` are forwarded to the constructors.
+# Raises ``ValueError`` if``crawler`` is``None``.
+#  Raises typeError is instance is None
+# Creates a class instance using 'from_crawler' constructor
+def build_from_crawler(objcls, crawler, /, *args, **kwargs):
+    if crawler is None: 
+        raise ValueError("Specify crawler.")
     if crawler and hasattr(objcls, "from_crawler"):
         instance = objcls.from_crawler(crawler, *args, **kwargs)
         method_name = "from_crawler"
-    elif hasattr(objcls, "from_settings"):
+    else:
+        instance = objcls(*args, **kwargs)
+        method_name = "__new__"
+    if instance is None:
+        raise TypeError(f"{objcls.__qualname__}.{method_name} returned None")
+    return instance
+            
+
+#``*args`` and ``**kwargs`` are forwarded to the constructors.
+# Raises ``ValueError`` if``settings`` is``None``.
+#  Raises typeError is instance is None
+# Creates a class instance using 'from_settings' constructor
+def build_from_settings(objcls, settings, /, *args, **kwargs):
+    if settings is None: 
+        raise ValueError("Specify settings.")
+    if settings and hasattr(objcls, "from_settings"):
         instance = objcls.from_settings(settings, *args, **kwargs)
         method_name = "from_settings"
     else:
@@ -170,6 +206,8 @@ def create_instance(objcls, settings, crawler, *args, **kwargs):
     if instance is None:
         raise TypeError(f"{objcls.__qualname__}.{method_name} returned None")
     return instance
+
+
 
 
 @contextmanager
