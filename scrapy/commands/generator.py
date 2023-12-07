28556f1cde72e53c7f2fa0634ed1320a6219c769
Yerim Shin <103637869+yyyeee1108@users.noreply.github.com>
2023-12-08 00:26:47 +0900

    feat: generator 명령어 번역 기능 추가
    - scrapy generator start 입력 시 처음에 번역 여부 물어봄
    - 기본 언어는 영어로 맞춰둠

--- scrapy/commands/generator.py
+++ scrapy/commands/generator.py
@@ -2,6 +2,38 @@ from scrapy.commands import ScrapyCommand
 from scrapy.exceptions import UsageError
 import subprocess
 
+
+# papago 번역 API 사용
+import requests
+import json
+
+def setLanguage():
+    text = "[Korean]: ko [English]: en [Japanese]:ja [Simplified Chinese]: zh-CN\n[Traditional Chinese]: zh-TW [Vietnamese]: vi [Indonesian]: id [Thai]: th\n[German]: de [Russian]: ru [Spanish]: es [Italian]: it\n[French]: fr\n"
+    language = str(input(f"{text}Enter the language code you want to translate to: "))
+    return language
+
+def translate(text, language):
+
+    # APP 등록 - access token
+    CLIENT_ID, CLIENT_SECRET = "7Yw8ht8mnWqY2U1MEaaJ", "vCwi0FpefV"
+    
+    # request (en 외 언어로 번역도 가능)
+    #text = "Error: Project names must begin with a letter and contain"+" only\nletters, numbers and underscores"
+    url = 'https://openapi.naver.com/v1/papago/n2mt'
+    headers = {
+        'Content-Type': 'application/json',
+        'X-Naver-Client-Id': CLIENT_ID,
+        'X-Naver-Client-Secret': CLIENT_SECRET
+    }
+    data = {'source': 'en', 'target': language, 'text': text}
+    
+    # post 방식으로 서버 쪽으로 요청
+    response = requests.post(url, json.dumps(data), headers=headers) 
+    
+    # json() 후 key 값을 사용하여 원하는 텍스트 접근
+    after_text = response.json()['message']['result']['translatedText']
+    return after_text
+
 class Command(ScrapyCommand):
     requires_project = False
     default_settings = {"LOG_ENABLED": False}
@@ -26,10 +58,18 @@ class Command(ScrapyCommand):
             command_name = args[0]
             # 처리할 특정 명령어가 입력되었을 때의 로직 추가
             if command_name == "start":
-                self.generator()
+                isTranslate = input("Do you want to translate it?(y/n): ")
+                while(True):
+                    if isTranslate=="y":
+                        self.generator_translate()
+                        break
+                    elif isTranslate=="n":
+                        self.generator()
+                        break
+                    else:
+                        print("Enter \'y\' or \'n\'")
+                        isTranslate = input("Do you want to translate it?(y/n): ")
                 
-            # 예: 특정 명령어에 대한 도움말 출력
-            # print(f"Help for '{command_name}' command...")
         else:
             # 모든 명령어에 대한 도움말 출력
             self.print_commands_help()
@@ -38,45 +78,45 @@ class Command(ScrapyCommand):
         # 여기에 사용 가능한 모든 명령어와 간단한 설명을 출력하는 로직 추가
 
         # 예: 간단한 설명 출력
-        print("프로젝트 생성을 돕기 위한 명령어 입니다")
-        print("<scrapy generator start> 명령어를 입력하여 손쉽게 프로젝트 구성을 시작하세요")
+        print("This is a command to help create a project")
+        print("Easily create a project by entering the <scrapy generator start> command")
         # 추가 명령어에 대한 설명도 출력 가능
 
     def generator(self):
-        print("\n=======================[기본 설정]=======================\n")
+        print("\n=======================[default setting]=======================\n")
 
         while(True):
             # 프로젝트 생성
-            project_name = input("프로젝트의 이름을 입력하세요: ")
+            project_name = input("Enter the name of the project: ")
 
             # 스파이더 이름 입력
-            spider_name = input("스파이더의 이름을 입력하세요: ")
+            spider_name = input("Enter the name of the Spider: ")
 
             # 크롤링 주소 입력
-            spider_url = input("크롤링 할 url 주소를 입력하세요: ")
+            spider_url = input("Enter the website URL to crawl: ")
 
             # 템플릿 입력
-            template_list = ["basic - 가장 기본적인 스파이더 템플릿. 모든 필수적인 요소들을 포함하고 있습니다",
-                                "crawl - \'CrawlSpider\'를 사용하는 스파이더 템플릿입니다.\n           링크를 따라가며 크롤링하는 데 유용합니다",
-                                "csvfeed - CSV 파일을 이용하여 데이터를 가져오는 템플릿입니다",
-                                "xmlfeed - XML 파일을 이용하여 데이터를 가져오는 템플릿입니다"]
-            print("\n============================[템플릿 목록]============================\n")
+            template_list = ["basic - This is the most fundamental spider template, containing all the essential elements required",
+                                "crawl - Utilizes \'CrawlSpider\' for spiders\n Useful for crawling by following links",
+                                "csvfeed - Utilizes CSV files for importing data",
+                                "xmlfeed - Utilizes XML files for importing data"]
+            print("\n============================[template list]============================\n")
             for i in range(len(template_list)):
                 text = str(i+1)+". "+template_list[i]
                 print(f"{text}")
-            spider_template = input("\n템플릿을 입력하세요: ")
+            spider_template = input("\nEnter the template: ")
 
-            print("\n============================[입력 확인]============================\n")
-            print(f"1. 프로젝트 이름: {project_name}")
-            print(f"2. 스파이더 이름: {spider_name}")
-            print(f"3. 크롤링할 주소: {spider_url}")
-            print(f"4. 템플릿 선택: {spider_template}")
+            print("\n============================[input check]============================\n")
+            print(f"1. project name: {project_name}")
+            print(f"2. Spider name: {spider_name}")
+            print(f"3. URL to crwal: {spider_url}")
+            print(f"4. template: {spider_template}")
 
-            answer = input("\n입력한 정보가 맞으십니까?(y/n): ")
+            answer = input("\nIs the information you entered correct?(y/n): ")
             
             while(True):
                 if  answer == "y":
-                    print("\n================================[생성]================================\n")
+                    print("\n================================[create process]================================\n")
                     # 입력받은 값 바탕으로 startproject 명령어 실행
                     subprocess.run(f'scrapy startproject {project_name}')
                     # 입력받은 값 바탕으로 genspider 명령어 실행
@@ -85,24 +125,97 @@ class Command(ScrapyCommand):
                 elif answer == "n":
                     break
                 else:
-                    print("\'y\' 또는 \'n\'를 입력하시오")
-                    answer = input("\n입력한 정보가 맞으십니까?(y/n): ")
+                    print("Enter \'y\' or \'n\'")
+                    answer = input("\nIs the information you entered correct?(y/n): ")
 
             if(answer=="y"):
                 break
             else:
                 continue
         
+        print("\n====================================================================\n")
+
         while(True):
-            isOpen = input("생성된 프로젝트를 vs code에서 여시겠습니까?(y/n): ")
+            isOpen = input("Do you want to open the created project in VSCode?(y/n): ")
             if isOpen == "y":
                 subprocess.run("code .",cwd=f"{project_name}",shell=True)
                 break
             elif isOpen =="n":
-                print("프로젝트 생성을 종료합니다")
+                isOpenExploler = input("Do you want to open the Project folder?(y/n): ")
+                if isOpenExploler == "y":
+                    subprocess.run("explorer .",cwd=f"{project_name}",shell=True)
+                print("Ends the project creation")
                 break
             else:
-                print("\'y\' 또는 \'n\'를 입력하시오")
+                print("Enter \'y\' or \'n\'")
+    
+    def generator_translate(self):
+        language = setLanguage()
+        print(translate("\n=======================[default setting]=======================\n",language))
+
+        while(True):
+            # 프로젝트 생성
+            project_name = input(translate("Enter the name of the project:",language)+" ")
+
+            # 스파이더 이름 입력
+            spider_name = input(translate("Enter the name of the [Spider]:",language)+" ")
+
+            # 크롤링 주소 입력
+            spider_url = input(translate("Enter the website URL to crawl:",language)+" ")
+
+            # 템플릿 입력
+            template_list = ["basic - "+translate("This is the most fundamental [Spider] template, containing all the essential elements required",language),
+                                "crawl - "+translate("Utilizes [CrawlSpider] for [Spiders]\n Useful for crawling by following links",language),
+                                "csvfeed - "+translate("Utilizes CSV files for importing data",language),
+                                "xmlfeed - "+translate("Utilizes XML files for importing data",language)]
+            print(translate("\n============================[template list]============================\n",language))
+            for i in range(len(template_list)):
+                text = str(i+1)+". "+template_list[i]
+                print(f"{text}")
+            spider_template = input(translate("\nEnter the template:",language)+" ")
+
+            print(translate("\n============================[input check]============================\n",language))
+            print(translate("1. project name:",language),project_name)
+            print(translate("2. [Spider] name:",language),spider_name)
+            print(translate("3. URL to crwal:",language),spider_url)
+            print(translate("4. template:",language),spider_template)
+
+            answer = input(translate("\nIs the information you entered correct?(y/n):",language)+" ")
+            
+            while(True):
+                if  answer == "y":
+                    print(translate("\n================================[create process]================================\n",language))
+                    # 입력받은 값 바탕으로 startproject 명령어 실행
+                    subprocess.run(f'scrapy startproject {project_name}')
+                    # 입력받은 값 바탕으로 genspider 명령어 실행
+                    subprocess.run(f'scrapy genspider -t {spider_template} {spider_name} {spider_url}',cwd=f"{project_name}",shell=True)
+                    break
+                elif answer == "n":
+                    break
+                else:
+                    print(translate("Enter \'y\' or \'n\'",language))
+                    answer = input(translate("\nIs the information you entered correct?(y/n): ",language)+" ")
+
+            if(answer=="y"):
+                break
+            else:
+                continue
+
+        print("\n====================================================================\n")
+
+        while(True):
+            isOpen = input(translate("Do you want to open the created project in VSCode?(y/n): ",language)+" ")
+            if isOpen == "y":
+                subprocess.run("code .",cwd=f"{project_name}",shell=True)
+                break
+            elif isOpen =="n":
+                isOpenExploler = input(translate("Do you want to open the Project folder?(y/n): ",language))
+                if isOpenExploler == "y":
+                    subprocess.run("explorer .",cwd=f"{project_name}",shell=True)
+                print(translate("Ends the project creation",language))
+                break
+            else:
+                print(translate("Enter \'y\' or \'n\'",language))
 
                 
         
