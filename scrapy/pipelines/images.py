da7c394021cce4030c76138f46cf91043b53e926
M Ikram Ullah Khan <44160462+IkramKhanNiazi@users.noreply.github.com>
2024-01-22 13:28:31 +0500

    Update images.py

--- scrapy/pipelines/images.py
+++ scrapy/pipelines/images.py
@@ -219,6 +219,7 @@ class ImagesPipeline(FilesPipeline):
 
     def get_media_requests(self, item, info):
         urls = ItemAdapter(item).get(self.images_urls_field, [])
+        urls = urls if isinstance(urls, list) else [urls]
         return [Request(u, callback=NO_CALLBACK) for u in urls]
 
     def item_completed(self, results, item, info):
