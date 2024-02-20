53231d02fd74e10b871990ab5344e9c165027c52
Elias Ram <eliasram@kth.se>
2024-02-20 20:51:13 +0100

    test #10 added tests for http2 stream

---
+++ tests/test_http2_stream.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from unittest.mock import Mock
+
+from h2.exceptions import StreamClosedError
+from scrapy.core.http2.stream import Stream, StreamCloseReason
+from scrapy.http import Request
+
+
+class Http2StreamTestCase(TestCase):
+
+    def test_close_when_stream_closed_server(self):
+        stream = Stream(0, Request(url="http://fakeUrl.com"), Mock())
+        stream.metadata["stream_closed_server"] = True
+        
+        self.assertRaises(StreamClosedError, stream.close, StreamCloseReason.ENDED)
+
+    def test_close_with_invalid_reason(self):
+        stream = Stream(0, Request(url="http://fakeUrl.com"), Mock())
+        
+        self.assertRaises(TypeError, stream.close, Mock())
