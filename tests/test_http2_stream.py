5f218b4367d2971c9d23730a715d2473f1924d44
Elias Ram <eliasram@kth.se>
2024-02-20 22:46:02 +0100

    format #13 fixed formatting to pass pre commit checks

--- tests/test_http2_stream.py
+++ tests/test_http2_stream.py
@@ -1,20 +1,18 @@
 from unittest import TestCase
 from unittest.mock import Mock
 
-from h2.exceptions import StreamClosedError
-from scrapy.core.http2.stream import Stream, StreamCloseReason
+from scrapy.core.http2.stream import Stream, StreamClosedError, StreamCloseReason
 from scrapy.http import Request
 
 
 class Http2StreamTestCase(TestCase):
-
     def test_close_when_stream_closed_server(self):
         stream = Stream(0, Request(url="http://fakeUrl.com"), Mock())
         stream.metadata["stream_closed_server"] = True
-        
+
         self.assertRaises(StreamClosedError, stream.close, StreamCloseReason.ENDED)
 
     def test_close_with_invalid_reason(self):
         stream = Stream(0, Request(url="http://fakeUrl.com"), Mock())
-        
+
         self.assertRaises(TypeError, stream.close, Mock())
