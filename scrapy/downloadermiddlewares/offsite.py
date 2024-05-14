d2f1e00a6afa46c26a334a24217d5bf605bab9fb
Adrián Chaves <adrian@chaves.io>
2024-05-14 18:54:11 +0200

    Merge 2.11.2 changes (#6363)

--- scrapy/downloadermiddlewares/offsite.py
+++ scrapy/downloadermiddlewares/offsite.py
@@ -1,64 +1,35 @@
-"""
-Offsite Spider Middleware
-
-See documentation in docs/topics/spider-middleware.rst
-"""
-
-from __future__ import annotations
-
 import logging
 import re
 import warnings
-from typing import TYPE_CHECKING, Any, AsyncIterable, Iterable, Set
 
-from scrapy import Spider, signals
-from scrapy.crawler import Crawler
-from scrapy.exceptions import ScrapyDeprecationWarning
-from scrapy.http import Request, Response
-from scrapy.statscollectors import StatsCollector
+from scrapy import signals
+from scrapy.exceptions import IgnoreRequest
 from scrapy.utils.httpobj import urlparse_cached
-
-warnings.warn(
-    "The scrapy.spidermiddlewares.offsite module is deprecated, use "
-    "scrapy.downloadermiddlewares.offsite instead.",
-    ScrapyDeprecationWarning,
-)
-
-if TYPE_CHECKING:
-    # typing.Self requires Python 3.11
-    from typing_extensions import Self
 
 logger = logging.getLogger(__name__)
 
 
 class OffsiteMiddleware:
-    def __init__(self, stats: StatsCollector):
-        self.stats: StatsCollector = stats
-
     @classmethod
-    def from_crawler(cls, crawler: Crawler) -> Self:
-        assert crawler.stats
+    def from_crawler(cls, crawler):
         o = cls(crawler.stats)
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
+        crawler.signals.connect(o.request_scheduled, signal=signals.request_scheduled)
         return o
 
-    def process_spider_output(
-        self, response: Response, result: Iterable[Any], spider: Spider
-    ) -> Iterable[Any]:
-        return (r for r in result if self._filter(r, spider))
+    def __init__(self, stats):
+        self.stats = stats
+        self.domains_seen = set()
 
-    async def process_spider_output_async(
-        self, response: Response, result: AsyncIterable[Any], spider: Spider
-    ) -> AsyncIterable[Any]:
-        async for r in result:
-            if self._filter(r, spider):
-                yield r
+    def spider_opened(self, spider):
+        self.host_regex = self.get_host_regex(spider)
 
-    def _filter(self, request: Any, spider: Spider) -> bool:
-        if not isinstance(request, Request):
-            return True
+    def request_scheduled(self, request, spider):
+        self.process_request(request, spider)
+
+    def process_request(self, request, spider):
         if request.dont_filter or self.should_follow(request, spider):
-            return True
+            return None
         domain = urlparse_cached(request).hostname
         if domain and domain not in self.domains_seen:
             self.domains_seen.add(domain)
@@ -69,15 +40,15 @@ class OffsiteMiddleware:
             )
             self.stats.inc_value("offsite/domains", spider=spider)
         self.stats.inc_value("offsite/filtered", spider=spider)
-        return False
+        raise IgnoreRequest
 
-    def should_follow(self, request: Request, spider: Spider) -> bool:
+    def should_follow(self, request, spider):
         regex = self.host_regex
         # hostname can be None for wrong urls (like javascript links)
         host = urlparse_cached(request).hostname or ""
         return bool(regex.search(host))
 
-    def get_host_regex(self, spider: Spider) -> re.Pattern[str]:
+    def get_host_regex(self, spider):
         """Override this method to implement a different offsite policy"""
         allowed_domains = getattr(spider, "allowed_domains", None)
         if not allowed_domains:
@@ -93,26 +64,14 @@ class OffsiteMiddleware:
                     "allowed_domains accepts only domains, not URLs. "
                     f"Ignoring URL entry {domain} in allowed_domains."
                 )
-                warnings.warn(message, URLWarning)
+                warnings.warn(message)
             elif port_pattern.search(domain):
                 message = (
                     "allowed_domains accepts only domains without ports. "
                     f"Ignoring entry {domain} in allowed_domains."
                 )
-                warnings.warn(message, PortWarning)
+                warnings.warn(message)
             else:
                 domains.append(re.escape(domain))
         regex = rf'^(.*\.)?({"|".join(domains)})$'
         return re.compile(regex)
-
-    def spider_opened(self, spider: Spider) -> None:
-        self.host_regex: re.Pattern[str] = self.get_host_regex(spider)
-        self.domains_seen: Set[str] = set()
-
-
-class URLWarning(Warning):
-    pass
-
-
-class PortWarning(Warning):
-    pass
