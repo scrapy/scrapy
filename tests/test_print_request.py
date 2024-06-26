c0678d7962f9695f8cfa0f50eb17431bec462e3a
PeterMayrany <66109992+PeterMayrany@users.noreply.github.com>
2024-06-26 14:54:56 +0200

    added new test for print_request()

---
+++ tests/test_print_request.py
@@ -0,0 +1,38 @@
+import unittest
+from scrapy.commands import parse
+from scrapy.http import Request
+from scrapy.settings import Settings
+
+class TestPrintRequests(unittest.TestCase):
+    def setUp(self):
+        self.command = parse.Command()
+        self.command.settings = Settings()
+
+    def tearDown(self):
+        self.command.write_print_request_coverage_to_file()
+
+    def test_print_requests_no_level_no_request(self):
+        self.command.requests = {}
+        self.command.print_requests(None, colour=False)
+        self.assertEqual(parse.print_requests_coverage["run_1"], "hit")
+        self.assertEqual(parse.print_requests_coverage["run_1.2"], "hit")
+    
+    def test_print_requests_no_level_with_request(self):
+        self.command.requests = {1: [Request("http://coverage.com")]}
+        self.command.print_requests(None, colour=False)
+        self.assertEqual(parse.print_requests_coverage["run_1"], "hit")
+        self.assertEqual(parse.print_requests_coverage["run_1.1"], "hit")
+
+    # both cases below cover the "main" else clause
+    def test_print_requests_with_level_with_request(self):
+        self.command.requests = {1: [Request("http://coverage.com")]}
+        self.command.print_requests(1, colour=False)
+        self.assertEqual(parse.print_requests_coverage["run_2"], "hit")
+
+    def test_print_requests_with_level_no_request(self):
+        self.command.requests = {1: [Request("http://coverage.com")]}
+        self.command.print_requests(2, colour=False)
+        self.assertEqual(parse.print_requests_coverage["run_2"], "hit")
+
+if __name__ == '__main__':
+    unittest.main()
