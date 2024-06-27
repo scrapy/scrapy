5619f7b00727dfbed16c38675962885bc42b1a7d
PeterMayrany <66109992+PeterMayrany@users.noreply.github.com>
2024-06-27 19:05:46 +0200

    Update test_print_request.py

--- tests/test_print_request.py
+++ tests/test_print_request.py
@@ -1,37 +1,36 @@
 import unittest
+
 from scrapy.commands import parse
 from scrapy.http import Request
-from scrapy.settings import Settings
 
 class TestPrintRequests(unittest.TestCase):
     def setUp(self):
         self.command = parse.Command()
-        self.command.settings = Settings()
 
     def tearDown(self):
         self.command.write_print_request_coverage_to_file()
 
     def test_print_requests_no_level_no_request(self):
         self.command.requests = {}
-        self.command.print_requests(None, colour=False) #runs the function with a unique case
+        self.command.print_requests(None, colour=False) #comment out to get 0%
         self.assertEqual(parse.print_requests_coverage["run_1"], "hit")
         self.assertEqual(parse.print_requests_coverage["run_1.2"], "hit")
     
     def test_print_requests_no_level_with_request(self):
-        self.command.requests = {1: [Request("http://coverage.com")]}
-        self.command.print_requests(None, colour=False)
+        self.command.requests = {1: "http://examplerequest.com"}
+        self.command.print_requests(None, colour=False) #comment out to get 0%
         self.assertEqual(parse.print_requests_coverage["run_1"], "hit")
         self.assertEqual(parse.print_requests_coverage["run_1.1"], "hit")
 
     # both cases below cover the "main" else clause
     def test_print_requests_with_level_with_request(self):
-        self.command.requests = {1: [Request("http://coverage.com")]}
-        self.command.print_requests(1, colour=False)
+        self.command.requests = {1: "http://examplerequest.com"}
+        self.command.print_requests(1, colour=False) #comment out to get 0%
         self.assertEqual(parse.print_requests_coverage["run_2"], "hit")
 
     def test_print_requests_with_level_no_request(self):
-        self.command.requests = {1: [Request("http://coverage.com")]}
-        self.command.print_requests(2, colour=False)
+        self.command.requests = {1: "http://examplerequest.com"}
+        self.command.print_requests(2, colour=False) #comment out to get 0%
         self.assertEqual(parse.print_requests_coverage["run_2"], "hit")
 
 if __name__ == '__main__':
