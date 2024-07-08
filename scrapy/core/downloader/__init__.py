035ae28886819a3c555b199395a633800cd744b6
Adrián Chaves <adrian@chaves.io>
2024-07-08 10:29:33 +0200

    Minor cleanup

--- scrapy/core/downloader/__init__.py
+++ scrapy/core/downloader/__init__.py
@@ -134,9 +134,9 @@ class Downloader:
         )
         self._stats = crawler.stats
 
-        default_response_max_active_size = 5000000
+        default_response_max_active_size = 5_000_000
         scraper_max_active_size = self.settings.getint(
-            "SCRAPER_MAX_ACTIVE_SIZE", 5000000
+            "SCRAPER_MAX_ACTIVE_SIZE", default_response_max_active_size
         )
         if scraper_max_active_size != default_response_max_active_size:
             warn(
