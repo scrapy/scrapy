521e68998afbdfa91b2b917f6831aa230f8346ea
Georgiy Zatserklianyi <george.zatseklyany@gmail.com>
2024-08-10 17:30:11 +0200

    start_requests: test_spidermiddleware updated

--- tests/test_spidermiddleware.py
+++ tests/test_spidermiddleware.py
@@ -1,5 +1,5 @@
 import collections.abc
-from typing import Optional
+from typing import Optional, Union
 from unittest import mock
 
 from testfixtures import LogCapture
@@ -328,12 +328,13 @@ class ProcessStartRequestsSimpleMiddleware:
 class ProcessStartRequestsSimple(BaseAsyncSpiderMiddlewareTestCase):
     """process_start_requests tests for simple start_requests"""
 
-    ITEM_TYPE = Request
+    ITEM_TYPE = Union[Request, dict]
     MW_SIMPLE = ProcessStartRequestsSimpleMiddleware
 
     def _start_requests(self):
-        for i in range(3):
+        for i in range(2):
             yield Request(f"https://example.com/{i}", dont_filter=True)
+        yield {"name": "test item"}
 
     @defer.inlineCallbacks
     def _get_middleware_result(self, *mw_classes, start_index: Optional[int] = None):
