5794071f9679c89ef4ee75e8a627274b2464b65b
Andrey Rakhmatullin <wrar@wrar.name>
2024-08-23 15:48:01 +0500

    Typing fixes and updates. (#6460)

--- scrapy/utils/ssl.py
+++ scrapy/utils/ssl.py
@@ -20,7 +20,7 @@ def x509name_to_string(x509name: X509Name) -> str:
     # from OpenSSL.crypto.X509Name.__repr__
     result_buffer: Any = pyOpenSSLutil.ffi.new("char[]", 512)
     pyOpenSSLutil.lib.X509_NAME_oneline(
-        x509name._name, result_buffer, len(result_buffer)  # type: ignore[attr-defined]
+        x509name._name, result_buffer, len(result_buffer)
     )
 
     return ffi_buf_to_string(result_buffer)
