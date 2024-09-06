c008fa6dfcfb1f5ff2a9cfffff86329e408a1ba5
lucasbelo777 <lucas.belo@cialdnb.com>
2024-09-06 09:08:16 -0300

    chore: adjusting superclasses types

--- scrapy/pipelines/media.py
+++ scrapy/pipelines/media.py
@@ -264,7 +264,7 @@ class MediaPipeline(ABC):
         info: SpiderInfo,
         *,
         item: Any = None,
-    ) -> FileInfo:
+    ) -> Deferred:
         """Handler for success downloads"""
         raise NotImplementedError()
 
