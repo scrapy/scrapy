e848c829ef74b96fcc7d0d79cf41457bb04a5dc6
LucasSD <lucas.stonedrake@gmail.com>
2024-10-09 08:26:46 +0100

    Revert signal.py to functions for backwards compatibility

--- scrapy/utils/signal.py
+++ scrapy/utils/signal.py
@@ -1,39 +1,37 @@
 import warnings
 
 
-class SignalDeprecated:
-    def send_catch_log(self, *args, **kwargs):
-        warnings.warn(
-            "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
-            "Please use `scrapy.utils.signal_ops` instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        from .signal_ops import send_catch_log
+def send_catch_log(*args, **kwargs):
+    warnings.warn(
+        "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
+        "Please use `scrapy.utils.signal_ops` instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    from .signal_ops import send_catch_log
 
-        return send_catch_log(*args, **kwargs)
-
-    def send_catch_log_deferred(self, *args, **kwargs):
-        warnings.warn(
-            "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
-            "Please use `scrapy.utils.signal_ops` instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        from .signal_ops import send_catch_log_deferred
-
-        return send_catch_log_deferred(*args, **kwargs)
-
-    def disconnect_all(self, *args, **kwargs):
-        warnings.warn(
-            "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
-            "Please use `scrapy.utils.signal_ops` instead.",
-            DeprecationWarning,
-            stacklevel=2,
-        )
-        from .signal_ops import disconnect_all
-
-        return disconnect_all(*args, **kwargs)
+    return send_catch_log(*args, **kwargs)
 
 
-signal = SignalDeprecated()
+def send_catch_log_deferred(*args, **kwargs):
+    warnings.warn(
+        "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
+        "Please use `scrapy.utils.signal_ops` instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    from .signal_ops import send_catch_log_deferred
+
+    return send_catch_log_deferred(*args, **kwargs)
+
+
+def disconnect_all(*args, **kwargs):
+    warnings.warn(
+        "The `scrapy.utils.signal` module is deprecated and will be removed in a future release. "
+        "Please use `scrapy.utils.signal_ops` instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    from .signal_ops import disconnect_all
+
+    return disconnect_all(*args, **kwargs)
