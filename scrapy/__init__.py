53f57480517ee268bc519aa3dccae6a6f4346525
Laerte Pereira <laertefbk@gmail.com>
2024-10-26 16:41:34 -0300

    Deprecate scrapy.twisted_version

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -4,9 +4,6 @@ Scrapy - a web crawling and web scraping framework written for Python
 
 import pkgutil
 import sys
-import warnings
-
-from twisted import version as _txv
 
 # Declare top-level shortcuts
 from scrapy.http import FormRequest, Request
@@ -17,7 +14,6 @@ from scrapy.spiders import Spider
 __all__ = [
     "__version__",
     "version_info",
-    "twisted_version",
     "Spider",
     "Request",
     "FormRequest",
@@ -30,13 +26,22 @@ __all__ = [
 # Scrapy and Twisted versions
 __version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").strip()
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split("."))
-twisted_version = (_txv.major, _txv.minor, _txv.micro)
 
 
-# Ignore noisy twisted deprecation warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning, module="twisted")
+def __getattr__(name: str):
+    if name == "twisted_version":
+        import warnings
+
+        from twisted import version as _txv
+
+        from scrapy.exceptions import ScrapyDeprecationWarning
+
+        warnings.warn(
+            "The scrapy.twisted_version attribute is deprecated, use twisted.version instead",
+            ScrapyDeprecationWarning,
+        )
+        return _txv.major, _txv.minor, _txv.micro
 
 
 del pkgutil
 del sys
-del warnings
