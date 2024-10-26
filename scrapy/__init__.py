718b797e4c33ba14c6a81882caef51af4ab6cfc8
Laerte Pereira <laertefbk@gmail.com>
2024-10-26 17:22:35 -0300

    raise default exception if attribute is not found

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -28,7 +28,7 @@ __version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split("."))
 
 
-def __getattr__(name):
+def __getattr__(name: str):
     if name == "twisted_version":
         import warnings
 
@@ -41,6 +41,8 @@ def __getattr__(name):
             ScrapyDeprecationWarning,
         )
         return _txv.major, _txv.minor, _txv.micro
+
+    raise AttributeError()
 
 
 del pkgutil
