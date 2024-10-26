1f787a637dd4d505cd425c58b3620bf8e76d4f29
Laerte Pereira <laertefbk@gmail.com>
2024-10-26 16:44:27 -0300

    fix: typing

--- scrapy/__init__.py
+++ scrapy/__init__.py
@@ -28,7 +28,7 @@ __version__ = (pkgutil.get_data(__package__, "VERSION") or b"").decode("ascii").
 version_info = tuple(int(v) if v.isdigit() else v for v in __version__.split("."))
 
 
-def __getattr__(name: str):
+def __getattr__(name: str) -> None | tuple:
     if name == "twisted_version":
         import warnings
 
