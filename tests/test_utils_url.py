a195af304d2823cc686fd7354790e52239208d82
Laerte Pereira <5853172+Laerte@users.noreply.github.com>
2024-12-18 03:50:44 -0300

    Deprecate w3lib objects importable from scrapy.utils.url (#6586)

--- tests/test_utils_url.py
+++ tests/test_utils_url.py
@@ -1,10 +1,14 @@
 import unittest
+import warnings
+
+import pytest
 
 from scrapy.linkextractors import IGNORED_EXTENSIONS
 from scrapy.spiders import Spider
 from scrapy.utils.misc import arg_to_iter
 from scrapy.utils.url import (
     _is_filesystem_path,
+    _public_w3lib_objects,
     add_http_if_no_scheme,
     guess_scheme,
     strip_url,
@@ -605,6 +609,27 @@ class IsPathTestCase(unittest.TestCase):
             self.assertEqual(
                 _is_filesystem_path(input_value), output_value, input_value
             )
+
+
+@pytest.mark.parametrize(
+    "obj_name",
+    [
+        "_unquotepath",
+        "_safe_chars",
+        "parse_url",
+        *_public_w3lib_objects,
+    ],
+)
+def test_deprecated_imports_from_w3lib(obj_name):
+    with warnings.catch_warnings(record=True) as warns:
+        obj_type = "attribute" if obj_name == "_safe_chars" else "function"
+        message = f"The scrapy.utils.url.{obj_name} {obj_type} is deprecated, use w3lib.url.{obj_name} instead."
+
+        from importlib import import_module
+
+        getattr(import_module("scrapy.utils.url"), obj_name)
+
+        assert message in warns[0].message.args
 
 
 if __name__ == "__main__":
