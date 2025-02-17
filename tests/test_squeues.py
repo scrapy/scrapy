963e094bca6f5db0bc407f92eb29761a2b23b286
Andrey Rakhmatullin <wrar@wrar.name>
2025-02-17 23:23:23 +0500

    Adjust the test_nonserializable_object() regex for Python <= 3.11.

--- tests/test_squeues.py
+++ tests/test_squeues.py
@@ -32,7 +32,7 @@ class MyLoader(ItemLoader):
 def nonserializable_object_test(self):
     q = self.queue()
     with pytest.raises(
-        ValueError, match="unmarshallable object|Can't get local object"
+        ValueError, match="unmarshallable object|Can't (get|pickle) local object"
     ):
         q.push(lambda x: x)
     # Selectors should fail (lxml.html.HtmlElement objects can't be pickled)
