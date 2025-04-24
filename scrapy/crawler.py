91475cdd4e8ab104b291a505cd93e7b88620f087
fbenevolo <benevolofelipe@gmail.com>
2025-04-24 16:53:52 -0300

    added docstring to LRU and cache_max_size property

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -85,6 +85,7 @@ class Crawler:
         self.engine: ExecutionEngine | None = None
 
         self._component_cache: dict = {}
+        self._cache_max_size: int = 5
 
     def _update_root_log_handler(self) -> None:
         if get_scrapy_root_handler() is not None:
@@ -214,6 +215,16 @@ class Crawler:
         return self._get_component(cls, self.engine.downloader.middleware.middlewares)
 
     def LRU(self) -> None:
+        """Remove the least recently used component from the component cache.
+
+        This method implements the Least Recently Used (LRU) cache eviction policy,
+        removing the component that has not been accessed for the longest time.
+
+        .. versionadded:: 2.12
+
+        This method is called internally when the component cache reaches its
+        maximum size of 5 items.
+        """
         older = 0
         key_older = None
         for component in self._component_cache.items():
@@ -240,8 +251,9 @@ class Crawler:
                 "extension manager has been created."
             )
 
-        for key, items in self._component_cache.values():
-            items[1] += 1
+        # Update the cache timestamp for all components
+        for k in self._component_cache:
+            self._component_cache[k][1] += 1
 
         in_cache = self._component_cache.get(cls)
         if in_cache:
@@ -249,7 +261,7 @@ class Crawler:
             return in_cache
 
         result = self._get_component(cls, self.extensions.middlewares)
-        if len(self._component_cache) >= 5:
+        if len(self._component_cache) >= self._cache_max_size:
             self.LRU()
 
         self._component_cache[cls] = [result, 0]
