d577e85b02e8271ad60ca9da2e8de62a46028d04
Andrey Rakhmatullin <wrar@wrar.name>
2025-05-14 17:38:14 +0500

    Remove extra method copies.

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -391,33 +391,6 @@ class CrawlerRunner(CrawlerRunnerBase):
             self._active.discard(d)
             self.bootstrap_failed |= not getattr(crawler, "spider", None)
 
-    def create_crawler(
-        self, crawler_or_spidercls: type[Spider] | str | Crawler
-    ) -> Crawler:
-        """
-        Return a :class:`~scrapy.crawler.Crawler` object.
-
-        * If ``crawler_or_spidercls`` is a Crawler, it is returned as-is.
-        * If ``crawler_or_spidercls`` is a Spider subclass, a new Crawler
-          is constructed for it.
-        * If ``crawler_or_spidercls`` is a string, this function finds
-          a spider with this name in a Scrapy project (using spider loader),
-          then creates a Crawler instance for it.
-        """
-        if isinstance(crawler_or_spidercls, Spider):
-            raise ValueError(
-                "The crawler_or_spidercls argument cannot be a spider object, "
-                "it must be a spider class (or a Crawler object)"
-            )
-        if isinstance(crawler_or_spidercls, Crawler):
-            return crawler_or_spidercls
-        return self._create_crawler(crawler_or_spidercls)
-
-    def _create_crawler(self, spidercls: str | type[Spider]) -> Crawler:
-        if isinstance(spidercls, str):
-            spidercls = self.spider_loader.load(spidercls)
-        return Crawler(spidercls, self.settings)
-
     def stop(self) -> Deferred[Any]:
         """
         Stops simultaneously all the crawling jobs taking place.
