0fcf1fb1d9217716166267eb2bb0035c61596003
SATYAM GUPTA <143061930+Satyamgupta2365@users.noreply.github.com>
2025-05-20 18:00:35 +0530

    settings.py

---
+++ scrapy-priority-scheduler/scrapy_priority_scheduler/settings.py
@@ -0,0 +1,5 @@
+# Custom settings for the PriorityScheduler plugin
+CUSTOM_SETTINGS = {
+    "SCHEDULER": "scrapy_priority_scheduler.scheduler.PriorityScheduler",
+    "PRIORITY_SCHEDULER_MULTIPLIER": 2.0,
+}
