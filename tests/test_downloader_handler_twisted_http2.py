8ae418df44b7a107a4abe1a718721742d7e33fc0
Andrey Rakhmatullin <wrar@wrar.name>
2025-06-02 19:02:08 +0500

    Rewrite download handler tests to coroutines. (#6846)

--- tests/test_downloader_handler_twisted_http2.py
+++ tests/test_downloader_handler_twisted_http2.py
@@ -15,6 +15,10 @@ from twisted.web.http import H2_ENABLED
 
 from scrapy.http import Request
 from scrapy.spiders import Spider
+from scrapy.utils.defer import (
+    deferred_f_from_coro_f,
+    maybe_deferred_to_future,
+)
 from scrapy.utils.misc import build_from_crawler
 from scrapy.utils.test import get_crawler
 from tests.mockserver import ssl_context_factory
@@ -50,15 +54,14 @@ class H2DownloadHandlerMixin:
 class TestHttps2(H2DownloadHandlerMixin, TestHttps11Base):
     HTTP2_DATALOSS_SKIP_REASON = "Content-Length mismatch raises InvalidBodyLengthError"
 
-    def test_protocol(self):
+    @deferred_f_from_coro_f
+    async def test_protocol(self):
         request = Request(self.getURL("host"), method="GET")
-        d = self.download_request(request, Spider("foo"))
-        d.addCallback(lambda r: r.protocol)
-        d.addCallback(self.assertEqual, "h2")
-        return d
+        response = await self.download_request(request, Spider("foo"))
+        assert response.protocol == "h2"
 
-    @defer.inlineCallbacks
-    def test_download_with_maxsize_very_large_file(self):
+    @deferred_f_from_coro_f
+    async def test_download_with_maxsize_very_large_file(self):
         from twisted.internet import reactor
 
         with mock.patch("scrapy.core.http2.stream.logger") as logger:
@@ -67,8 +70,10 @@ class TestHttps2(H2DownloadHandlerMixin, TestHttps11Base):
             def check(logger):
                 logger.error.assert_called_once_with(mock.ANY)
 
-            d = self.download_request(request, Spider("foo", download_maxsize=1500))
-            yield self.assertFailure(d, defer.CancelledError, error.ConnectionAborted)
+            with pytest.raises((defer.CancelledError, error.ConnectionAborted)):
+                await self.download_request(
+                    request, Spider("foo", download_maxsize=1500)
+                )
 
             # As the error message is logged in the dataReceived callback, we
             # have to give a bit of time to the reactor to process the queue
@@ -76,13 +81,13 @@ class TestHttps2(H2DownloadHandlerMixin, TestHttps11Base):
             d = defer.Deferred()
             d.addCallback(check)
             reactor.callLater(0.1, d.callback, logger)
-            yield d
+            await maybe_deferred_to_future(d)
 
-    @defer.inlineCallbacks
-    def test_unsupported_scheme(self):
+    @deferred_f_from_coro_f
+    async def test_unsupported_scheme(self):
         request = Request("ftp://unsupported.scheme")
-        d = self.download_request(request, Spider("foo"))
-        yield self.assertFailure(d, SchemeNotSupported)
+        with pytest.raises(SchemeNotSupported):
+            await self.download_request(request, Spider("foo"))
 
     def test_download_broken_content_cause_data_loss(self, url="broken"):
         pytest.skip(self.HTTP2_DATALOSS_SKIP_REASON)
@@ -102,70 +107,60 @@ class TestHttps2(H2DownloadHandlerMixin, TestHttps11Base):
     def test_download_broken_chunked_content_allow_data_loss_via_setting(self):
         pytest.skip(self.HTTP2_DATALOSS_SKIP_REASON)
 
-    def test_concurrent_requests_same_domain(self):
+    @deferred_f_from_coro_f
+    async def test_concurrent_requests_same_domain(self):
         spider = Spider("foo")
 
         request1 = Request(self.getURL("file"))
-        d1 = self.download_request(request1, spider)
-        d1.addCallback(lambda r: r.body)
-        d1.addCallback(self.assertEqual, b"0123456789")
+        response1 = await self.download_request(request1, spider)
+        assert response1.body == b"0123456789"
 
         request2 = Request(self.getURL("echo"), method="POST")
-        d2 = self.download_request(request2, spider)
-        d2.addCallback(lambda r: r.headers["Content-Length"])
-        d2.addCallback(self.assertEqual, b"79")
-
-        return defer.DeferredList([d1, d2])
+        response2 = await self.download_request(request2, spider)
+        assert response2.headers["Content-Length"] == b"79"
 
     @pytest.mark.xfail(reason="https://github.com/python-hyper/h2/issues/1247")
-    def test_connect_request(self):
+    @deferred_f_from_coro_f
+    async def test_connect_request(self):
         request = Request(self.getURL("file"), method="CONNECT")
-        d = self.download_request(request, Spider("foo"))
-        d.addCallback(lambda r: r.body)
-        d.addCallback(self.assertEqual, b"")
-        return d
+        response = await self.download_request(request, Spider("foo"))
+        assert response.body == b""
 
-    def test_custom_content_length_good(self):
+    @deferred_f_from_coro_f
+    async def test_custom_content_length_good(self):
         request = Request(self.getURL("contentlength"))
         custom_content_length = str(len(request.body))
         request.headers["Content-Length"] = custom_content_length
-        d = self.download_request(request, Spider("foo"))
-        d.addCallback(lambda r: r.text)
-        d.addCallback(self.assertEqual, custom_content_length)
-        return d
+        response = await self.download_request(request, Spider("foo"))
+        assert response.text == custom_content_length
 
-    def test_custom_content_length_bad(self):
+    @deferred_f_from_coro_f
+    async def test_custom_content_length_bad(self):
         request = Request(self.getURL("contentlength"))
         actual_content_length = str(len(request.body))
         bad_content_length = str(len(request.body) + 1)
         request.headers["Content-Length"] = bad_content_length
-        log = LogCapture()
-        d = self.download_request(request, Spider("foo"))
-        d.addCallback(lambda r: r.text)
-        d.addCallback(self.assertEqual, actual_content_length)
-        d.addCallback(
-            lambda _: log.check_present(
-                (
-                    "scrapy.core.http2.stream",
-                    "WARNING",
-                    f"Ignoring bad Content-Length header "
-                    f"{bad_content_length!r} of request {request}, sending "
-                    f"{actual_content_length!r} instead",
-                )
+        with LogCapture() as log:
+            response = await self.download_request(request, Spider("foo"))
+        assert response.text == actual_content_length
+        log.check_present(
+            (
+                "scrapy.core.http2.stream",
+                "WARNING",
+                f"Ignoring bad Content-Length header "
+                f"{bad_content_length!r} of request {request}, sending "
+                f"{actual_content_length!r} instead",
             )
         )
-        d.addCallback(lambda _: log.uninstall())
-        return d
 
-    def test_duplicate_header(self):
+    @deferred_f_from_coro_f
+    async def test_duplicate_header(self):
         request = Request(self.getURL("echo"))
         header, value1, value2 = "Custom-Header", "foo", "bar"
         request.headers.appendlist(header, value1)
         request.headers.appendlist(header, value2)
-        d = self.download_request(request, Spider("foo"))
-        d.addCallback(lambda r: json.loads(r.text)["headers"][header])
-        d.addCallback(self.assertEqual, [value1, value2])
-        return d
+        response = await self.download_request(request, Spider("foo"))
+        assert json.loads(response.text)["headers"][header] == [value1, value2]
 
 
 class Https2WrongHostnameTestCase(H2DownloadHandlerMixin, TestHttpsWrongHostnameBase):
@@ -222,12 +217,13 @@ class Https2ProxyTestCase(H2DownloadHandlerMixin, TestHttpProxyBase):
         self.download_handler = build_from_crawler(
             self.download_handler_cls, get_crawler()
         )
-        self.download_request = self.download_handler.download_request
 
     def getURL(self, path):
         return f"{self.scheme}://{self.host}:{self.portno}/{path}"
 
-    @defer.inlineCallbacks
-    def test_download_with_proxy_https_timeout(self):
+    @deferred_f_from_coro_f
+    async def test_download_with_proxy_https_timeout(self):
         with pytest.raises(NotImplementedError):
-            yield super().test_download_with_proxy_https_timeout()
+            await maybe_deferred_to_future(
+                super().test_download_with_proxy_https_timeout()
+            )
