4f76f772b2d0fa21587f55a98c02f1bed85ad97f
Andrey Rakhmatullin <wrar@wrar.name>
2025-06-04 15:24:00 +0500

    Clarify the header.

--- scrapy/settings/default_settings.py
+++ scrapy/settings/default_settings.py
@@ -1,17 +1,16 @@
-"""
-This module contains the default values for all settings used by Scrapy.
+"""This module contains the default values for all settings used by Scrapy.
 
 For more information about these settings you can read the settings
 documentation in docs/topics/settings.rst
 
 Scrapy developers, if you add a setting here remember to:
 
-* add it in alphabetical order , with the exception that enabling flags and
+* add it in alphabetical order, with the exception that enabling flags and
   other high-level settings for a group should come first in their group
+  and pairs like host/port and user/password should be in the usual order
 * group similar settings without leaving blank lines
 * add its documentation to the available settings documentation
   (docs/topics/settings.rst)
-
 """
 
 import sys
