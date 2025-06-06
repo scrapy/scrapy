148753a15608714cfb50497902e762b966093c9e
Andrey Rakhmatullin <wrar@wrar.name>
2025-06-06 15:18:27 +0500

    Don't try to close ExecutionEngine.downloader when it doesn't exist.

--- scrapy/core/engine.py
+++ scrapy/core/engine.py
@@ -174,7 +174,8 @@ class ExecutionEngine:
             return self.close_spider(
                 self.spider, reason="shutdown"
             )  # will also close downloader
-        self.downloader.close()
+        if hasattr(self, "downloader"):
+            self.downloader.close()
         return succeed(None)
 
     def pause(self) -> None:
