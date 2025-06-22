1d37c9323bd3989741cc1b111ffee24af27449fb
Andrey Rakhmatullin <wrar@wrar.name>
2025-06-22 22:01:49 +0500

    Refactoring of test_utils_*.

--- tests/test_toplevel.py
+++ tests/test_toplevel.py
@@ -1,31 +1,35 @@
 import scrapy
 
 
-class TestToplevel:
-    def test_version(self):
-        assert isinstance(scrapy.__version__, str)
+def test_version():
+    assert isinstance(scrapy.__version__, str)
 
-    def test_version_info(self):
-        assert isinstance(scrapy.version_info, tuple)
 
-    def test_request_shortcut(self):
-        from scrapy.http import FormRequest, Request
+def test_version_info():
+    assert isinstance(scrapy.version_info, tuple)
 
-        assert scrapy.Request is Request
-        assert scrapy.FormRequest is FormRequest
 
-    def test_spider_shortcut(self):
-        from scrapy.spiders import Spider
+def test_request_shortcut():
+    from scrapy.http import FormRequest, Request
 
-        assert scrapy.Spider is Spider
+    assert scrapy.Request is Request
+    assert scrapy.FormRequest is FormRequest
 
-    def test_selector_shortcut(self):
-        from scrapy.selector import Selector
 
-        assert scrapy.Selector is Selector
+def test_spider_shortcut():
+    from scrapy.spiders import Spider
 
-    def test_item_shortcut(self):
-        from scrapy.item import Field, Item
+    assert scrapy.Spider is Spider
 
-        assert scrapy.Item is Item
-        assert scrapy.Field is Field
+
+def test_selector_shortcut():
+    from scrapy.selector import Selector
+
+    assert scrapy.Selector is Selector
+
+
+def test_item_shortcut():
+    from scrapy.item import Field, Item
+
+    assert scrapy.Item is Item
+    assert scrapy.Field is Field
--- tests/test_utils_gz.py
+++ tests/test_utils_gz.py
@@ -11,47 +11,51 @@ from tests import tests_datadir
 SAMPLEDIR = Path(tests_datadir, "compressed")
 
 
-class TestGunzip:
-    def test_gunzip_basic(self):
-        r1 = Response(
-            "http://www.example.com",
-            body=(SAMPLEDIR / "feed-sample1.xml.gz").read_bytes(),
-        )
-        assert gzip_magic_number(r1)
+def test_gunzip_basic():
+    r1 = Response(
+        "http://www.example.com",
+        body=(SAMPLEDIR / "feed-sample1.xml.gz").read_bytes(),
+    )
+    assert gzip_magic_number(r1)
 
-        r2 = Response("http://www.example.com", body=gunzip(r1.body))
-        assert not gzip_magic_number(r2)
-        assert len(r2.body) == 9950
+    r2 = Response("http://www.example.com", body=gunzip(r1.body))
+    assert not gzip_magic_number(r2)
+    assert len(r2.body) == 9950
 
-    def test_gunzip_truncated(self):
-        text = gunzip((SAMPLEDIR / "truncated-crc-error.gz").read_bytes())
-        assert text.endswith(b"</html")
 
-    def test_gunzip_no_gzip_file_raises(self):
-        with pytest.raises(BadGzipFile):
-            gunzip((SAMPLEDIR / "feed-sample1.xml").read_bytes())
+def test_gunzip_truncated():
+    text = gunzip((SAMPLEDIR / "truncated-crc-error.gz").read_bytes())
+    assert text.endswith(b"</html")
 
-    def test_gunzip_truncated_short(self):
-        r1 = Response(
-            "http://www.example.com",
-            body=(SAMPLEDIR / "truncated-crc-error-short.gz").read_bytes(),
-        )
-        assert gzip_magic_number(r1)
 
-        r2 = Response("http://www.example.com", body=gunzip(r1.body))
-        assert r2.body.endswith(b"</html>")
-        assert not gzip_magic_number(r2)
+def test_gunzip_no_gzip_file_raises():
+    with pytest.raises(BadGzipFile):
+        gunzip((SAMPLEDIR / "feed-sample1.xml").read_bytes())
 
-    def test_is_gzipped_empty(self):
-        r1 = Response("http://www.example.com")
-        assert not gzip_magic_number(r1)
 
-    def test_gunzip_illegal_eof(self):
-        text = html_to_unicode(
-            "charset=cp1252", gunzip((SAMPLEDIR / "unexpected-eof.gz").read_bytes())
-        )[1]
-        expected_text = (SAMPLEDIR / "unexpected-eof-output.txt").read_text(
-            encoding="utf-8"
-        )
-        assert len(text) == len(expected_text)
-        assert text == expected_text
+def test_gunzip_truncated_short():
+    r1 = Response(
+        "http://www.example.com",
+        body=(SAMPLEDIR / "truncated-crc-error-short.gz").read_bytes(),
+    )
+    assert gzip_magic_number(r1)
+
+    r2 = Response("http://www.example.com", body=gunzip(r1.body))
+    assert r2.body.endswith(b"</html>")
+    assert not gzip_magic_number(r2)
+
+
+def test_is_gzipped_empty():
+    r1 = Response("http://www.example.com")
+    assert not gzip_magic_number(r1)
+
+
+def test_gunzip_illegal_eof():
+    text = html_to_unicode(
+        "charset=cp1252", gunzip((SAMPLEDIR / "unexpected-eof.gz").read_bytes())
+    )[1]
+    expected_text = (SAMPLEDIR / "unexpected-eof-output.txt").read_text(
+        encoding="utf-8"
+    )
+    assert len(text) == len(expected_text)
+    assert text == expected_text
