b91fb99e0f739e046ea47051a3b68cc250295a99
Miguel Songco <126127866+miguelsongco@users.noreply.github.com>
2025-08-06 20:13:35 -0400

    attempt to fix 201 problem

--- scrapy/pipelines/files.py
+++ scrapy/pipelines/files.py
@@ -638,7 +638,31 @@ class FilesPipeline(MediaPipeline):
     ) -> FileInfo:
         referer = referer_str(request)
 
-        if response.status != 200:
+        # Enhanced handling for 201 Created responses
+        if response.status == 201:
+            if not response.body:
+                # If there's no body, check for Location header
+                location = response.headers.get(b"Location")
+                if location:
+                    new_url = location.decode()
+                    logger.debug(
+                        "File (201): no body, following Location header to %(new_url)s "
+                        "(referer: %(referer)s)",
+                        {"new_url": new_url, "referer": referer},
+                        extra={"spider": info.spider},
+                    )
+                    # Create new request to the Location URL
+                    new_request = request.replace(url=new_url, dont_filter=True)
+                    response = self._download_func(new_request, info.spider)
+                else:
+                    logger.warning(
+                        "File (201): no body and no Location header for %(request)s",
+                        {"request": request},
+                        extra={"spider": info.spider},
+                    )
+                    raise FileException("no-content-or-location")
+                    
+        if response.status not in (200, 201):
             logger.warning(
                 "File (code: %(status)s): Error downloading file from "
                 "%(request)s referred in <%(referer)s>",
