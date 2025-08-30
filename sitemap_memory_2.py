7788a595b43be5e981f11fd278a82792b28c4476
abebus <anamaev263@gmail.com>
2025-08-30 18:09:12 +0300

    more cleaner approach for releasing data + fix typing
    (+accidentally merged wrong branch)

--- sitemap_memory_2.py
+++
@@ -1,9 +0,0 @@
-from scrapy.utils.sitemap import Sitemap
-
-with open("/mnt/d/Downloads/sitemap_cz.xml", 'rb') as f:
-    content = f.read()
-
-import memray
-
-with memray.Tracker(f"{__file__}_memray.bin"):
-    Sitemap(content)
