a39351161dc4dbdbd2f7277d8ecda10e396e7fc5
Andrey Rakhmatullin <wrar@wrar.name>
2025-09-20 22:34:27 +0500

    Cache the startproject result in TestProjectBase.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import argparse
 import json
 from io import StringIO
+from shutil import copytree
 from typing import TYPE_CHECKING
 from unittest import mock
 
@@ -66,11 +67,22 @@ class TestProjectBase:
 
     project_name = "testproject"
 
-    @pytest.fixture
-    def proj_path(self, tmp_path: Path) -> Path:
-        """Create a Scrapy project in a temporary directory and return its path."""
+    @pytest.fixture(scope="session")
+    def _proj_path_cached(self, tmp_path_factory: pytest.TempPathFactory) -> Path:
+        """Create a Scrapy project in a temporary directory and return its path.
+
+        Used as a cache for ``proj_path``.
+        """
+        tmp_path = tmp_path_factory.mktemp("proj")
         call("startproject", self.project_name, cwd=tmp_path)
         return tmp_path / self.project_name
+
+    @pytest.fixture
+    def proj_path(self, tmp_path: Path, _proj_path_cached: Path) -> Path:
+        """Copy a pre-generated Scrapy project into a temporary directory and return its path."""
+        proj_path = tmp_path / self.project_name
+        copytree(_proj_path_cached, proj_path)
+        return proj_path
 
 
 class TestCommandCrawlerProcess(TestProjectBase):
