6b25bbb9d2ac86773dd4619a61dd17dfd79da020
Leonardo Tozzo <86009937+Icorebleidd@users.noreply.github.com>
2025-11-14 22:00:55 +0100

    Remove OldStyleClass from deprecation tests
    Removed OldStyleClass from test assertions to clean up deprecated code.

--- tests/test_utils_deprecate.py
+++ tests/test_utils_deprecate.py
@@ -169,9 +169,6 @@ class TestWarnWhenSubclassed:
             class UnrelatedClass:
                 pass
 
-            class OldStyleClass:
-                pass
-
         assert issubclass(UpdatedUserClass1, NewName)
         assert issubclass(UpdatedUserClass1a, NewName)
         assert issubclass(UpdatedUserClass1, DeprecatedName)
@@ -204,9 +201,6 @@ class TestWarnWhenSubclassed:
             class UnrelatedClass:
                 pass
 
-            class OldStyleClass:
-                pass
-
         assert isinstance(UpdatedUserClass2(), NewName)
         assert isinstance(UpdatedUserClass2a(), NewName)
         assert isinstance(UpdatedUserClass2(), DeprecatedName)
@@ -216,7 +210,6 @@ class TestWarnWhenSubclassed:
         assert not isinstance(OutdatedUserClass2a(), OutdatedUserClass2)
         assert not isinstance(OutdatedUserClass2(), OutdatedUserClass2a)
         assert not isinstance(UnrelatedClass(), DeprecatedName)
-        assert not isinstance(OldStyleClass(), DeprecatedName)
 
     def test_clsdict(self):
         with warnings.catch_warnings():
