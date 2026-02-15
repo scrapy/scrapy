f88eb01499999f2c09b2f45ed0b1fa686aacb84b
Andrey Rakhmatullin <wrar@wrar.name>
2026-02-15 12:25:35 +0500

    Refactor test_engine_loop.py.

--- tests/utils/__init__.py
+++ tests/utils/__init__.py
@@ -1,15 +1,26 @@
+import asyncio
 import os
 from pathlib import Path
 
 from twisted.internet.defer import Deferred
 
+from scrapy.utils.asyncio import is_asyncio_available
+from scrapy.utils.defer import maybe_deferred_to_future
 
-def twisted_sleep(seconds):
+
+def twisted_sleep(seconds: float):
     from twisted.internet import reactor
 
-    d = Deferred()
+    d: Deferred[None] = Deferred()
     reactor.callLater(seconds, d.callback, None)
     return d
+
+
+async def async_sleep(seconds: float) -> None:
+    if is_asyncio_available():
+        await asyncio.sleep(seconds)
+    else:
+        await maybe_deferred_to_future(twisted_sleep(seconds))
 
 
 def get_script_run_env() -> dict[str, str]:
