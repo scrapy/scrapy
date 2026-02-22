a82873c2b4917d670e52a41d62211bce9b970380
priyanshupaikra <priyanshu.paikra@gmail.com>
2026-02-22 22:24:23 +0530

    fix CrawlTestCase.test_start_requests_lazyness

--- scrapy/core/engine.py
+++ scrapy/core/engine.py
@@ -303,12 +303,14 @@ class ExecutionEngine:
             self._slot.heartbeat.start(self._SLOT_HEARTBEAT_INTERVAL)
 
             while self._start and self.spider and self.running:
-                await self._process_start_next()
-                if not self.needs_backout():
-                    # Give room for the outcome of self._process_start_next() to be
-                    # processed before continuing with the next iteration.
-                    self._slot.nextcall.schedule()
+                if self.needs_backout():
                     await self._slot.nextcall.wait()
+                    continue
+                await self._process_start_next()
+                # Give room for the outcome of self._process_start_next() to be
+                # processed before continuing with the next iteration.
+                self._slot.nextcall.schedule()
+                await self._slot.nextcall.wait()
         except (asyncio.exceptions.CancelledError, CancelledError):
             # self.stop_async() has cancelled us, nothing to do
             return
