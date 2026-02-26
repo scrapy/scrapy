d1e29557cb89c2a035481b5ed1f644b4082bcc0f
Julian Hamze <jhamze03@gmail.com>
2026-02-26 09:24:24 -0500

    Covered 'non-zero port' branch for httpx

--- tests/test_downloader_handler_httpx.py
+++ tests/test_downloader_handler_httpx.py
@@ -55,6 +55,19 @@ class TestHttp11(HttpxDownloadHandlerMixin, TestHttp11Base):
         )
 
     @coroutine_test
+    async def test_bind_address_port_warning(
+        self, caplog: pytest.LogCaptureFixture, mockserver: MockServer
+    ) -> None:
+        request = Request(mockserver.url("/client-ip"))
+        async with self.get_dh(
+            {"DOWNLOAD_BIND_ADDRESS": ("127.0.0.2", 12345)}
+        ) as download_handler:
+            response = await download_handler.download_request(request)
+        assert response.body == b"127.0.0.2"
+        assert "DOWNLOAD_BIND_ADDRESS specifies a port (12345)" in caplog.text
+        assert "Ignoring the port" in caplog.text
+
+    @coroutine_test
     async def test_unsupported_proxy(
         self, caplog: pytest.LogCaptureFixture, mockserver: MockServer
     ) -> None:
