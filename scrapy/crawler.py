7a1d74f2d23bccdfdfad0df0e2dd18ea945f9d8e
Kaileshwar16 <kaileshwar2005@gmail.com>
2026-03-27 14:54:13 +0530

    change on crawler defaults and news.rst formatting

--- scrapy/crawler.py
+++ scrapy/crawler.py
@@ -634,11 +634,7 @@ class CrawlerProcessBase(CrawlerRunnerBase):
             )
             resolver_cls_path = self.settings.get("DNS_RESOLVER")
         else:
-            resolver_cls_path = self.settings.get(
-                "TWISTED_DNS_RESOLVER", "scrapy.resolver.CachingThreadedResolver"
-            )
-        if not resolver_cls_path:
-            resolver_cls_path = "scrapy.resolver.CachingThreadedResolver"
+            resolver_cls_path = self.settings.get("TWISTED_DNS_RESOLVER")
 
         resolver_class = load_object(resolver_cls_path)
 
