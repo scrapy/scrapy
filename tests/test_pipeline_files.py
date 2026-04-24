929686d38189dfd4f8faac14e7bcab2f75b98015
Andrey Rakhmatullin <wrar@wrar.name>
2026-04-24 13:39:48 +0500

    Fix another test for .php handling change.

--- tests/test_pipeline_files.py
+++ tests/test_pipeline_files.py
@@ -115,8 +115,8 @@ class TestFilesPipeline:
         req1 = Request("http://foo.bar/baz.txt?fizz")
         assert file_path(req1) == "full/a2b4913a62f65445aeae2bac08cd8c3b41d7195e.txt"
 
-        req2 = Request("http://foo.bar/get_img.php?file=photo.jpg")
-        assert file_path(req2) == "full/118230fd648f1080c81c234d5e2463ea496f8c05.jpg"
+        req2 = Request("http://foo.bar/get_img.foo?file=photo.jpg")
+        assert file_path(req2) == "full/7fc9461c9fd836515bea6983373097203a7d748e.jpg"
 
     def test_file_path(self):
         file_path = self.pipeline.file_path
