d8d7de2339b3aeabdc144fa7d0d11cae3ecfd1bb
Sriniketh24 <subramanians1@ufl.edu>
2026-06-26 07:56:59 +0530

    Fix FTPDownloadHandler not closing FTP connection after download (#7667)

--- scrapy/core/downloader/handlers/ftp.py
+++ scrapy/core/downloader/handlers/ftp.py
@@ -119,7 +119,10 @@ class FTPDownloadHandler(BaseDownloadHandler):
                 httpcode = self.CODE_MAPPING.get(ftpcode, self.CODE_MAPPING["default"])
                 return Response(url=request.url, status=httpcode, body=message.encode())
             raise
-        protocol.close()
+        finally:
+            protocol.close()
+            assert client.transport
+            client.transport.loseConnection()
         headers = {"local filename": protocol.filename or b"", "size": protocol.size}
         body = protocol.filename or protocol.body.read()
         respcls = responsetypes.from_args(url=request.url, body=body)
