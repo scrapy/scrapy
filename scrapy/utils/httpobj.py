58ed9fdcccc9d8a3c278a0bd0af54ff77863b034
Adrian <adrian@zyte.com>
2026-07-24 17:08:58 +0200

    Document urlparse_cached (#7777)

--- scrapy/utils/httpobj.py
+++ scrapy/utils/httpobj.py
@@ -16,8 +16,15 @@ _urlparse_cache: WeakKeyDictionary[Request | Response, ParseResult] = (
 
 
 def urlparse_cached(request_or_response: Request | Response) -> ParseResult:
-    """Return urlparse.urlparse caching the result, where the argument can be a
-    Request or Response object
+    """Return the result of parsing the URL of *request_or_response*, a
+    :class:`~scrapy.Request` or :class:`~scrapy.http.Response` object, with
+    :func:`urllib.parse.urlparse`.
+
+    The result is cached, using a :class:`weakref.WeakKeyDictionary` keyed on
+    *request_or_response*, so that the URL of a given object is parsed only
+    once. Prefer this function over calling :func:`urllib.parse.urlparse` on
+    ``request_or_response.url`` directly when the same URL may be parsed more
+    than once.
     """
     if request_or_response not in _urlparse_cache:
         _urlparse_cache[request_or_response] = urlparse(request_or_response.url)
