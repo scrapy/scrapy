b14d15e51383403eb29716a0affc95c27b583f46
Adrian Chaves <adrian@zyte.com>
2026-07-24 16:31:28 +0200

    Silence mypy

--- tests/test_engine_close_spider.py
+++ tests/test_engine_close_spider.py
@@ -67,7 +67,9 @@ async def test_exception_downloader(
     engine = ExecutionEngine(crawler, lambda _: None)
     crawler.engine = engine
     await engine.open_spider_async()
-    engine.downloader.close = Mock(side_effect=Exception("close failed"))
+    engine.downloader.close = Mock(  # type: ignore[method-assign]
+        side_effect=Exception("close failed")
+    )
     await engine.close_spider_async()
     assert "Downloader close failure" in caplog.text
 
