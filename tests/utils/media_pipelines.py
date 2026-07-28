c4676575478e9747e0213ddeeca7e2bc242d2dea
Adrian Chaves <adrian@zyte.com>
2026-07-28 15:06:17 +0200

    Improve test coverage for scrapy.pipelines

--- tests/utils/media_pipelines.py
+++ tests/utils/media_pipelines.py
@@ -3,6 +3,12 @@ from __future__ import annotations
 from typing import Any
 
 from scrapy.http.request import NO_CALLBACK, Request
+from scrapy.pipelines.media import MediaPipeline
+from scrapy.utils.spider import DefaultSpider
+
+# required by persist_file() and stat_file(), but as some stores don't use the argument
+# we can pass this singleton to keep type hints correct
+DUMMY_SPIDER_INFO = MediaPipeline.SpiderInfo(DefaultSpider())
 
 
 async def mocked_download_func(request: Request) -> Any:
