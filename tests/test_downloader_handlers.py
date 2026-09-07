cc840d63889cc6575f7ba573b1c587f118b320fe
Adrian Chaves <adrian@zyte.com>
2026-09-07 09:56:43 +0200

    Fix mypy, pylint and codspeed CI failures

--- tests/test_downloader_handlers.py
+++ tests/test_downloader_handlers.py
@@ -423,7 +423,7 @@ class TestDataURI:
         request = Request("data:image/png;base64,iVBORw0KGgo%3D")
         response = await self.download_request(request)
         assert response.body == b"\x89PNG\r\n\x1a\n"
-        assert type(response) is responsetypes.from_mimetype("image/png")  # pylint: disable=unidiomatic-typecheck
+        assert type(response) is responsetypes.from_mimetype("image/png")
         assert not isinstance(response, TextResponse)
 
     @coroutine_test
